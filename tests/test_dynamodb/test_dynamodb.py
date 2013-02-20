25734f0c85b30bd7ac906595d5b0646fcee47679
zmsmith <zach@yipit.com>
2013-02-19 20:01:31 -0500

    Fix misnamed test

--- tests/test_dynamodb/test_dynamodb.py
+++ tests/test_dynamodb/test_dynamodb.py
@@ -5,7 +5,7 @@ from moto.dynamodb import dynamodb_backend
 
 
 @mock_dynamodb
-def test_create_table():
+def test_list_tables():
     name = "TestTable"
     dynamodb_backend.create_table(name)
     conn = boto.connect_dynamodb('the_key', 'the_secret')
