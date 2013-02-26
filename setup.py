024624045f7b7abe8dc473f3b1408ea96182900d
Steve Pulec <spulec@gmail.com>
2013-02-25 23:56:41 -0500

    add boto to install requires

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='moto',
-    version='0.0.1',
+    version='0.0.2',
     description='Moto is a library that allows your python tests to easily mock out the boto library',
     author='Steve Pulec',
     author_email='spulec@gmail',
     url='https://github.com/spulec/moto',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "boto"
+    ],
 )
