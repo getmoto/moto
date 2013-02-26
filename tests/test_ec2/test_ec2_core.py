156c9ba4c44d268fa332ef2870c558389473b20b
Steve Pulec <spulec@gmail.com>
2013-02-25 23:29:15 -0500

    test ec2 not implemented

---
+++ tests/test_ec2/test_ec2_core.py
@@ -0,0 +1,8 @@
+import requests
+from moto import mock_ec2
+
+
+@mock_ec2
+def test_not_implemented_method():
+    requests.post.when.called_with("https://ec2.us-east-1.amazonaws.com/",
+        data={'Action':['foobar']}).should.throw(NotImplementedError)
