91b61c7be54f69c29294cd76362219b355c52ffd
Steve Pulec <spulec@gmail.com>
2013-02-26 15:15:20 -0500

    another s3 http fix

--- moto/s3/urls.py
+++ moto/s3/urls.py
@@ -3,7 +3,7 @@ from .responses import all_buckets, bucket_response, key_response
 base_url = "https?://(.*).s3.amazonaws.com"
 
 urls = {
-    'https://s3.amazonaws.com/$': all_buckets,
+    'https?://s3.amazonaws.com/$': all_buckets,
     '{0}/$'.format(base_url): bucket_response,
     '{}/(.+)'.format(base_url): key_response,
 }
