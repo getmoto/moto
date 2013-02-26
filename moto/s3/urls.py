3542f4ec3074658c4cbf9044dfdbb3b13f8a53b0
Steve Pulec <spulec@gmail.com>
2013-02-26 15:12:38 -0500

    make s3 handle http

--- moto/s3/urls.py
+++ moto/s3/urls.py
@@ -1,6 +1,6 @@
 from .responses import all_buckets, bucket_response, key_response
 
-base_url = "https://(.*).s3.amazonaws.com"
+base_url = "https?://(.*).s3.amazonaws.com"
 
 urls = {
     'https://s3.amazonaws.com/$': all_buckets,
