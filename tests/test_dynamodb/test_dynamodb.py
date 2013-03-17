d49baae6440fde3dc809448e76d5e580cc83782f
Steve Pulec <spulec@gmail.com>
2013-03-17 18:39:21 -0400

    add STS test

--- tests/test_dynamodb/test_dynamodb.py
+++ tests/test_dynamodb/test_dynamodb.py
@@ -1,6 +1,7 @@
 import boto
 import sure  # flake8: noqa
 from freezegun import freeze_time
+import requests
 
 from moto import mock_dynamodb
 from moto.dynamodb import dynamodb_backend
@@ -35,3 +36,10 @@ def test_list_tables_layer_1():
 def test_describe_missing_table():
     conn = boto.connect_dynamodb('the_key', 'the_secret')
     conn.describe_table.when.called_with('messages').should.throw(DynamoDBResponseError)
+
+
+@mock_dynamodb
+def test_sts_handler():
+    res = requests.post("https://sts.amazonaws.com/", data={"GetSessionToken": ""})
+    res.ok.should.be.ok
+    res.text.should.contain("SecretAccessKey")
