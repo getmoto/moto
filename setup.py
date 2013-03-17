ec3ab5ade76276c6b600e54d9cfb940ab9ba9bd3
Steve Pulec <spulec@gmail.com>
2013-03-17 18:24:54 -0400

    fix httpretty dependency link

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
         "boto",
         "Jinja2",
         "flask",
-        "spulec-httpretty"
+        "spulec-httpretty>=0.5.12",
     ],
     dependency_links=[
-        'http://github.com/spulec/HTTPretty/tarball/master#egg=spulec-httpretty',
+        'http://github.com/spulec/HTTPretty/tarball/master#egg=spulec-httpretty-0.5.12',
     ]
 )
