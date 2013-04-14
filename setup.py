afc7a64a341f08fde6a51ed8acc79f9691ce5ef8
Steve Pulec <spulec@gmail.com>
2013-04-14 11:17:25 -0400

    0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='moto',
-    version='0.1.2',
+    version='0.1.3',
     description='A library that allows your python tests to easily'
                 ' mock out the boto library',
     author='Steve Pulec',
