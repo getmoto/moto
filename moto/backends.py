ded410460f3e8f3c9a614d0f3eeda8d0febd4a09
Steve Pulec <spulec@gmail.com>
2013-08-07 20:36:58 -0400

    Add EMR backend

--- moto/backends.py
+++ moto/backends.py
@@ -2,6 +2,7 @@ from moto.autoscaling import autoscaling_backend
 from moto.dynamodb import dynamodb_backend
 from moto.ec2 import ec2_backend
 from moto.elb import elb_backend
+from moto.emr import emr_backend
 from moto.s3 import s3_backend
 from moto.ses import ses_backend
 from moto.sqs import sqs_backend
@@ -12,6 +13,7 @@ BACKENDS = {
     'dynamodb': dynamodb_backend,
     'ec2': ec2_backend,
     'elb': elb_backend,
+    'emr': emr_backend,
     's3': s3_backend,
     'ses': ses_backend,
     'sqs': sqs_backend,
