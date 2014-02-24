390d3b900cb686ba141298d2924a9fb2bd66f1a2
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-02-24 12:08:00 +0200

    add a first failing unit test for keypairs

--- tests/test_ec2/test_key_pairs.py
+++ tests/test_ec2/test_key_pairs.py
@@ -5,5 +5,6 @@ from moto import mock_ec2
 
 
 @mock_ec2
-def test_key_pairs():
-    pass
+def test_key_pairs_empty():
+    conn = boto.connect_ec2('the_key', 'the_secret')
+    assert len(conn.get_all_key_pairs()) == 0
