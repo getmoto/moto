fb558bb1ca0b27ad516a9c3c01f39e033497d5c4
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-03-20 18:29:39 +0200

    add support for sts get_federation_token

--- moto/sts/responses.py
+++ moto/sts/responses.py
@@ -12,6 +12,15 @@ class TokenResponse(BaseResponse):
         template = Template(GET_SESSION_TOKEN_RESPONSE)
         return template.render(token=token)
 
+    def get_federation_token(self):
+        duration = int(self.querystring.get('DurationSeconds', [43200])[0])
+        policy = self.querystring.get('Policy', None)[0]
+        name = self.querystring.get('Name')[0]
+        token = sts_backend.get_federation_token(
+            duration=duration, name=name, policy=policy)
+        template = Template(GET_FEDERATION_TOKEN_RESPONSE)
+        return template.render(token=token)
+
     def assume_role(self):
         role_session_name = self.querystring.get('RoleSessionName')[0]
         role_arn = self.querystring.get('RoleArn')[0]
@@ -44,6 +53,35 @@ GET_SESSION_TOKEN_RESPONSE = """<GetSessionTokenResponse xmlns="https://sts.amaz
     <RequestId>58c5dbae-abef-11e0-8cfe-09039844ac7d</RequestId>
   </ResponseMetadata>
 </GetSessionTokenResponse>"""
+
+
+GET_FEDERATION_TOKEN_RESPONSE = """<GetFederationTokenResponse xmlns="https://sts.amazonaws.com/doc/
+2011-06-15/">
+  <GetFederationTokenResult>
+    <Credentials>
+      <SessionToken>
+       AQoDYXdzEPT//////////wEXAMPLEtc764bNrC9SAPBSM22wDOk4x4HIZ8j4FZTwdQW
+       LWsKWHGBuFqwAeMicRXmxfpSPfIeoIYRqTflfKD8YUuwthAx7mSEI/qkPpKPi/kMcGd
+       QrmGdeehM4IC1NtBmUpp2wUE8phUZampKsburEDy0KPkyQDYwT7WZ0wq5VSXDvp75YU
+       9HFvlRd8Tx6q6fE8YQcHNVXAkiY9q6d+xo0rKwT38xVqr7ZD0u0iPPkUL64lIZbqBAz
+       +scqKmlzm8FDrypNC9Yjc8fPOLn9FX9KSYvKTr4rvx3iSIlTJabIQwj2ICCR/oLxBA==
+      </SessionToken>
+      <SecretAccessKey>
+       wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY
+      </SecretAccessKey>
+      <Expiration>{{ token.expiration_ISO8601 }}</Expiration>
+      <AccessKeyId>AKIAIOSFODNN7EXAMPLE</AccessKeyId>
+    </Credentials>
+    <FederatedUser>
+      <Arn>arn:aws:sts::123456789012:federated-user/{{ token.name }}</Arn>
+      <FederatedUserId>123456789012:{{ token.name }}</FederatedUserId>
+    </FederatedUser>
+    <PackedPolicySize>6</PackedPolicySize>
+  </GetFederationTokenResult>
+  <ResponseMetadata>
+    <RequestId>c6104cbe-af31-11e0-8154-cbc7ccf896c7</RequestId>
+  </ResponseMetadata>
+</GetFederationTokenResponse>"""
 
 
 ASSUME_ROLE_RESPONSE = """<AssumeRoleResponse xmlns="https://sts.amazonaws.com/doc/
