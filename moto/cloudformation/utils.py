e18046397de6623ffe33435f0c3ba9a6b837df3a
Steve Pulec <spulec@gmail.com>
2014-03-24 09:42:41 -0400

    Fix string formatting for py26

--- moto/cloudformation/utils.py
+++ moto/cloudformation/utils.py
@@ -3,4 +3,4 @@ import uuid
 
 def generate_stack_id(stack_name):
     random_id = uuid.uuid4()
-    return "arn:aws:cloudformation:us-east-1:123456789:stack/{}/{}".format(stack_name, random_id)
+    return "arn:aws:cloudformation:us-east-1:123456789:stack/{0}/{1}".format(stack_name, random_id)
