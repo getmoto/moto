df7e1398559db5bd9823b89a251686bb567c5b30
Steve Pulec <spulec@gmail.com>
2014-05-14 09:19:23 -0400

    0.3.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='moto',
-    version='0.3.0',
+    version='0.3.1',
     description='A library that allows your python tests to easily'
                 ' mock out the boto library',
     author='Steve Pulec',
