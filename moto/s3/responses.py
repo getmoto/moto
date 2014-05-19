dcf48a7a45d627909c950a06c9fc2a76a3adb3c4
Frank Mata <matafc@gmail.com>
2014-05-19 16:38:04 -0700

    key.size now reflects the same behavior as boto, added unit test

--- moto/s3/responses.py
+++ moto/s3/responses.py
@@ -287,7 +287,7 @@ class ResponseObject(object):
         if key:
             headers.update(key.metadata)
             headers.update(key.response_dict)
-            return 200, headers, ""
+            return 200, headers, key.value
         else:
             return 404, headers, ""
 
