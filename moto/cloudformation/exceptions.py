90191675a29d9627e99b7cceb85d3b3b02b988e6
Joseph Lawson <joe@joekiller.com>
2014-10-23 14:39:15 -0400

    Deleted Stack Fix and ValidationError for DescribeStacks.
      * stop backed from trying to iterate over empty list of deleted stacks.
      * Update DescribeStacks to throw ValidationError if stack name or id doesn't exist.

--- moto/cloudformation/exceptions.py
+++ moto/cloudformation/exceptions.py
@@ -1,6 +1,26 @@
 from __future__ import unicode_literals
+from boto.exception import BotoServerError
+from jinja2 import Template
+
 
 
 class UnformattedGetAttTemplateException(Exception):
     description = 'Template error: resource {0} does not support attribute type {1} in Fn::GetAtt'
     status_code = 400
+
+
+class ValidationError(BotoServerError):
+    def __init__(self, name_or_id):
+        template = Template(STACK_DOES_NOT_EXIST_RESPONSE)
+        super(ValidationError, self).__init__(status=400, reason='Bad Request',
+                                              body=template.render(name_or_id=name_or_id))
+
+STACK_DOES_NOT_EXIST_RESPONSE = """<ErrorResponse xmlns="http://cloudformation.amazonaws.com/doc/2010-05-15/">
+  <Error>
+    <Type>Sender</Type>
+    <Code>ValidationError</Code>
+    <Message>Stack:{{ name_or_id }} does not exist</Message>
+  </Error>
+  <RequestId>cf4c737e-5ae2-11e4-a7c9-ad44eEXAMPLE</RequestId>
+</ErrorResponse>
+"""
