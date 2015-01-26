4f822f58e87a23b5e08c1ea09d5f7798ae2ee486
Chris Henry <henry.christopher@gmail.com>
2015-01-26 13:33:11 -0500

    Loosen the url pattern a bit. I was seeing boto send requests to rds.amazonaws.com

--- moto/rds2/urls.py
+++ moto/rds2/urls.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from .responses import RDS2Response
 
 url_bases = [
-    "https?://rds.(.+).amazonaws.com",
+    "https?://rds(.+).amazonaws.com",
 ]
 
 url_paths = {
