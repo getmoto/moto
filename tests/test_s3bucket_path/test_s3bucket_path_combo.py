f46c2508c03850097b5c3812a5d93edc09ec6ede
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-02-20 21:50:49 +0200

    allow running mock_s3 and mock_s3bucket_path one after the other

---
+++ tests/test_s3bucket_path/test_s3bucket_path_combo.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+import boto
+from boto.s3.connection import OrdinaryCallingFormat
+
+from moto import mock_s3bucket_path, mock_s3
+
+
+def create_connection(key=None, secret=None):
+    return boto.connect_s3(key, secret, calling_format=OrdinaryCallingFormat())
+
+
+def test_bucketpath_combo_serial():
+    @mock_s3bucket_path
+    def make_bucket_path():
+        conn = create_connection()
+        conn.create_bucket('mybucketpath')
+
+    @mock_s3
+    def make_bucket():
+        conn = boto.connect_s3('the_key', 'the_secret')
+        conn.create_bucket('mybucket')
+
+    make_bucket()
+    make_bucket_path()
