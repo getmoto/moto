8573333932ad4f4dcd6c80ed119c022cfdcdc81f
Steve Pulec <spulec@gmail.com>
2015-08-02 10:04:13 -0400

    0.4.8

--- moto/__init__.py
+++ moto/__init__.py
@@ -3,7 +3,7 @@ import logging
 logging.getLogger('boto').setLevel(logging.CRITICAL)
 
 __title__ = 'moto'
-__version__ = '0.4.7'
+__version__ = '0.4.8'
 
 from .autoscaling import mock_autoscaling  # flake8: noqa
 from .cloudformation import mock_cloudformation  # flake8: noqa
