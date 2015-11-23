a06f8b15f546eb25f4afee3681cea912cab3ac01
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2015-11-23 14:09:31 +0100

    Add moto.core.utils.underscores_to_camelcase()

--- tests/test_core/test_utils.py
+++ tests/test_core/test_utils.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 import sure
 
-from moto.core.utils import camelcase_to_underscores
+from moto.core.utils import camelcase_to_underscores, underscores_to_camelcase
 
 
 def test_camelcase_to_underscores():
@@ -12,3 +12,11 @@ def test_camelcase_to_underscores():
     }
     for arg, expected in cases.items():
         camelcase_to_underscores(arg).should.equal(expected)
+
+
+def test_underscores_to_camelcase():
+    cases = {
+        "the_new_attribute": "theNewAttribute",
+    }
+    for arg, expected in cases.items():
+        underscores_to_camelcase(arg).should.equal(expected)
