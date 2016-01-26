8b998fbbc37792352f9f2fd234e787c1f3bc078c
Bastien Chatelard <chatel.bast@gmail.com>
2016-01-26 12:37:43 +0100

    Revert "Fix s3 url regex to ensure path-based bucket and key does not catch."
    This reverts commit a7938ed9ec814fa9cf53272ceb65e84d11d50dc1.

--- moto/s3/urls.py
+++ moto/s3/urls.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from moto.compat import OrderedDict
 from .responses import S3ResponseInstance
 
 url_bases = [
@@ -7,13 +8,13 @@ url_bases = [
     "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3(.*).amazonaws.com"
 ]
 
-url_paths = {
+url_paths = OrderedDict([
     # subdomain bucket
-    '{0}/$': S3ResponseInstance.bucket_response,
+    ('{0}/$', S3ResponseInstance.bucket_response),
 
     # subdomain key of path-based bucket
-    '{0}/(?P<key_or_bucket_name>[^/]+)/?$': S3ResponseInstance.ambiguous_response,
+    ('{0}/(?P<key_or_bucket_name>.+)', S3ResponseInstance.ambiguous_response),
 
     # path-based bucket + key
-    '{0}/(?P<bucket_name_path>[a-zA-Z0-9\-_./]+)/(?P<key_name>.+)': S3ResponseInstance.key_response,
-}
+    ('{0}/(?P<bucket_name_path>[a-zA-Z0-9\-_./]+)/(?P<key_name>.+)', S3ResponseInstance.key_response),
+])
