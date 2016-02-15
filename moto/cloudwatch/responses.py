d8ed18aaacb8f5a500a985d019c5cd5b1a01d415
beeva-antonioirizar <antonio.irizar@beeva.com>
2016-02-15 13:02:04 +0100

    add tag (<DescribeAlarmsResult>) to describe_alarms_template. Because it is miss and it can't work with boto3

--- moto/cloudwatch/responses.py
+++ moto/cloudwatch/responses.py
@@ -95,52 +95,54 @@ PUT_METRIC_ALARM_TEMPLATE = """<PutMetricAlarmResponse xmlns="http://monitoring.
 </PutMetricAlarmResponse>"""
 
 DESCRIBE_ALARMS_TEMPLATE = """<DescribeAlarmsResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
-    <MetricAlarms>
-        {% for alarm in alarms %}
-        <member>
-            <ActionsEnabled>{{ alarm.actions_enabled }}</ActionsEnabled>
-            <AlarmActions>
-                {% for action in alarm.alarm_actions %}
-                <member>{{ action }}</member>
-                {% endfor %}
-            </AlarmActions>
-            <AlarmArn>{{ alarm.arn }}</AlarmArn>
-            <AlarmConfigurationUpdatedTimestamp>{{ alarm.configuration_updated_timestamp }}</AlarmConfigurationUpdatedTimestamp>
-            <AlarmDescription>{{ alarm.description }}</AlarmDescription>
-            <AlarmName>{{ alarm.name }}</AlarmName>
-            <ComparisonOperator>{{ alarm.comparison_operator }}</ComparisonOperator>
-            <Dimensions>
-                {% for dimension in alarm.dimensions %}
-                <member>
-                    <Name>{{ dimension.name }}</Name>
-                    <Value>{{ dimension.value }}</Value>
-                </member>
-                {% endfor %}
-            </Dimensions>
-            <EvaluationPeriods>{{ alarm.evaluation_periods }}</EvaluationPeriods>
-            <InsufficientDataActions>
-                {% for action in alarm.insufficient_data_actions %}
-                <member>{{ action }}</member>
-                {% endfor %}
-            </InsufficientDataActions>
-            <MetricName>{{ alarm.metric_name }}</MetricName>
-            <Namespace>{{ alarm.namespace }}</Namespace>
-            <OKActions>
-                {% for action in alarm.ok_actions %}
-                <member>{{ action }}</member>
-                {% endfor %}
-            </OKActions>
-            <Period>{{ alarm.period }}</Period>
-            <StateReason>{{ alarm.state_reason }}</StateReason>
-            <StateReasonData>{{ alarm.state_reason_data }}</StateReasonData>
-            <StateUpdatedTimestamp>{{ alarm.state_updated_timestamp }}</StateUpdatedTimestamp>
-            <StateValue>{{ alarm.state_value }}</StateValue>
-            <Statistic>{{ alarm.statistic }}</Statistic>
-            <Threshold>{{ alarm.threshold }}</Threshold>
-            <Unit>{{ alarm.unit }}</Unit>
-        </member>
-        {% endfor %}
-    </MetricAlarms>
+    <DescribeAlarmsResult>
+        <MetricAlarms>
+            {% for alarm in alarms %}
+            <member>
+                <ActionsEnabled>{{ alarm.actions_enabled }}</ActionsEnabled>
+                <AlarmActions>
+                    {% for action in alarm.alarm_actions %}
+                    <member>{{ action }}</member>
+                    {% endfor %}
+                </AlarmActions>
+                <AlarmArn>{{ alarm.arn }}</AlarmArn>
+                <AlarmConfigurationUpdatedTimestamp>{{ alarm.configuration_updated_timestamp }}</AlarmConfigurationUpdatedTimestamp>
+                <AlarmDescription>{{ alarm.description }}</AlarmDescription>
+                <AlarmName>{{ alarm.name }}</AlarmName>
+                <ComparisonOperator>{{ alarm.comparison_operator }}</ComparisonOperator>
+                <Dimensions>
+                    {% for dimension in alarm.dimensions %}
+                    <member>
+                        <Name>{{ dimension.name }}</Name>
+                        <Value>{{ dimension.value }}</Value>
+                    </member>
+                    {% endfor %}
+                </Dimensions>
+                <EvaluationPeriods>{{ alarm.evaluation_periods }}</EvaluationPeriods>
+                <InsufficientDataActions>
+                    {% for action in alarm.insufficient_data_actions %}
+                    <member>{{ action }}</member>
+                    {% endfor %}
+                </InsufficientDataActions>
+                <MetricName>{{ alarm.metric_name }}</MetricName>
+                <Namespace>{{ alarm.namespace }}</Namespace>
+                <OKActions>
+                    {% for action in alarm.ok_actions %}
+                    <member>{{ action }}</member>
+                    {% endfor %}
+                </OKActions>
+                <Period>{{ alarm.period }}</Period>
+                <StateReason>{{ alarm.state_reason }}</StateReason>
+                <StateReasonData>{{ alarm.state_reason_data }}</StateReasonData>
+                <StateUpdatedTimestamp>{{ alarm.state_updated_timestamp }}</StateUpdatedTimestamp>
+                <StateValue>{{ alarm.state_value }}</StateValue>
+                <Statistic>{{ alarm.statistic }}</Statistic>
+                <Threshold>{{ alarm.threshold }}</Threshold>
+                <Unit>{{ alarm.unit }}</Unit>
+            </member>
+            {% endfor %}
+        </MetricAlarms>
+    </DescribeAlarmsResult>
 </DescribeAlarmsResponse>"""
 
 DELETE_METRIC_ALARMS_TEMPLATE = """<DeleteMetricAlarmResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
