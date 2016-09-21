9a7556b3af97c9ecbfad0f5d72ddb683df3ca301
Alfred Moreno <malfred@amazon.com>
2016-09-20 17:41:23 -0700

    Add support and unittests for Route53 tags

--- moto/route53/urls.py
+++ moto/route53/urls.py
@@ -10,5 +10,6 @@ url_paths = {
     '{0}hostedzone/[^/]+$': responses.get_or_delete_hostzone_response,
     '{0}hostedzone/[^/]+/rrset/?$': responses.rrset_response,
     '{0}healthcheck': responses.health_check_response,
-    '{0}tags|trafficpolicyinstances/*': responses.not_implemented_response,
+    '{0}tags/(healthcheck|hostedzone)/*': responses.list_or_change_tags_for_resource_request,
+    '{0}trafficpolicyinstances/*': responses.not_implemented_response
 }
