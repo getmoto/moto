5dc8e59fab9bd2009437914cb574b1c479239d8b
mfranke <2mf@users.noreply.github.com>
2016-12-04 00:15:24 +0100

    Fix s3bucket_path (#784)
    * check HTTP header for IPv4 or IPv6 addresses and default to path based S3
    
    * improved IPv4 and IPv6 checking with optional ports
    
    * typo
    
    * subdomain bucket creation with trailing '/' did not work
    
    * Use regex for Host field check to determine IPv4/IPv6
    
    * add testcases for trailing slash, IPv4 and IPv6

--- tests/test_s3bucket_path/test_bucket_path_server.py
+++ tests/test_s3bucket_path/test_bucket_path_server.py
@@ -31,6 +31,16 @@ def test_s3_server_bucket_create():
     res.status_code.should.equal(200)
     res.data.should.contain(b"ListBucketResult")
 
+    res = test_client.put('/foobar2/', 'http://localhost:5000')
+    res.status_code.should.equal(200)
+
+    res = test_client.get('/')
+    res.data.should.contain(b'<Name>foobar2</Name>')
+
+    res = test_client.get('/foobar2/', 'http://localhost:5000')
+    res.status_code.should.equal(200)
+    res.data.should.contain(b"ListBucketResult")
+
     res = test_client.get('/missing-bucket', 'http://localhost:5000')
     res.status_code.should.equal(404)
 
@@ -55,5 +65,39 @@ def test_s3_server_post_to_bucket():
     })
 
     res = test_client.get('/foobar2/the-key', 'http://localhost:5000/')
+    res.status_code.should.equal(200)
+    res.data.should.equal(b"nothing")
+
+
+def test_s3_server_put_ipv6():
+    backend = server.create_backend_app("s3bucket_path")
+    test_client = backend.test_client()
+
+    res = test_client.put('/foobar2', 'http://[::]:5000/')
+    res.status_code.should.equal(200)
+
+    test_client.post('/foobar2', "https://[::]:5000/", data={
+        'key': 'the-key',
+        'file': 'nothing'
+    })
+
+    res = test_client.get('/foobar2/the-key', 'http://[::]:5000/')
+    res.status_code.should.equal(200)
+    res.data.should.equal(b"nothing")
+
+
+def test_s3_server_put_ipv4():
+    backend = server.create_backend_app("s3bucket_path")
+    test_client = backend.test_client()
+
+    res = test_client.put('/foobar2', 'http://127.0.0.1:5000/')
+    res.status_code.should.equal(200)
+
+    test_client.post('/foobar2', "https://127.0.0.1:5000/", data={
+        'key': 'the-key',
+        'file': 'nothing'
+    })
+
+    res = test_client.get('/foobar2/the-key', 'http://127.0.0.1:5000/')
     res.status_code.should.equal(200)
     res.data.should.equal(b"nothing")
