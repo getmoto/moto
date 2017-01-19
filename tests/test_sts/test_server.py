f68b2963db4ac7b8f0943d8b9ea101fdcbd451a5
Jesse Szwedko <jesse.szwedko@gmail.com>
2017-01-18 19:59:04 -0800

    sts: Implement get_caller_identity (#806)
    Return a canned response
    
    Signed-off-by: Andrew Harris <andrew.harris@getbraintree.com>

--- tests/test_sts/test_server.py
+++ tests/test_sts/test_server.py
@@ -26,3 +26,14 @@ def test_sts_get_federation_token():
     res.status_code.should.equal(200)
     res.data.should.contain(b"SessionToken")
     res.data.should.contain(b"AccessKeyId")
+
+
+def test_sts_get_caller_identity():
+    backend = server.create_backend_app("sts")
+    test_client = backend.test_client()
+
+    res = test_client.get('/?Action=GetCallerIdentity')
+    res.status_code.should.equal(200)
+    res.data.should.contain(b"Arn")
+    res.data.should.contain(b"UserId")
+    res.data.should.contain(b"Account")
