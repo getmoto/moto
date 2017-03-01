d530bcf4a7a2fdce06474b8bec452e07d1f25aed
Steve Pulec <spulec@gmail.com>
2017-02-28 21:29:28 -0500

    remove py26.

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
     test_suite="tests",
     classifiers=[
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
