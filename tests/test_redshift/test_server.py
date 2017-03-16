6666351757c2c2083a88158a132f446112109b9d
Steve Pulec <spulec@gmail.com>
2017-03-15 22:05:11 -0400

    Fix redshift server to default to xml.

--- tests/test_redshift/test_server.py
+++ tests/test_redshift/test_server.py
@@ -18,7 +18,5 @@ def test_describe_clusters():
 
     res = test_client.get('/?Action=DescribeClusters')
 
-    json_data = json.loads(res.data.decode("utf-8"))
-    clusters = json_data['DescribeClustersResponse'][
-        'DescribeClustersResult']['Clusters']
-    list(clusters).should.equal([])
+    result = res.data.decode("utf-8")
+    result.should.contain("<DescribeClustersResponse><DescribeClustersResult><Clusters></Clusters></DescribeClustersResult")
