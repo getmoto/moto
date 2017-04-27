c5c2b34322d6a5fe1c2f14c25f2b288afed611fe
Andrii Piasetskyi <apiasetskyi@dataxu.com>
2017-04-27 10:19:25 +0300

    Incremented package version for setuptools

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extras_require = {
 
 setup(
     name='moto',
-    version='0.4.31',
+    version='0.4.32alpha',
     description='A library that allows your python tests to easily'
                 ' mock out the boto library',
     author='Steve Pulec',
