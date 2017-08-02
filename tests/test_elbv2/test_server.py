2f05f6c9eaec6eeae5b6d47c5ede92a7c70f8723
Jack Danger <github@jackcanty.com>
2017-08-02 13:29:14 -0700

    Adding version string to server tests

--- tests/test_elbv2/test_server.py
+++ tests/test_elbv2/test_server.py
@@ -12,6 +12,6 @@ def test_elbv2_describe_load_balancers():
     backend = server.create_backend_app("elbv2")
     test_client = backend.test_client()
 
-    res = test_client.get('/?Action=DescribeLoadBalancers')
+    res = test_client.get('/?Action=DescribeLoadBalancers&Version=2015-12-01')
 
     res.data.should.contain(b'DescribeLoadBalancersResponse')
