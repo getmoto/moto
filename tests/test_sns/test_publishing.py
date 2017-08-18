38cba371bbeb453ab65bf8a934ff000ae1b2a9f9
Toshiya Kawasaki <toshiya@abeja.asia>
2017-08-18 15:45:25 +0900

    add test for publishing sns message to sqs

--- tests/test_sns/test_publishing.py
+++ tests/test_sns/test_publishing.py
@@ -9,6 +9,9 @@ from moto.packages.responses import responses
 from moto import mock_sns_deprecated, mock_sqs_deprecated
 
 
+MESSAGE_FROM_SQS_TEMPLATE = '{\n  "Message": "%s",\n  "MessageId": "%s",\n  "Signature": "EXAMPLElDMXvB8r9R83tGoNn0ecwd5UjllzsvSvbItzfaMpN2nk5HVSw7XnOn/49IkxDKz8YrlH2qJXj2iZB0Zo2O71c4qQk1fMUDi3LGpij7RCW7AW9vYYsSqIKRnFS94ilu7NFhUzLiieYr4BKHpdTmdD6c0esKEYBpabxDSc=",\n  "SignatureVersion": "1",\n  "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-f3ecfb7224c7233fe7bb5f59f96de52f.pem",\n  "Subject": "my subject",\n  "Timestamp": "2015-01-01T12:00:00.000Z",\n  "TopicArn": "arn:aws:sns:%s:123456789012:some-topic",\n  "Type": "Notification",\n  "UnsubscribeURL": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:123456789012:some-topic:2bcfbf39-05c3-41de-beaa-fcfcc21c8f55"\n}'
+
+
 @mock_sqs_deprecated
 @mock_sns_deprecated
 def test_publish_to_sqs():
@@ -24,11 +27,15 @@ def test_publish_to_sqs():
     conn.subscribe(topic_arn, "sqs",
                    "arn:aws:sqs:us-east-1:123456789012:test-queue")
 
-    conn.publish(topic=topic_arn, message="my message")
+    message_to_publish = 'my message'
+    with freeze_time("2015-01-01 12:00:00"):
+        published_message = conn.publish(topic=topic_arn, message=message_to_publish)
+    published_message_id = published_message['PublishResponse']['PublishResult']['MessageId']
 
     queue = sqs_conn.get_queue("test-queue")
     message = queue.read(1)
-    message.get_body().should.equal('my message')
+    expected = MESSAGE_FROM_SQS_TEMPLATE  % (message_to_publish, published_message_id, 'us-east-1')
+    message.get_body().should.equal(expected)
 
 
 @mock_sqs_deprecated
@@ -46,8 +53,12 @@ def test_publish_to_sqs_in_different_region():
     conn.subscribe(topic_arn, "sqs",
                    "arn:aws:sqs:us-west-2:123456789012:test-queue")
 
-    conn.publish(topic=topic_arn, message="my message")
+    message_to_publish = 'my message'
+    with freeze_time("2015-01-01 12:00:00"):
+        published_message = conn.publish(topic=topic_arn, message=message_to_publish)
+    published_message_id = published_message['PublishResponse']['PublishResult']['MessageId']
 
     queue = sqs_conn.get_queue("test-queue")
     message = queue.read(1)
-    message.get_body().should.equal('my message')
+    expected = MESSAGE_FROM_SQS_TEMPLATE  % (message_to_publish, published_message_id, 'us-west-1')
+    message.get_body().should.equal(expected)
--- tests/test_sns/test_publishing_boto3.py
+++ tests/test_sns/test_publishing_boto3.py
@@ -10,6 +10,10 @@ import sure  # noqa
 
 from moto.packages.responses import responses
 from moto import mock_sns, mock_sqs
+from freezegun import freeze_time
+
+
+MESSAGE_FROM_SQS_TEMPLATE = '{\n  "Message": "%s",\n  "MessageId": "%s",\n  "Signature": "EXAMPLElDMXvB8r9R83tGoNn0ecwd5UjllzsvSvbItzfaMpN2nk5HVSw7XnOn/49IkxDKz8YrlH2qJXj2iZB0Zo2O71c4qQk1fMUDi3LGpij7RCW7AW9vYYsSqIKRnFS94ilu7NFhUzLiieYr4BKHpdTmdD6c0esKEYBpabxDSc=",\n  "SignatureVersion": "1",\n  "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-f3ecfb7224c7233fe7bb5f59f96de52f.pem",\n  "Subject": "my subject",\n  "Timestamp": "2015-01-01T12:00:00.000Z",\n  "TopicArn": "arn:aws:sns:%s:123456789012:some-topic",\n  "Type": "Notification",\n  "UnsubscribeURL": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:123456789012:some-topic:2bcfbf39-05c3-41de-beaa-fcfcc21c8f55"\n}'
 
 
 @mock_sqs
@@ -26,12 +30,15 @@ def test_publish_to_sqs():
     conn.subscribe(TopicArn=topic_arn,
                    Protocol="sqs",
                    Endpoint="arn:aws:sqs:us-east-1:123456789012:test-queue")
-
-    conn.publish(TopicArn=topic_arn, Message="my message")
+    message = 'my message'
+    with freeze_time("2015-01-01 12:00:00"):
+        published_message = conn.publish(TopicArn=topic_arn, Message=message)
+    published_message_id = published_message['MessageId']
 
     queue = sqs_conn.get_queue_by_name(QueueName="test-queue")
     messages = queue.receive_messages(MaxNumberOfMessages=1)
-    messages[0].body.should.equal('my message')
+    expected = MESSAGE_FROM_SQS_TEMPLATE  % (message, published_message_id, 'us-east-1')
+    messages[0].body.should.equal(expected)
 
 
 @mock_sqs
@@ -58,10 +65,15 @@ def test_publish_to_sqs_dump_json():
             }
         }]
     }, sort_keys=True)
-    conn.publish(TopicArn=topic_arn, Message=message)
+    with freeze_time("2015-01-01 12:00:00"):
+        published_message = conn.publish(TopicArn=topic_arn, Message=message)
+    published_message_id = published_message['MessageId']
+
     queue = sqs_conn.get_queue_by_name(QueueName="test-queue")
     messages = queue.receive_messages(MaxNumberOfMessages=1)
-    expected = u'{\\"Records\\": [{\\"eventSource\\": \\"aws:s3\\", \\"eventVersion\\": \\"2.0\\", \\"s3\\": {\\"s3SchemaVersion\\": \\"1.0\\"}}]}'
+
+    escaped = message.replace('"', '\\"')
+    expected = MESSAGE_FROM_SQS_TEMPLATE  % (escaped, published_message_id, 'us-east-1')
     messages[0].body.should.equal(expected)
 
 
@@ -80,11 +92,15 @@ def test_publish_to_sqs_in_different_region():
                    Protocol="sqs",
                    Endpoint="arn:aws:sqs:us-west-2:123456789012:test-queue")
 
-    conn.publish(TopicArn=topic_arn, Message="my message")
+    message = 'my message'
+    with freeze_time("2015-01-01 12:00:00"):
+        published_message = conn.publish(TopicArn=topic_arn, Message=message)
+    published_message_id = published_message['MessageId']
 
     queue = sqs_conn.get_queue_by_name(QueueName="test-queue")
     messages = queue.receive_messages(MaxNumberOfMessages=1)
-    messages[0].body.should.equal('my message')
+    expected = MESSAGE_FROM_SQS_TEMPLATE  % (message, published_message_id, 'us-west-1')
+    messages[0].body.should.equal(expected)
 
 
 @freeze_time("2013-01-01")
