b088811d0686011f80f8fdb10a69d5364436ff64
Jack Danger <github@jackcanty.com>
2017-09-07 11:31:42 -0700

    bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
 
 setup(
     name='moto',
-    version='1.1.1',
+    version='1.1.2',
     description='A library that allows your python tests to easily'
                 ' mock out the boto library',
     author='Steve Pulec',
