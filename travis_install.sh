7b113ca7849bf9051e1a21678f52cb421d8891f1
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-09-14 16:19:01 -0700

    another attempt

--- travis_install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-travis_retry pip install boto==2.45.0
-travis_retry pip install .
--- travis_moto_server.sh
+++ travis_moto_server.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
-cd /moto
 
 travis_retry() {
     "$@"
 }
-source /moto/travis_install.sh
+
+pip install /moto/dist/moto*.gz
 pip install flask
 moto_server -H 0.0.0.0 -p 5000
