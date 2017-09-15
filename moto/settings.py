641a93176d12481c9f168485d7e81159f40767a7
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-09-14 17:47:28 -0700

    refactor fix

--- moto/settings.py
+++ moto/settings.py
@@ -1,5 +1,5 @@
 import os
 
 TEST_SERVER_MODE = os.environ.get('TEST_SERVER_MODE', '0').lower() == 'true'
-SERVER_HOSTNAME = 'moto_server' if settings.TEST_SERVER_MODE else 'localhost'
+SERVER_HOSTNAME = 'moto_server' if TEST_SERVER_MODE else 'localhost'
 SERVER_BASE_URL = 'http://{}:5000'.format(SERVER_HOSTNAME)
