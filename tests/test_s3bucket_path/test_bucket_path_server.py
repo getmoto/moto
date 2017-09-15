7b0652ef1eb051dcc103d4c886340f20fe7084a6
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-09-15 14:50:14 -0700

    fix protocol

--- tests/test_s3bucket_path/test_bucket_path_server.py
+++ tests/test_s3bucket_path/test_bucket_path_server.py
@@ -65,7 +65,7 @@ def test_s3_server_post_to_bucket():
         'file': 'nothing'
     })
 
-    res = test_client.get('/foobar2/the-key', 'https://localhost:5000/')
+    res = test_client.get('/foobar2/the-key', 'http://localhost:5000/')
     res.status_code.should.equal(200)
     res.data.should.equal(b"nothing")
 
