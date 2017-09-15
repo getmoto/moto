6e735b0c683dd6de9f8e6ad2e744d46b2b6f8194
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-09-14 19:18:59 -0700

    wait for service to come up

---
+++ wait_for.py
@@ -0,0 +1,20 @@
+import time
+
+try:
+    import urllib2 as urllib
+    from urllib2 import URLError as ConnError
+except ImportError:
+    import urllib.request as urllib
+    from urllib.error import URLError as ConnError
+
+
+start_ts = time.time()
+print("Waiting for service to come up")
+while True:
+    try:
+        urllib.urlopen('http://localhost:5000/')
+    except ConnError:
+        elapsed_s = time.time() - start_ts
+        if elapsed_s > 30:
+            raise
+        print('.')
