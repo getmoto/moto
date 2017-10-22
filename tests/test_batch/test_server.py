fbc984933bce2caeb3855d8f0a747b6a447ac0da
Terry Cain <terry@terrys-home.co.uk>
2017-10-22 21:36:39 +0100

    Added server test

--- tests/test_batch/test_server.py
+++ tests/test_batch/test_server.py
@@ -9,8 +9,11 @@ from moto import mock_batch
 Test the different server responses
 '''
 
+
 @mock_batch
 def test_batch_list():
     backend = server.create_backend_app("batch")
     test_client = backend.test_client()
-    # do test
+
+    res = test_client.get('/v1/describecomputeenvironments')
+    res.status_code.should.equal(200)
