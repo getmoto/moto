5561a69ec06ba33e524d79afd517013831fccefb
Toshiya Kawasaki <toshiya@abeja.asia>
2017-10-23 15:41:59 +0900

    remove unused import

--- moto/backends.py
+++ moto/backends.py
@@ -35,7 +35,6 @@ from moto.sqs import sqs_backends
 from moto.ssm import ssm_backends
 from moto.sts import sts_backends
 from moto.xray import xray_backends
-from moto.iot import iot_backends
 
 BACKENDS = {
     'acm': acm_backends,
@@ -74,7 +73,6 @@ BACKENDS = {
     'route53': route53_backends,
     'lambda': lambda_backends,
     'xray': xray_backends,
-    'iot': iot_backends,
 }
 
 
