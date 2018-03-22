136d491502c46636b71cdaae77bac4719d68f20e
Jack Danger <github@jackcanty.com>
2018-03-22 10:00:35 -0700

    bumping to version 1.3.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ else:
 
 setup(
     name='moto',
-    version='1.3.0',
+    version='1.3.1',
     description='A library that allows your python tests to easily'
                 ' mock out the boto library',
     author='Steve Pulec',
