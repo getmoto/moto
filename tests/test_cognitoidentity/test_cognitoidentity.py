2455de8282a87ea2b08cf2799e6db8968a39c24e
Barry Ruffner <barry_ruffner@intuit.com>
2018-04-03 14:08:20 -0700

    Added a print.

--- tests/test_cognitoidentity/test_cognitoidentity.py
+++ tests/test_cognitoidentity/test_cognitoidentity.py
@@ -1,26 +1,20 @@
 from __future__ import unicode_literals
 
-import base64
-import botocore.client
 import boto3
-import hashlib
-import io
-import json
-import zipfile
+
+from moto import mock_cognitoidentity
 import sure  # noqa
 
-from freezegun import freeze_time
-from moto import mock_cognitoidentity, settings
 
 @mock_cognitoidentity
 def test_create_identity_pool():
     conn = boto3.client('cognito-identity', 'us-west-2')
 
     result = conn.create_identity_pool(IdentityPoolName='TestPool',
-        AllowUnauthenticatedIdentities=False,          
-        SupportedLoginProviders={'graph.facebook.com':'123456789012345'},
+        AllowUnauthenticatedIdentities=False,
+        SupportedLoginProviders={'graph.facebook.com': '123456789012345'},
         DeveloperProviderName='devname',
-        OpenIdConnectProviderARNs=['arn:aws:rds:eu-west-2:123456789012:db:mysql-db',],
+        OpenIdConnectProviderARNs=['arn:aws:rds:eu-west-2:123456789012:db:mysql-db'],
         CognitoIdentityProviders=[
             {
                 'ProviderName': 'testprovider',
@@ -28,8 +22,9 @@ def test_create_identity_pool():
                 'ServerSideTokenCheck': True
             },
         ],
-        SamlProviderARNs=['arn:aws:rds:eu-west-2:123456789012:db:mysql-db',])
+        SamlProviderARNs=['arn:aws:rds:eu-west-2:123456789012:db:mysql-db'])
     assert result['IdentityPoolId'] != ''
+
 
 @mock_cognitoidentity
 def test_get_id():
@@ -37,15 +32,18 @@ def test_get_id():
     result = conn.get_id(AccountId='someaccount',
         IdentityPoolId='us-west-2:12345',
         Logins={
-                'someurl': '12345'
+            'someurl': '12345'
         })
+    print(result)
     assert result['IdentityId'].startswith('us-west-2')
+
 
 @mock_cognitoidentity
 def test_get_credentials_for_identity():
     conn = boto3.client('cognito-identity', 'us-west-2')
     result = conn.get_credentials_for_identity(IdentityId='12345')
     assert result['IdentityId'] == '12345'
+
 
 @mock_cognitoidentity
 def test_get_open_id_token_for_developer_identity():
