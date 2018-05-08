134251d5b9b82226bc602c16f294d38e0f6638c8
Jordan Zucker <jordan.zucker@gmail.com>
2018-05-08 12:43:50 -0700

    Stubbed out tests for dms

---
+++ tests/test_dms/test_server.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+import sure  # noqa
+
+import moto.server as server
+
+'''
+Test the different server responses
+'''
+
+
+# def test_describe_jobflows():
+#     backend = server.create_backend_app("dms")
+#     test_client = backend.test_client()
+#
+#     res = test_client.get('/?Action=DescribeJobFlows')
+#
+#     res.data.should.contain(b'<DescribeJobFlowsResult>')
+#     res.data.should.contain(b'<JobFlows>')
