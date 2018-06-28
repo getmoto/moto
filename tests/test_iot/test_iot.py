10f96b2ccfa55114c9f2b32355efc4decd0ebb0f
Stephan Huber <stephan.huber@solutions.endress.com>
2018-06-28 12:59:08 +0200

    next_token (pagination) added for `list_thing_types`

--- tests/test_iot/test_iot.py
+++ tests/test_iot/test_iot.py
@@ -82,7 +82,30 @@ def test_list_thing_types():
     thing_types.should_not.have.key('nextToken')
     thing_types['thingTypes'][0]['thingTypeName'].should.equal('51')
     thing_types['thingTypes'][-1]['thingTypeName'].should.equal('100')
-    # TODO test list_thing_types with filters
+
+
+@mock_iot
+def test_list_thing_types_with_typename_filter():
+    client = boto3.client('iot', region_name='ap-northeast-1')
+
+    client.create_thing_type(thingTypeName='thing')
+    client.create_thing_type(thingTypeName='thingType')
+    client.create_thing_type(thingTypeName='thingTypeName')
+    client.create_thing_type(thingTypeName='thingTypeNameGroup')
+    client.create_thing_type(thingTypeName='shouldNotFind')
+    client.create_thing_type(thingTypeName='find me it shall not')
+
+    thing_types = client.list_thing_types(thingTypeName='thing')
+    thing_types.should_not.have.key('nextToken')
+    thing_types.should.have.key('thingTypes').which.should.have.length_of(4)
+    thing_types['thingTypes'][0]['thingTypeName'].should.equal('thing')
+    thing_types['thingTypes'][-1]['thingTypeName'].should.equal('thingTypeNameGroup')
+
+    thing_types = client.list_thing_types(thingTypeName='thingTypeName')
+    thing_types.should_not.have.key('nextToken')
+    thing_types.should.have.key('thingTypes').which.should.have.length_of(2)
+    thing_types['thingTypes'][0]['thingTypeName'].should.equal('thingTypeName')
+    thing_types['thingTypes'][-1]['thingTypeName'].should.equal('thingTypeNameGroup')
 
 
 @mock_iot
