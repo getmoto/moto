d0f7768e61130251616daff41093b2beed01f094
Alec Rajeev <rajeeva2012@gmail.com>
2018-08-11 18:47:01 -0400

    fixed unit test to use right parameter

--- tests/test_ec2/test_reserved_instances.py
+++ tests/test_ec2/test_reserved_instances.py
@@ -496,7 +496,7 @@ def test_reserved_instance_offering_id_valid_region():
 def test_purchase_reserved_instances():
     client = boto3.client("ec2", region_name="eu-central-1")
 
-    reserved_instance = client.purchase_reserved_instances_offering(ReservedInstanceOfferingId="3818be01-41a1-4ed2-8f2c-75cbd0abf7cc", InstanceCount=1)
+    reserved_instance = client.purchase_reserved_instances_offering(ReservedInstancesOfferingId="3818be01-41a1-4ed2-8f2c-75cbd0abf7cc", InstanceCount=1)
 
     len(reserved_instance["ReservedInstancesId"]).should.equal(36)
 
@@ -506,7 +506,7 @@ def test_purchase_invalid_reserved_instance_id():
     client = boto3.client("ec2", region_name="eu-central-1")
     
     with assert_raises(ClientError) as err:
-        client.purchase_reserved_instances_offering(ReservedInstanceOfferingId="9818be01-41a1-4ed2-8f2c-75cbd0abf7cc", InstanceCount=1)
+        client.purchase_reserved_instances_offering(ReservedInstancesOfferingId="9818be01-41a1-4ed2-8f2c-75cbd0abf7cc", InstanceCount=1)
     
     e = err.exception
     e.response["Error"]["Code"].should.equal("InvalidReservedInstancesOfferingId")
@@ -517,7 +517,7 @@ def test_purchase_invalid_reserved_instance_in_wrong_region():
     client = boto3.client("ec2", region_name="us-east-1")
     
     with assert_raises(ClientError) as err:
-        client.purchase_reserved_instances_offering(ReservedInstanceOfferingId="3818be01-41a1-4ed2-8f2c-75cbd0abf7cc", InstanceCount=1)
+        client.purchase_reserved_instances_offering(ReservedInstancesOfferingId="3818be01-41a1-4ed2-8f2c-75cbd0abf7cc", InstanceCount=1)
     
     e = err.exception
     e.response["Error"]["Code"].should.equal("InvalidReservedInstancesOfferingId")
