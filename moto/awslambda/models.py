a6d9cadac3361c8822344011e114a4aa54c4f309
Terry Cain <terrycain@users.noreply.github.com>
2019-05-22 22:01:28 +0100

    Change docker library version check (#2214)

--- moto/awslambda/models.py
+++ moto/awslambda/models.py
@@ -45,7 +45,7 @@ except ImportError:
 
 _stderr_regex = re.compile(r'START|END|REPORT RequestId: .*')
 _orig_adapter_send = requests.adapters.HTTPAdapter.send
-docker_3 = docker.__version__.startswith("3")
+docker_3 = docker.__version__[0] >= '3'
 
 
 def zip2tar(zip_bytes):
