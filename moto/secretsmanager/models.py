8e9bbcbc99c04a45a82583bea1288dd15cea4a6b
Sam Erika Clotfelter <sam.clotfelter@teamcmp.com>
2019-06-11 16:14:51 -0400

    Added VersionIdsToStages

--- moto/secretsmanager/models.py
+++ moto/secretsmanager/models.py
@@ -173,6 +173,10 @@ class SecretsManagerBackend(BaseBackend):
 
         secret = self.secrets[secret_id]
 
+        version_ids_to_stages = {}
+        for version_id, version in secret['versions'].items():
+            version_ids_to_stages[version_id] = version['version_stages']
+
         response = json.dumps({
             "ARN": secret_arn(self.region, secret['secret_id']),
             "Name": secret['name'],
@@ -187,7 +191,8 @@ class SecretsManagerBackend(BaseBackend):
             "LastChangedDate": None,
             "LastAccessedDate": None,
             "DeletedDate": secret.get('deleted_date', None),
-            "Tags": secret['tags']
+            "Tags": secret['tags'],
+            "VersionIdsToStages": version_ids_to_stages,
         })
 
         return response
