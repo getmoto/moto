af0205b6a33ab00f6b2ad2833cf9cc0998386407
Steve Pulec <spulec@gmail.com>
2019-07-06 17:40:36 -0500

    Fix wrong tag for cloudwatch metrics response. Closes #2267.

--- moto/cloudwatch/responses.py
+++ moto/cloudwatch/responses.py
@@ -275,7 +275,7 @@ GET_METRIC_STATISTICS_TEMPLATE = """<GetMetricStatisticsResponse xmlns="http://m
       <Label>{{ label }}</Label>
       <Datapoints>
         {% for datapoint in datapoints %}
-            <Datapoint>
+            <member>
               {% if datapoint.sum is not none %}
               <Sum>{{ datapoint.sum }}</Sum>
               {% endif %}
@@ -302,7 +302,7 @@ GET_METRIC_STATISTICS_TEMPLATE = """<GetMetricStatisticsResponse xmlns="http://m
 
               <Timestamp>{{ datapoint.timestamp }}</Timestamp>
               <Unit>{{ datapoint.unit }}</Unit>
-            </Datapoint>
+            </member>
         {% endfor %}
       </Datapoints>
     </GetMetricStatisticsResult>
