1ae641fab893aa05375b29dd7829477c981a0500
Wessel van der Veen <w.vanderveen@thesio.eu>
2019-08-31 09:08:12 +0200

    adds basic implementation for describe-identity-pool

---
+++ moto/cognitoidentity/exceptions.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+import json
+
+from werkzeug.exceptions import BadRequest
+
+
+class ResourceNotFoundError(BadRequest):
+
+    def __init__(self, message):
+        super(ResourceNotFoundError, self).__init__()
+        self.description = json.dumps({
+            "message": message,
+            '__type': 'ResourceNotFoundException',
+        })
