e2af07df446850483fd9f3c1edd0a7340d3510a8
Stijn Seghers <stijnseghers@gmail.com>
2020-01-16 17:30:50 +1300

    Keep pkg_resources import function-local (~0.06s)

--- moto/core/responses.py
+++ moto/core/responses.py
@@ -20,7 +20,6 @@ import six
 from six.moves.urllib.parse import parse_qs, urlparse
 
 import xmltodict
-from pkg_resources import resource_filename
 from werkzeug.exceptions import HTTPException
 
 import boto3
@@ -766,6 +765,9 @@ class AWSServiceSpec(object):
     """
 
     def __init__(self, path):
+        # Importing pkg_resources takes ~60ms; keep it local
+        from pkg_resources import resource_filename  # noqa
+
         self.path = resource_filename("botocore", path)
         with io.open(self.path, "r", encoding="utf-8") as f:
             spec = json.load(f)
