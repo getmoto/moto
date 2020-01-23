e3906043d7a77ccb3c656088bff5e8fce2386ec0
Andrey Kislyuk <kislyuk@gmail.com>
2020-01-22 16:58:25 -0800

    Fix linter error

--- moto/awslambda/models.py
+++ moto/awslambda/models.py
@@ -394,6 +394,7 @@ class LambdaFunction(BaseModel):
             env_vars.update(self.environment_vars)
 
             container = output = exit_code = None
+            log_config = docker.types.LogConfig(type=docker.types.LogConfig.types.JSON)
             with _DockerDataVolumeContext(self) as data_vol:
                 try:
                     run_kwargs = (
@@ -409,7 +410,7 @@ class LambdaFunction(BaseModel):
                         volumes=["{}:/var/task".format(data_vol.name)],
                         environment=env_vars,
                         detach=True,
-                        log_config=docker.types.LogConfig(type=docker.types.LogConfig.types.JSON),
+                        log_config=log_config,
                         **run_kwargs
                     )
                 finally:
