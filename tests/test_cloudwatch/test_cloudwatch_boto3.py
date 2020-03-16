9d3ee116d3ec53c6ee20d2df0823ac09694e0f37
Tim Gatzemeier <tim.gatzemeier@gmx.de>
2020-03-16 20:14:41 +0100

    add test case for actions_enabled field

--- tests/test_cloudwatch/test_cloudwatch_boto3.py
+++ tests/test_cloudwatch/test_cloudwatch_boto3.py
@@ -128,11 +128,13 @@ def test_alarm_state():
     len(resp["MetricAlarms"]).should.equal(1)
     resp["MetricAlarms"][0]["AlarmName"].should.equal("testalarm1")
     resp["MetricAlarms"][0]["StateValue"].should.equal("ALARM")
+    resp["MetricAlarms"][0]["ActionsEnabled"].should.equal("True")
 
     resp = client.describe_alarms(StateValue="OK")
     len(resp["MetricAlarms"]).should.equal(1)
     resp["MetricAlarms"][0]["AlarmName"].should.equal("testalarm2")
     resp["MetricAlarms"][0]["StateValue"].should.equal("OK")
+    resp["MetricAlarms"][0]["ActionsEnabled"].should.equal("True")
 
     # Just for sanity
     resp = client.describe_alarms()
