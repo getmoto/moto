7ea419dd54f475660f4e927ad39d62d226a43513
pvbouwel <petervanbouwel@gmail.com>
2020-04-11 11:07:22 +0100

    Better DDB expressions support1: TokenizationDDB
    Currently the mock for DynamoDB has adhoc code to implement
    its updateExpression functionality.  This series will
    transform the logic such that Update Expressions are processed
    as follows:
     1) Expression gets parsed into a tokenlist (tokenized) -> This commit
     2) Tokenlist get transformed to expression tree (AST)
     3) The AST gets validated (full semantic correctness)
     4) AST gets processed to perform the update
    
    This alows for a more realistic mocking. It will throw exceptions much
    more aggressively avoiding situations where a test passes against the
    mock but fails with an exception when running against AWS.
    
    Introduction of step 3 also allows to have the update expression as an
    atomic unit of work. So updates at the start of the expression cannot
    be performed if there is an error further down the expression.
    
    This specific commit will tokenize expressions but the tokenlist is not
    yet used. It is purely to keep clear boundaries.  It does do a minor
    refactoring of the exceptions to allow more re-use and to ease testing.
    
    This series of changes is to aid providing a long-term solution for
    https://github.com/spulec/moto/issues/2806.

--- moto/dynamodb2/exceptions.py
+++ moto/dynamodb2/exceptions.py
@@ -2,9 +2,59 @@ class InvalidIndexNameError(ValueError):
     pass
 
 
-class InvalidUpdateExpression(ValueError):
-    pass
+class MockValidationException(ValueError):
+    def __init__(self, message):
+        self.exception_msg = message
 
 
-class ItemSizeTooLarge(Exception):
-    message = "Item size has exceeded the maximum allowed size"
+class InvalidUpdateExpression(MockValidationException):
+    invalid_update_expression_msg = (
+        "The document path provided in the update expression is invalid for update"
+    )
+
+    def __init__(self):
+        super(InvalidUpdateExpression, self).__init__(
+            self.invalid_update_expression_msg
+        )
+
+
+class UpdateExprSyntaxError(MockValidationException):
+    update_expr_syntax_error_msg = (
+        "Invalid UpdateExpression: Syntax error; {error_detail}"
+    )
+
+    def __init__(self, error_detail):
+        self.error_detail = error_detail
+        super(UpdateExprSyntaxError, self).__init__(
+            self.update_expr_syntax_error_msg.format(error_detail=error_detail)
+        )
+
+
+class InvalidTokenException(UpdateExprSyntaxError):
+    token_detail_msg = 'token: "{token}", near: "{near}"'
+
+    def __init__(self, token, near):
+        self.token = token
+        self.near = near
+        super(InvalidTokenException, self).__init__(
+            self.token_detail_msg.format(token=token, near=near)
+        )
+
+
+class InvalidExpressionAttributeNameKey(MockValidationException):
+    invalid_expr_attr_name_msg = (
+        'ExpressionAttributeNames contains invalid key: Syntax error; key: "{key}"'
+    )
+
+    def __init__(self, key):
+        self.key = key
+        super(InvalidExpressionAttributeNameKey, self).__init__(
+            self.invalid_expr_attr_name_msg.format(key=key)
+        )
+
+
+class ItemSizeTooLarge(MockValidationException):
+    item_size_too_large_msg = "Item size has exceeded the maximum allowed size"
+
+    def __init__(self):
+        super(ItemSizeTooLarge, self).__init__(self.item_size_too_large_msg)
