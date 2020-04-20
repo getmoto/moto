ad0805de0ec3546767f5c13141e3d072b8c6f496
Michael Käufl <michael-k@users.noreply.github.com>
2020-04-20 09:19:24 +0200

    Add Python 3.8 to trove classifiers

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Testing",
     ],
