889b8cc3cf33bf2973ed3e76314d117b868fe69a
usmankb <usman@krazybee.com>
2020-05-12 02:07:16 +0530

    added assertions

--- tests/test_s3/test_s3.py
+++ tests/test_s3/test_s3.py
@@ -2149,16 +2149,21 @@ def test_boto3_copy_object_with_versioning():
     data.should.equal(b"test2")
 
 
-@mock_s3_deprecated
+@mock_s3
 def test_s3_abort_multipart_data_with_invalid_upload_and_key():
-    conn = boto.connect_s3("the_key", "the_secret")
-    bucket = conn.create_bucket("foobar")
+    client = boto3.client("s3", region_name=DEFAULT_REGION_NAME)
 
-    with assert_raises(S3ResponseError) as ce:
-        bucket.cancel_multipart_upload(
-            key_name="foobar",
-            upload_id="dummy_upload_id"
+    client.create_bucket(
+        Bucket="blah", CreateBucketConfiguration={"LocationConstraint": "eu-west-1"}
+    )
+
+    with assert_raises(Exception) as err:
+        client.abort_multipart_upload(
+            Bucket="blah",
+            Key="foobar",
+            UploadId="dummy_upload_id"
         )
+    err.exception.response["Error"]["Code"].should.equal("NoSuchUpload")
 
 
 @mock_s3
