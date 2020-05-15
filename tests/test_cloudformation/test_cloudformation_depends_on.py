b38a36864f483f3c775f3e479b88180a3a314fb7
zbrookle <zachb1996@yahoo.com>
2020-05-15 18:20:40 -0400

    BUG: Remove subnet creation

--- tests/test_cloudformation/test_cloudformation_depends_on.py
+++ tests/test_cloudformation/test_cloudformation_depends_on.py
@@ -2,15 +2,6 @@ import boto3
 from moto import mock_cloudformation, mock_ec2
 import json
 
-@mock_ec2
-def make_subnet():
-    ec2 = boto3.resource("ec2")
-    default_vpc = list(ec2.vpcs.all())[0]
-    ec2.create_subnet(
-        VpcId=default_vpc.id, CidrBlock="172.31.48.0/20", AvailabilityZone="us-east-1a"
-    )
-
-
 depends_on_template = {
     "AWSTemplateFormatVersion": "2010-09-09",
     "Resources": {
@@ -43,7 +34,9 @@ depends_on_template_json = json.dumps(depends_on_template)
 @mock_cloudformation
 @mock_ec2
 def test_create_stack_with_depends_on():
-    make_subnet()
-    boto3.client("cloudformation").create_stack(
+    boto3.client("cloudformation", region_name="us-east-1").create_stack(
         StackName="depends_on_test", TemplateBody=depends_on_template_json
     )
+
+if __name__ == '__main__':
+    test_create_stack_with_depends_on()
