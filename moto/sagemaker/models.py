bede70fe766fa98f6f1cba7553896df106b925da
Joseph Weitekamp <jweite@amazon.com>
2020-06-29 14:24:04 -0400

    Sagemaker Notebook Support, take-1: create, describe, start, stop, delete.

--- moto/sagemaker/models.py
+++ moto/sagemaker/models.py
@@ -1,14 +1,109 @@
+import datetime
 from moto.core import BaseBackend
 from moto.ec2 import ec2_backends
 from moto.sts.models import ACCOUNT_ID
+from moto.core.exceptions import RESTError
+import moto.sagemaker.validators as validators
+
 
 class FakeSagemakerNotebookInstance:
-    def __init__(self, name, instance_type, execution_role, region_name):
-        self.name = name
-        self.instance_type = instance_type
-        self.execution_role = execution_role
+    def __init__(
+        self,
+        region_name,
+        notebook_instance_name,
+        instance_type,
+        role_arn,
+        subnet_id,
+        security_group_ids,
+        kms_key_id,
+        tags,
+        lifecycle_config_name,
+        direct_internet_access,
+        volume_size_in_gb,
+        accelerator_types,
+        default_code_repository,
+        additional_code_repositories,
+        root_access,
+    ):
+        self.validate_volume_size_in_gb(volume_size_in_gb)
+        self.validate_instance_type(instance_type)
+
         self.region_name = region_name
-        # ToDo: Validate these params.  Raise if bad.
+        self.notebook_instance_name = notebook_instance_name
+        self.instance_type = instance_type
+        self.role_arn = role_arn
+        self.subnet_id = subnet_id
+        self.security_group_ids = security_group_ids
+        self.kms_key_id = kms_key_id
+        self.tags = tags
+        self.lifecycle_config_name = lifecycle_config_name
+        self.direct_internet_access = direct_internet_access
+        self.volume_size_in_gb = volume_size_in_gb
+        self.accelerator_types = accelerator_types
+        self.default_code_repository = default_code_repository
+        self.additional_code_repositories = additional_code_repositories
+        self.root_access = root_access
+        self.status = None
+        self.creation_time = self.last_modified_time = datetime.datetime.now()
+        self.start()
+
+    def validate_volume_size_in_gb(self, volume_size_in_gb):
+        if not validators.is_integer_between(volume_size_in_gb, mn=5, optional=True):
+            message = "Invalid range for parameter VolumeSizeInGB, value: {}, valid range: 5-inf"
+            raise RESTError(
+                error_type="ValidationException",
+                message=message,
+                template="error_json",
+            )
+
+    def validate_instance_type(self, instance_type):
+        VALID_INSTANCE_TYPES = [
+            "ml.p2.xlarge",
+            "ml.m5.4xlarge",
+            "ml.m4.16xlarge",
+            "ml.t3.xlarge",
+            "ml.p3.16xlarge",
+            "ml.t2.xlarge",
+            "ml.p2.16xlarge",
+            "ml.c4.2xlarge",
+            "ml.c5.2xlarge",
+            "ml.c4.4xlarge",
+            "ml.c5d.2xlarge",
+            "ml.c5.4xlarge",
+            "ml.c5d.4xlarge",
+            "ml.c4.8xlarge",
+            "ml.c5d.xlarge",
+            "ml.c5.9xlarge",
+            "ml.c5.xlarge",
+            "ml.c5d.9xlarge",
+            "ml.c4.xlarge",
+            "ml.t2.2xlarge",
+            "ml.c5d.18xlarge",
+            "ml.t3.2xlarge",
+            "ml.t3.medium",
+            "ml.t2.medium",
+            "ml.c5.18xlarge",
+            "ml.p3.2xlarge",
+            "ml.m5.xlarge",
+            "ml.m4.10xlarge",
+            "ml.t2.large",
+            "ml.m5.12xlarge",
+            "ml.m4.xlarge",
+            "ml.t3.large",
+            "ml.m5.24xlarge",
+            "ml.m4.2xlarge",
+            "ml.p2.8xlarge",
+            "ml.m5.2xlarge",
+            "ml.p3.8xlarge",
+            "ml.m4.4xlarge",
+        ]
+        if not validators.is_one_of(instance_type, VALID_INSTANCE_TYPES):
+            message = f"Value '{instance_type}' at 'instanceType' failed to satisfy constraint: Member must satisfy enum value set: {VALID_INSTANCE_TYPES}"
+            raise RESTError(
+                error_type="ValidationException",
+                message=message,
+                template="error_json",
+            )
 
     @property
     def arn(self):
@@ -18,8 +113,24 @@ class FakeSagemakerNotebookInstance:
             + ":"
             + str(ACCOUNT_ID)
             + ":notebook-instance/"
-            + self.name
+            + self.notebook_instance_name
         )
+
+    @property
+    def url(self):
+        return (
+            f"{self.notebook_instance_name}.notebook.{self.region_name}.sagemaker.aws"
+        )
+
+    def start(self):
+        self.status = "InService"
+
+    @property
+    def is_deletable(self):
+        return self.status in ["Stopped", "Failed"]
+
+    def stop(self):
+        self.status = "Stopped"
 
 
 class SageMakerBackend(BaseBackend):
@@ -32,12 +143,78 @@ class SageMakerBackend(BaseBackend):
         super().reset()
         self.region_name = region_name
 
-    def create_notebook_instance(self, name, instance_type, execution_role):
-        # if name in self.notebook_instances:
-        #     raise Exception("Notebook Instance named {name} already exists")
-        notebook_instance = FakeSagemakerNotebookInstance(name, instance_type, execution_role, self.region_name)
-        self.notebook_instances[name] = notebook_instance
+    def create_notebook_instance(
+        self,
+        notebook_instance_name,
+        instance_type,
+        role_arn,
+        subnet_id=None,
+        security_group_ids=[],
+        kms_key_id=None,
+        tags=[],
+        lifecycle_config_name=None,
+        direct_internet_access="Enabled",
+        volume_size_in_gb=5,
+        accelerator_types=None,
+        default_code_repository=None,
+        additional_code_repositories=[],
+        root_access=None,
+    ):
+        self._validate_unique_name(notebook_instance_name)
+
+        notebook_instance = FakeSagemakerNotebookInstance(
+            self.region_name,
+            notebook_instance_name,
+            instance_type,
+            role_arn,
+            subnet_id=subnet_id,
+            security_group_ids=security_group_ids,
+            kms_key_id=kms_key_id,
+            tags=tags,
+            lifecycle_config_name=lifecycle_config_name,
+            direct_internet_access=direct_internet_access
+            if direct_internet_access is not None
+            else "Enabled",
+            volume_size_in_gb=volume_size_in_gb if volume_size_in_gb is not None else 5,
+            accelerator_types=accelerator_types,
+            default_code_repository=default_code_repository,
+            additional_code_repositories=additional_code_repositories,
+            root_access=root_access,
+        )
+        self.notebook_instances[notebook_instance_name] = notebook_instance
         return notebook_instance
+
+    def _validate_unique_name(self, notebook_instance_name):
+        if notebook_instance_name in self.notebook_instances:
+            duplicate_arn = self.notebook_instances[notebook_instance_name].arn
+            message = f"Cannot create a duplicate Notebook Instance ({duplicate_arn})"
+            raise RESTError(
+                error_type="ValidationException",
+                message=message,
+                template="error_json",
+            )
+
+    def get_notebook_instance(self, notebook_instance_name):
+        try:
+            return self.notebook_instances[notebook_instance_name]
+        except KeyError:
+            message = "RecordNotFound"
+            raise RESTError(
+                error_type="ValidationException",
+                message=message,
+                template="error_json",
+            )
+
+    def delete_notebook_instance(self, notebook_instance_name):
+        notebook_instance = self.get_notebook_instance(notebook_instance_name)
+        if not notebook_instance.is_deletable:
+            message = f"Status ({notebook_instance.status}) not in ([Stopped, Failed]). Unable to transition to (Deleting) for Notebook Instance ({notebook_instance.arn})"
+            raise RESTError(
+                error_type="ValidationException",
+                message=message,
+                template="error_json",
+            )
+        del self.notebook_instances[notebook_instance_name]
 
 
 sagemaker_backends = {}
--- moto/sagemaker/responses.py
+++ moto/sagemaker/responses.py
@@ -14,15 +14,24 @@ class SageMakerResponse(BaseResponse):
 
     @amzn_request_id
     def create_notebook_instance(self):
-        name = self._get_param("NotebookInstanceName")
-        instance_type = self._get_param("InstanceType")
-        role_arn = self._get_param("RoleArn")
-        tags = self._get_param("Tags")
         try:
             sagemaker_notebook = self.sagemaker_backend.create_notebook_instance(
-                name,
-                instance_type,
-                role_arn,
+                notebook_instance_name=self._get_param("NotebookInstanceName"),
+                instance_type=self._get_param("InstanceType"),
+                subnet_id=self._get_param("SubnetId"),
+                security_group_ids=self._get_param("SecurityGroupIds"),
+                role_arn=self._get_param("RoleArn"),
+                kms_key_id=self._get_param("KmsKeyId"),
+                tags=self._get_param("Tags"),
+                lifecycle_config_name=self._get_param("LifecycleConfigName"),
+                direct_internet_access=self._get_param("DirectInternetAccess"),
+                volume_size_in_gb=self._get_param("VolumeSizeInGB"),
+                accelerator_types=self._get_param("AcceleratorTypes"),
+                default_code_repository=self._get_param("DefaultCodeRepository"),
+                additional_code_repositories=self._get_param(
+                    "AdditionalCodeRepositories"
+                ),
+                root_access=self._get_param("RootAccess"),
             )
             response = {
                 "NotebookInstanceArn": sagemaker_notebook.arn,
@@ -30,3 +39,59 @@ class SageMakerResponse(BaseResponse):
             return 200, {}, json.dumps(response)
         except AWSError as err:
             return err.response()
+
+    @amzn_request_id
+    def describe_notebook_instance(self):
+        notebook_instance_name = self._get_param("NotebookInstanceName")
+        try:
+            notebook_instance = self.sagemaker_backend.get_notebook_instance(
+                notebook_instance_name
+            )
+            response = {
+                "NotebookInstanceArn": notebook_instance.arn,
+                "NotebookInstanceName": notebook_instance.notebook_instance_name,
+                "NotebookInstanceStatus": notebook_instance.status,
+                "Url": notebook_instance.url,
+                "InstanceType": notebook_instance.instance_type,
+                "SubnetId": notebook_instance.subnet_id,
+                "SecurityGroups": notebook_instance.security_group_ids,
+                "RoleArn": notebook_instance.role_arn,
+                "KmsKeyId": notebook_instance.kms_key_id,
+                # ToDo: NetworkInterfaceId
+                "LastModifiedTime": str(notebook_instance.last_modified_time),
+                "CreationTime": str(notebook_instance.creation_time),
+                "NotebookInstanceLifecycleConfigName": notebook_instance.lifecycle_config_name,
+                "DirectInternetAccess": notebook_instance.direct_internet_access,
+                "VolumeSizeInGB": notebook_instance.volume_size_in_gb,
+                "AcceleratorTypes": notebook_instance.accelerator_types,
+                "DefaultCodeRepository": notebook_instance.default_code_repository,
+                "AdditionalCodeRepositories": notebook_instance.additional_code_repositories,
+                "RootAccess": notebook_instance.root_access,
+            }
+            return 200, {}, json.dumps(response)
+        except AWSError as err:
+            return err.response()
+
+    @amzn_request_id
+    def start_notebook_instance(self):
+        notebook_instance_name = self._get_param("NotebookInstanceName")
+        notebook_instance = self.sagemaker_backend.get_notebook_instance(
+            notebook_instance_name
+        )
+        notebook_instance.start()
+        return 200, {}, json.dumps("{}")
+
+    @amzn_request_id
+    def stop_notebook_instance(self):
+        notebook_instance_name = self._get_param("NotebookInstanceName")
+        notebook_instance = self.sagemaker_backend.get_notebook_instance(
+            notebook_instance_name
+        )
+        notebook_instance.stop()
+        return 200, {}, json.dumps("{}")
+
+    @amzn_request_id
+    def delete_notebook_instance(self):
+        notebook_instance_name = self._get_param("NotebookInstanceName")
+        self.sagemaker_backend.delete_notebook_instance(notebook_instance_name)
+        return 200, {}, json.dumps("{}")
