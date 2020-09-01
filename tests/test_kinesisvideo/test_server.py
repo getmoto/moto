a7b46c681f66c9a012716f14d6a07a689e1c3fc1
toshiya kawasaki <kawasakitoshiya@gmail.com>
2020-09-01 23:32:42 +0900

    add server test for kinesisvideo

--- tests/test_kinesisvideo/test_server.py
+++ tests/test_kinesisvideo/test_server.py
@@ -11,7 +11,8 @@ Test the different server responses
 
 
 @mock_kinesisvideo
-def test_kinesisvideo_list():
+def test_kinesisvideo_server_is_up():
     backend = server.create_backend_app("kinesisvideo")
     test_client = backend.test_client()
-    # do test
+    res = test_client.post("/listStreams")
+    res.status_code.should.equal(200)
