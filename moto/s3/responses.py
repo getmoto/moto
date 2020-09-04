7b1f0e3a75101cc498011acae15c3765d64ebe14
ruthbovell <ruth.bovell@bbc.co.uk>
2020-09-04 14:38:34 +0100

    3224 Enhancement - S3 Copy restored glacier objects
    - adds setter for expiry date
    - copy sets expiry date to none when source is glacier object
    - throws error for copying glacier object only if not restored/still restoring

--- moto/s3/responses.py
+++ moto/s3/responses.py
@@ -1259,8 +1259,16 @@ class ResponseObject(_TemplateEnvironmentMixin, ActionAuthenticatorMixin):
             )
 
             if key is not None:
-                if key.storage_class in ["GLACIER", "DEEP_ARCHIVE"]:
+                if key.storage_class in "GLACIER":
+                    if key.response_dict.get(
+                        "x-amz-restore"
+                    ) is None or 'ongoing-request="true"' in key.response_dict.get(
+                        "x-amz-restore"
+                    ):
+                        raise ObjectNotInActiveTierError(key)
+                if key.storage_class in "DEEP_ARCHIVE":
                     raise ObjectNotInActiveTierError(key)
+
                 self.backend.copy_key(
                     src_bucket,
                     src_key,
