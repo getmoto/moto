280ec08554fa28f84261d8fb1013bf55e514d96c
oriroza <ori@atidot.com>
2020-11-12 15:48:07 +0200

    patching moto to have a callback

--- moto/ecs/__init__.py
+++ moto/ecs/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from .models import ecs_backends
-from ..core.models import base_decorator, deprecated_base_decorator
-
-ecs_backend = ecs_backends["us-east-1"]
-mock_ecs = base_decorator(ecs_backends)
-mock_ecs_deprecated = deprecated_base_decorator(ecs_backends)
+from ..core.models import base_decorator, deprecated_base_decorator, atidot_base_decorator
+bk = ecs_backends
+ecs_backend = bk["us-east-1"]
+mock_ecs = atidot_base_decorator(bk)
+mock_ecs_deprecated = deprecated_base_decorator(bk)
