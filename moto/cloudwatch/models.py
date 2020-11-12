c91d5cb975db421793855c77c9689c1b2b5d57fe
usmankb <usman@krazybee.com>
2020-11-12 22:26:25 +0530

    Fix:CloudWatch List metrics with dimensions

--- moto/cloudwatch/models.py
+++ moto/cloudwatch/models.py
@@ -155,6 +155,17 @@ class FakeAlarm(BaseModel):
         self.state_updated_timestamp = datetime.utcnow()
 
 
+def are_dimensions_same(metric_dimensions, dimensions):
+    for dimension in metric_dimensions:
+        is_dimension_found = False
+        for new_dimension in dimensions:
+            if dimension.name == new_dimension.get('Name') and  dimension.value==new_dimension.get('Value'):
+                is_dimension_found = True
+        if not is_dimension_found:
+            return False
+    return True
+
+
 class MetricDatum(BaseModel):
     def __init__(self, namespace, name, value, dimensions, timestamp):
         self.namespace = namespace
@@ -165,15 +176,20 @@ class MetricDatum(BaseModel):
             Dimension(dimension["Name"], dimension["Value"]) for dimension in dimensions
         ]
 
-    def filter(self, namespace, name, dimensions):
+    def filter(self, namespace, name, dimensions, already_present_metrics):
         if namespace and namespace != self.namespace:
             return False
         if name and name != self.name:
             return False
+        for metric in already_present_metrics:
+            if dimensions and are_dimensions_same(metric.dimensions, dimensions):
+                return False
+
         if dimensions and any(
             Dimension(d["Name"], d["Value"]) not in self.dimensions for d in dimensions
         ):
             return False
+
         return True
 
 
@@ -354,6 +370,16 @@ class CloudWatchBackend(BaseBackend):
         for alarm_name in alarm_names:
             self.alarms.pop(alarm_name, None)
 
+    # def add_metric(self, namespace, metric_data, new_metric_info):
+    #     for value in self.metric_data:
+    #         if value.namespace == namespace and value.name == metric_data.get("MetricName") and value.value == float(metric_data.get("Value", 0)):
+    #             self.metric_data.remove(value)
+    #             self.metric_data.append(new_metric_info)
+    #             print(new_metric_info.value)
+    #             return
+    #
+    #     self.metric_data.append(new_metric_info)
+
     def put_metric_data(self, namespace, metric_data):
         for metric_member in metric_data:
             # Preserve "datetime" for get_metric_statistics comparisons
@@ -514,12 +540,12 @@ class CloudWatchBackend(BaseBackend):
 
     def get_filtered_metrics(self, metric_name, namespace, dimensions):
         metrics = self.get_all_metrics()
-        metrics = [
-            md
-            for md in metrics
-            if md.filter(namespace=namespace, name=metric_name, dimensions=dimensions)
-        ]
-        return metrics
+        new_metrics = []
+        for md in metrics:
+            if md.filter(namespace=namespace, name=metric_name,
+                         dimensions=dimensions, already_present_metrics=new_metrics):
+                new_metrics.append(md)
+        return new_metrics
 
     def _get_paginated(self, metrics):
         if len(metrics) > 500:
--- tests/test_cloudwatch/test_cloudwatch_boto3.py
+++ tests/test_cloudwatch/test_cloudwatch_boto3.py
@@ -350,6 +350,29 @@ def test_get_metric_statistics():
 
 
 @mock_cloudwatch
+def test_duplicate_put_metric_data():
+    conn = boto3.client("cloudwatch", region_name="us-east-1")
+    utc_now = datetime.now(tz=pytz.utc)
+
+    conn.put_metric_data(
+        Namespace="tester",
+        MetricData=[dict(MetricName="metric", Dimensions=[{"Name": "Name", "Value": "B"}], Value=1.5, Timestamp=utc_now)],
+
+    )
+
+    result = conn.list_metrics(Namespace="tester" ,Dimensions=[{"Name": "Name", "Value": "B"}])["Metrics"]
+    len(result).should.equal(1)
+
+    conn.put_metric_data(
+        Namespace="tester",
+        MetricData=[dict(MetricName="metric", Dimensions=[{"Name": "Name", "Value": "B"}], Value=1.5, Timestamp=utc_now)],
+    )
+
+    result = conn.list_metrics(Namespace="tester", Dimensions=[{"Name": "Name", "Value": "B"}])["Metrics"]
+    len(result).should.equal(1)
+
+
+@mock_cloudwatch
 @freeze_time("2020-02-10 18:44:05")
 def test_custom_timestamp():
     utc_now = datetime.now(tz=pytz.utc)
