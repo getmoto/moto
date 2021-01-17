e64e8651e10012d8960e2136561aa9206bd3a701
Nevil Macwan <macnev2013@gmail.com>
2021-01-17 11:26:51 +0000

    lint fixed for py37

--- tests/test_s3/test_s3.py
+++ tests/test_s3/test_s3.py
@@ -4980,7 +4980,7 @@ def test_object_headers():
     bucket = "my-bucket"
     s3 = boto3.client("s3")
     s3.create_bucket(Bucket=bucket)
-    
+
     res = s3.put_object(
         Bucket=bucket,
         Body=b"test",
