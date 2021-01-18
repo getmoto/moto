465bb7d6df3e8ef291e4cc306302e05443127048
Jordan Dimov <jdimov@a115.co.uk>
2021-01-18 10:49:42 +0000

    Adds further url patterns for medialive

--- moto/medialive/urls.py
+++ moto/medialive/urls.py
@@ -10,6 +10,9 @@ response = MediaLiveResponse()
 
 
 url_paths = {
-    "{0}/prod/channels": response.dispatch,
-    "{0}/prod/inputs": response.dispatch,
+    "{0}/prod/channels$": response.dispatch,
+    "{0}/prod/channels/(?P<channelid>[^/.]+)$": response.dispatch,
+    "{0}/prod/channels/(?P<channelid>[^/.]+)/start$": response.dispatch,
+    "{0}/prod/inputs$": response.dispatch,
+    "{0}/prod/channels/(?P<inputid>[^/.]+)$": response.dispatch,
 }
