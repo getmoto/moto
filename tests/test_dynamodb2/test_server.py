ac13dc71d2219e261f60e9a4bda163dcdc878b62
jbergknoff <jonathan@bergknoff.com>
2021-02-11 12:49:45 -0600

    Add CRC32 to DynamoDB responses

--- tests/test_dynamodb2/test_server.py
+++ tests/test_dynamodb2/test_server.py
@@ -17,3 +17,4 @@ def test_table_list():
     headers = {"X-Amz-Target": "TestTable.ListTables"}
     res = test_client.get("/", headers=headers)
     res.data.should.contain(b"TableNames")
+    res.headers["X-Amz-Crc32"].should.equal("363771991")
