c72670d536562ddfa9ac97cb064f08743e5f6139
Cristopher Pinzón <cristopher.pinzon@gmail.com>
2021-02-11 13:31:17 -0500

    Functionality added to SES service (#3670)
    * correct exceptions when mising parameters
    
    * test_render_template function
    
    * update ses template function
    
    * fix import
    
    * except fixed
    
    * tests and py2 fix

--- moto/ses/exceptions.py
+++ moto/ses/exceptions.py
@@ -36,6 +36,29 @@ class TemplateNameAlreadyExists(RESTError):
         )
 
 
+class ValidationError(RESTError):
+    code = 400
+
+    def __init__(self, message):
+        super(ValidationError, self).__init__("ValidationError", message)
+
+
+class InvalidParameterValue(RESTError):
+    code = 400
+
+    def __init__(self, message):
+        super(InvalidParameterValue, self).__init__("InvalidParameterValue", message)
+
+
+class InvalidRenderingParameterException:
+    code = 400
+
+    def __init__(self, message):
+        super(InvalidRenderingParameterException, self).__init__(
+            "InvalidRenderingParameterException", message
+        )
+
+
 class TemplateDoesNotExist(RESTError):
     code = 400
 
