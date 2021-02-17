ccd601574d4072be44dea20bd984d255716e1a07
Gordon Forsythe <baohx2000@gmail.com>
2021-02-17 12:43:57 -0700

    set character encoding

--- moto/cognitoidp/models.py
+++ moto/cognitoidp/models.py
@@ -195,7 +195,7 @@ class CognitoIdpUserPoolDomain(BaseModel):
                 self.custom_domain_config["CertificateArn"].encode("utf-8")
             ).hexdigest()
             return "{hash}.cloudfront.net".format(hash=hash[:16])
-        hash = hashlib.md5(self.user_pool_id).hexdigest()
+        hash = hashlib.md5(self.user_pool_id.encode("utf-8")).hexdigest()
         return "{hash}.amazoncognito.com".format(hash=hash[:16])
 
     def to_json(self, extended=True):
