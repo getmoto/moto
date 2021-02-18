54acb0d02e5e7c39a035fa17434769a45c3e30b9
Connor George <georgeconnorc@gmail.com>
2021-02-18 17:47:27 +0000

    test_support - addressed PR comments, to cycle through a faked number of check status responses

--- moto/support/models.py
+++ moto/support/models.py
@@ -3,7 +3,6 @@ from boto3 import Session
 from pkg_resources import resource_filename
 from moto.core import BaseBackend
 from moto.utilities.utils import load_resource
-import random
 
 
 checks_json = "resources/describe_trusted_advisor_checks.json"
@@ -14,6 +13,7 @@ class SupportBackend(BaseBackend):
     def __init__(self, region_name=None):
         super(SupportBackend, self).__init__()
         self.region_name = region_name
+        self.check_status = {}
 
     def reset(self):
         region_name = self.region_name
@@ -26,15 +26,37 @@ class SupportBackend(BaseBackend):
         return checks
 
     def refresh_trusted_advisor_check(self, check_id):
-        possible_statuses = ["none", "enqueued", "processing", "success", "abandoned"]
+        self.advance_check_status(check_id)
         status = {
             "status": {
                 "checkId": check_id,
-                "status": random.choice(possible_statuses),
+                "status": self.check_status[check_id],
                 "millisUntilNextRefreshable": 123,
             }
         }
         return status
+
+    def advance_check_status(self, check_id):
+        """
+        Fake an advancement through statuses on refreshing TA checks
+        """
+        if check_id not in self.check_status:
+            self.check_status[check_id] = "none"
+
+        elif self.check_status[check_id] == "none":
+            self.check_status[check_id] = "enqueued"
+
+        elif self.check_status[check_id] == "enqueued":
+            self.check_status[check_id] = "processing"
+
+        elif self.check_status[check_id] == "processing":
+            self.check_status[check_id] = "success"
+
+        elif self.check_status[check_id] == "success":
+            self.check_status[check_id] = "abandoned"
+
+        elif self.check_status[check_id] == "abandoned":
+            self.check_status[check_id] = "none"
 
 
 support_backends = {}
--- tests/test_support/test_support.py
+++ tests/test_support/test_support.py
@@ -68,3 +68,90 @@ def test_refresh_trusted_advisor_check_returns_an_expected_status():
     response = client.refresh_trusted_advisor_check(checkId=check_name)
     actual_status = [response["status"]["status"]]
     set(actual_status).issubset(possible_statuses).should.be.true
+
+
+@mock_support
+def test_refresh_trusted_advisor_check_cycles_to_new_status_on_each_call():
+    """
+    On each call, the next expected status is returned
+    """
+    client = boto3.client("support", "us-east-1")
+    check_name = "XXXIIIY"
+    actual_statuses = []
+    possible_statuses = ["none", "enqueued", "processing", "success", "abandoned"]
+
+    for status in possible_statuses:
+        response = client.refresh_trusted_advisor_check(checkId=check_name)
+        actual_statuses.append(response["status"]["status"])
+
+    actual_statuses.should.equal(possible_statuses)
+
+
+@mock_support
+def test_refresh_trusted_advisor_check_cycles_to_new_status_on_each_call():
+    """
+    Called only three times, only three expected statuses are returned
+    """
+    client = boto3.client("support", "us-east-1")
+    check_name = "XXXIIIY"
+    actual_statuses = []
+    possible_statuses = ["none", "enqueued", "processing"]
+
+    for status in possible_statuses:
+        response = client.refresh_trusted_advisor_check(checkId=check_name)
+        actual_statuses.append(response["status"]["status"])
+
+    unexpected_statuses = set(["success", "abandoned"]).issubset(actual_statuses)
+
+    actual_statuses.should.equal(
+        possible_statuses
+    ) and unexpected_statuses.should.be.false
+
+
+@mock_support
+def test_refresh_trusted_advisor_check_cycles_to_new_status_on_with_two_checks():
+    """
+    On each call, the next expected status is returned when additional checks are made
+    """
+    client = boto3.client("support", "us-east-1")
+    check_1_name = "XXXIIIY"
+    check_2_name = "XXXIIIZ"
+    check_1_statuses = []
+    check_2_statuses = []
+    possible_statuses = ["none", "enqueued", "processing", "success", "abandoned"]
+
+    for check in possible_statuses:
+        response = client.refresh_trusted_advisor_check(checkId=check_1_name)
+        check_1_statuses.append(response["status"]["status"])
+
+    for check in possible_statuses:
+        response = client.refresh_trusted_advisor_check(checkId=check_2_name)
+        check_2_statuses.append(response["status"]["status"])
+
+    check_1_statuses.should.equal(possible_statuses) and check_2_statuses.should.equal(
+        possible_statuses
+    )
+
+
+@mock_support
+def test_refresh_trusted_advisor_check_cycle_continues_on_full_cycle():
+    """
+    After cycling through all statuses, the check continues the cycle
+    """
+
+    client = boto3.client("support", "us-east-1")
+    check_name = "XXXIIIY"
+    possible_statuses = [
+        "none",
+        "enqueued",
+        "processing",
+        "success",
+        "abandoned",
+    ]
+
+    for status in possible_statuses:
+        client.refresh_trusted_advisor_check(checkId=check_name)
+
+    expected_none_response = client.refresh_trusted_advisor_check(checkId=check_name)
+
+    expected_none_response["status"]["status"].should.equal("none")
