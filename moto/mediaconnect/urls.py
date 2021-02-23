e9b71881e4e06d1afe20067436292c614f89adba
Jordan Dimov <jdimov@a115.co.uk>
2021-02-23 14:55:01 +0000

    Adds URLs

--- moto/mediaconnect/urls.py
+++ moto/mediaconnect/urls.py
@@ -11,4 +11,8 @@ response = MediaConnectResponse()
 
 url_paths = {
     "{0}/v1/flows": response.dispatch,
+    "{0}/v1/flows/(?P<flowarn>[^/.]+)": response.dispatch,
+    "{0}/v1/flows/start/(?P<flowarn>[^/.]+)": response.dispatch,
+    "{0}/v1/flows/stop/(?P<flowarn>[^/.]+)": response.dispatch,
+    "{0}/v1/tags/(?P<resourcearn>[^/.]+)": response.dispatch,
 }
