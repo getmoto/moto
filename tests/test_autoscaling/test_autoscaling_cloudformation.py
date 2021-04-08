11fbd9adf263c90773cf0667c398c91964218e2b
Bert Blommers <info@bertblommers.nl>
2021-04-08 14:57:41 +0100

    CloudFormation - duplicate deprecated tests

--- tests/test_autoscaling/test_autoscaling_cloudformation.py
+++ tests/test_autoscaling/test_autoscaling_cloudformation.py
@@ -1,11 +1,8 @@
 import boto3
+import json
 import sure  # noqa
 
-from moto import (
-    mock_autoscaling,
-    mock_cloudformation,
-    mock_ec2,
-)
+from moto import mock_autoscaling, mock_cloudformation, mock_ec2, mock_elb
 
 from .utils import setup_networking
 from tests import EXAMPLE_AMI_ID
@@ -276,3 +273,194 @@ Outputs:
     lt["LaunchTemplateId"].should.be.equal(launch_template_id)
     lt["LaunchTemplateName"].should.be.equal("test_launch_template_new")
     lt["Version"].should.be.equal("1")
+
+
+@mock_autoscaling
+@mock_elb
+@mock_cloudformation
+@mock_ec2
+def test_autoscaling_group_with_elb():
+    web_setup_template = {
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Resources": {
+            "my-as-group": {
+                "Type": "AWS::AutoScaling::AutoScalingGroup",
+                "Properties": {
+                    "AvailabilityZones": ["us-east-1a"],
+                    "LaunchConfigurationName": {"Ref": "my-launch-config"},
+                    "MinSize": "2",
+                    "MaxSize": "2",
+                    "DesiredCapacity": "2",
+                    "LoadBalancerNames": [{"Ref": "my-elb"}],
+                    "Tags": [
+                        {
+                            "Key": "propagated-test-tag",
+                            "Value": "propagated-test-tag-value",
+                            "PropagateAtLaunch": True,
+                        },
+                        {
+                            "Key": "not-propagated-test-tag",
+                            "Value": "not-propagated-test-tag-value",
+                            "PropagateAtLaunch": False,
+                        },
+                    ],
+                },
+            },
+            "my-launch-config": {
+                "Type": "AWS::AutoScaling::LaunchConfiguration",
+                "Properties": {
+                    "ImageId": EXAMPLE_AMI_ID,
+                    "InstanceType": "t2.medium",
+                    "UserData": "some user data",
+                },
+            },
+            "my-elb": {
+                "Type": "AWS::ElasticLoadBalancing::LoadBalancer",
+                "Properties": {
+                    "AvailabilityZones": ["us-east-1a"],
+                    "Listeners": [
+                        {
+                            "LoadBalancerPort": "80",
+                            "InstancePort": "80",
+                            "Protocol": "HTTP",
+                        }
+                    ],
+                    "LoadBalancerName": "my-elb",
+                    "HealthCheck": {
+                        "Target": "HTTP:80",
+                        "HealthyThreshold": "3",
+                        "UnhealthyThreshold": "5",
+                        "Interval": "30",
+                        "Timeout": "5",
+                    },
+                },
+            },
+        },
+    }
+
+    web_setup_template_json = json.dumps(web_setup_template)
+
+    cf = boto3.client("cloudformation", region_name="us-east-1")
+    ec2 = boto3.client("ec2", region_name="us-east-1")
+    elb = boto3.client("elb", region_name="us-east-1")
+    client = boto3.client("autoscaling", region_name="us-east-1")
+
+    cf.create_stack(StackName="web_stack", TemplateBody=web_setup_template_json)
+
+    autoscale_group = client.describe_auto_scaling_groups()["AutoScalingGroups"][0]
+    autoscale_group["LaunchConfigurationName"].should.contain("my-launch-config")
+    autoscale_group["LoadBalancerNames"].should.equal(["my-elb"])
+
+    # Confirm the Launch config was actually created
+    client.describe_launch_configurations()[
+        "LaunchConfigurations"
+    ].should.have.length_of(1)
+
+    # Confirm the ELB was actually created
+    elb.describe_load_balancers()["LoadBalancerDescriptions"].should.have.length_of(1)
+
+    resources = cf.list_stack_resources(StackName="web_stack")["StackResourceSummaries"]
+    as_group_resource = [
+        resource
+        for resource in resources
+        if resource["ResourceType"] == "AWS::AutoScaling::AutoScalingGroup"
+    ][0]
+    as_group_resource["PhysicalResourceId"].should.contain("my-as-group")
+
+    launch_config_resource = [
+        resource
+        for resource in resources
+        if resource["ResourceType"] == "AWS::AutoScaling::LaunchConfiguration"
+    ][0]
+    launch_config_resource["PhysicalResourceId"].should.contain("my-launch-config")
+
+    elb_resource = [
+        resource
+        for resource in resources
+        if resource["ResourceType"] == "AWS::ElasticLoadBalancing::LoadBalancer"
+    ][0]
+    elb_resource["PhysicalResourceId"].should.contain("my-elb")
+
+    # confirm the instances were created with the right tags
+    reservations = ec2.describe_instances()["Reservations"]
+
+    reservations.should.have.length_of(1)
+    reservations[0]["Instances"].should.have.length_of(2)
+    for instance in reservations[0]["Instances"]:
+        tag_keys = [t["Key"] for t in instance["Tags"]]
+        tag_keys.should.contain("propagated-test-tag")
+        tag_keys.should_not.contain("not-propagated-test-tag")
+
+
+@mock_autoscaling
+@mock_cloudformation
+@mock_ec2
+def test_autoscaling_group_update():
+    asg_template = {
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Resources": {
+            "my-as-group": {
+                "Type": "AWS::AutoScaling::AutoScalingGroup",
+                "Properties": {
+                    "AvailabilityZones": ["us-west-1a"],
+                    "LaunchConfigurationName": {"Ref": "my-launch-config"},
+                    "MinSize": "2",
+                    "MaxSize": "2",
+                    "DesiredCapacity": "2",
+                },
+            },
+            "my-launch-config": {
+                "Type": "AWS::AutoScaling::LaunchConfiguration",
+                "Properties": {
+                    "ImageId": EXAMPLE_AMI_ID,
+                    "InstanceType": "t2.medium",
+                    "UserData": "some user data",
+                },
+            },
+        },
+    }
+    asg_template_json = json.dumps(asg_template)
+
+    cf = boto3.client("cloudformation", region_name="us-west-1")
+    ec2 = boto3.client("ec2", region_name="us-west-1")
+    client = boto3.client("autoscaling", region_name="us-west-1")
+    cf.create_stack(StackName="asg_stack", TemplateBody=asg_template_json)
+
+    asg = client.describe_auto_scaling_groups()["AutoScalingGroups"][0]
+    asg["MinSize"].should.equal(2)
+    asg["MaxSize"].should.equal(2)
+    asg["DesiredCapacity"].should.equal(2)
+
+    asg_template["Resources"]["my-as-group"]["Properties"]["MaxSize"] = 3
+    asg_template["Resources"]["my-as-group"]["Properties"]["Tags"] = [
+        {
+            "Key": "propagated-test-tag",
+            "Value": "propagated-test-tag-value",
+            "PropagateAtLaunch": True,
+        },
+        {
+            "Key": "not-propagated-test-tag",
+            "Value": "not-propagated-test-tag-value",
+            "PropagateAtLaunch": False,
+        },
+    ]
+    asg_template_json = json.dumps(asg_template)
+    cf.update_stack(StackName="asg_stack", TemplateBody=asg_template_json)
+    asg = client.describe_auto_scaling_groups()["AutoScalingGroups"][0]
+    asg["MinSize"].should.equal(2)
+    asg["MaxSize"].should.equal(3)
+    asg["DesiredCapacity"].should.equal(2)
+
+    # confirm the instances were created with the right tags
+    reservations = ec2.describe_instances()["Reservations"]
+    running_instance_count = 0
+    for res in reservations:
+        for instance in res["Instances"]:
+            if instance["State"]["Name"] == "running":
+                running_instance_count += 1
+                instance["Tags"].should.contain(
+                    {"Key": "propagated-test-tag", "Value": "propagated-test-tag-value"}
+                )
+                tag_keys = [t["Key"] for t in instance["Tags"]]
+                tag_keys.should_not.contain("not-propagated-test-tag")
+    running_instance_count.should.equal(2)
