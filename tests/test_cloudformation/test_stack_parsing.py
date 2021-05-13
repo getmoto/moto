148df3a1dc9ae91bf45c4c8f83a22b67a3b273cd
sahilshah6196 <sahilshah@ebsco.com>
2021-05-13 16:50:55 -0400

    Fix bug.

--- tests/test_cloudformation/test_stack_parsing.py
+++ tests/test_cloudformation/test_stack_parsing.py
@@ -77,8 +77,8 @@ parameters = {
 
 ssm_parameter = {
     "Parameters": {
-        "SingleParam": {"Type": "AWS::SSM::Parameter::Value<String>"},
-        "ListParam": {"Type": "AWS::SSM::Parameter::Value<List<String>>"},
+        "SingleParamCfn": {"Type": "AWS::SSM::Parameter::Value<String>"},
+        "ListParamCfn": {"Type": "AWS::SSM::Parameter::Value<List<String>>"},
     }
 }
 
@@ -516,20 +516,25 @@ def test_short_form_func_in_yaml_teamplate():
 @mock_ssm
 def test_ssm_parameter_parsing():
     client = boto3.client("ssm")
-    client.put_parameter(Name="SingleParam", Value="string", Type="StringList")
     client.put_parameter(
-        Name="ListParam", Value="comma,separated,string", Type="StringList"
+        Name="/path/to/single/param", Value="string", Type="StringList"
+    )
+    client.put_parameter(
+        Name="/path/to/list/param", Value="comma,separated,string", Type="StringList"
     )
 
     stack = FakeStack(
         stack_id="test_id",
         name="test_stack",
         template=ssm_parameter_template_json,
-        parameters={"SingleParam": "string", "ListParam": "comma,separated,string",},
+        parameters={
+            "SingleParamCfn": "/path/to/single/param",
+            "ListParamCfn": "/path/to/list/param",
+        },
         region_name="us-west-1",
     )
 
-    stack.resource_map.resolved_parameters["SingleParam"].should.equal("string")
-    stack.resource_map.resolved_parameters["ListParam"].should.equal(
+    stack.resource_map.resolved_parameters["SingleParamCfn"].should.equal("string")
+    stack.resource_map.resolved_parameters["ListParamCfn"].should.equal(
         ["comma", "separated", "string"]
     )
