a76a8a5c0c1bfd48827a90ac58a3a9b081b55d0b
Moto Admin <admin@getmoto.org>
2021-07-07 14:30:15 +0000

    Increase version number post-release

--- moto/__init__.py
+++ moto/__init__.py
@@ -132,7 +132,7 @@ mock_mediastoredata = lazy_load(".mediastoredata", "mock_mediastoredata")
 # logging.getLogger('boto').setLevel(logging.CRITICAL)
 
 __title__ = "moto"
-__version__ = "2.0.11.dev"
+__version__ = "2.0.12.dev"
 
 
 try:
