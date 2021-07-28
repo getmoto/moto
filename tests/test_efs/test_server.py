cb52672982d6706bbd8a158af3db0d63bb052cdb
Patrick Greene <patrick.anton.greene@gmail.com>
2021-07-28 10:41:50 -0400

    Don't mix server and API in server tests.

--- tests/test_efs/test_server.py
+++ tests/test_efs/test_server.py
@@ -1,11 +1,16 @@
 from __future__ import unicode_literals
 
+import re
 from os import environ
+
 import pytest
-import boto3
 
 from moto import mock_efs, mock_ec2
 import moto.server as server
+
+
+FILE_SYSTEMS = "/2015-02-01/file-systems"
+MOUNT_TARGETS = "/2015-02-01/mount-targets"
 
 
 @pytest.fixture(scope="function")
@@ -18,36 +23,26 @@ def aws_credentials():
 
 
 @pytest.fixture(scope="function")
-def ec2(aws_credentials):
-    with mock_ec2():
-        yield boto3.client("ec2", region_name="us-east-1")
-
-
-@pytest.fixture(scope="function")
-def efs(aws_credentials):
+def efs_client(aws_credentials):
     with mock_efs():
-        yield boto3.client("efs", region_name="us-east-1")
+        yield server.create_backend_app("efs").test_client()
 
 
 @pytest.fixture(scope="function")
-def client(efs):
-    backend = server.create_backend_app("efs")
-    test_client = backend.test_client()
-    yield test_client
+def subnet_id(aws_credentials):
+    with mock_ec2():
+        ec2_client = server.create_backend_app("ec2").test_client()
+        resp = ec2_client.get("/?Action=DescribeSubnets")
+        subnet_ids = re.findall("<subnetId>(.*?)</subnetId>", resp.data.decode("utf-8"))
+        yield subnet_ids[0]
 
 
 @pytest.fixture(scope="function")
-def subnet_id(ec2):
-    desc_sn_resp = ec2.describe_subnets()
-    subnet = desc_sn_resp["Subnets"][0]
-    yield subnet["SubnetId"]
-
-
-@pytest.fixture(scope="function")
-def file_system_id(efs):
-    create_fs_resp = efs.create_file_system(CreationToken="foobarbaz", Backup=True)
-    create_fs_resp.pop("ResponseMetadata")
-    yield create_fs_resp["FileSystemId"]
+def file_system_id(efs_client):
+    resp = efs_client.post(
+        FILE_SYSTEMS, json={"CreationToken": "foobarbaz", "Backup": True}
+    )
+    yield resp.json["FileSystemId"]
 
 
 """
@@ -59,42 +54,48 @@ connect and respond. Tests of functionality are contained in `test_file_system` 
 """
 
 
-def test_efs_file_system_create(client):
-    res = client.post(
-        "/2015-02-01/file-systems", json={"CreationToken": "2398asdfkajsdf"}
-    )
+def test_efs_file_system_create(efs_client):
+    res = efs_client.post(FILE_SYSTEMS, json={"CreationToken": "2398asdfkajsdf"})
     assert res.status_code == 201
 
 
-def test_efs_file_system_describe(client):
-    res = client.get("/2015-02-01/file-systems")
+def test_efs_file_system_describe(efs_client):
+    res = efs_client.get(FILE_SYSTEMS)
     assert res.status_code == 200
 
 
-def test_efs_file_system_delete(file_system_id, client):
-    res = client.delete("/2015-02-01/file-systems/{}".format(file_system_id))
+def test_efs_file_system_delete(file_system_id, efs_client):
+    res = efs_client.delete("/2015-02-01/file-systems/{}".format(file_system_id))
     assert res.status_code == 204
 
 
-def test_efs_mount_target_create(file_system_id, subnet_id, client):
-    res = client.post(
+def test_efs_mount_target_create(file_system_id, subnet_id, efs_client):
+    res = efs_client.post(
         "/2015-02-01/mount-targets",
         json={"FileSystemId": file_system_id, "SubnetId": subnet_id,},
     )
     assert res.status_code == 200
 
 
-def test_efs_mount_target_describe(file_system_id, client):
-    res = client.get("/2015-02-01/mount-targets?FileSystemId={}".format(file_system_id))
+def test_efs_mount_target_describe(file_system_id, efs_client):
+    res = efs_client.get(
+        "/2015-02-01/mount-targets?FileSystemId={}".format(file_system_id)
+    )
     assert res.status_code == 200
 
 
-def test_efs_mount_target_delete(efs, file_system_id, subnet_id, client):
-    mt_info = efs.create_mount_target(FileSystemId=file_system_id, SubnetId=subnet_id)
-    res = client.delete("/2015-02-01/mount-targets/{}".format(mt_info["MountTargetId"]))
+def test_efs_mount_target_delete(file_system_id, subnet_id, efs_client):
+    create_res = efs_client.post(
+        "/2015-02-01/mount-targets",
+        json={"FileSystemId": file_system_id, "SubnetId": subnet_id,},
+    )
+    mt_id = create_res.json["MountTargetId"]
+    res = efs_client.delete("/2015-02-01/mount-targets/{}".format(mt_id))
     assert res.status_code == 204
 
 
-def test_efs_describe_backup_policy(file_system_id, client):
-    res = client.get("/2015-02-01/file-systems/{}/backup-policy".format(file_system_id))
+def test_efs_describe_backup_policy(file_system_id, efs_client):
+    res = efs_client.get(
+        "/2015-02-01/file-systems/{}/backup-policy".format(file_system_id)
+    )
     assert res.status_code == 200
