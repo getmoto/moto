010a7d09f0eeaa38b39db7382707d8051721aae5
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2021-07-30 21:48:52 -0300

    feat(events): Add policy in put_permission Also add RemoveAllPermissions in remove_permission

--- moto/events/models.py
+++ moto/events/models.py
@@ -21,6 +21,7 @@ from moto.events.exceptions import (
     InvalidEventPatternException,
     IllegalStatusException,
 )
+from moto.iam.policy_validation import IAMPolicyDocumentValidator
 from moto.utilities.tagging_service import TaggingService
 
 from uuid import uuid4
@@ -251,7 +252,7 @@ class EventBus(CloudFormationModel):
         self.name = name
         self.tags = tags or []
 
-        self._permissions = {}
+        self._statements = {}
 
     @property
     def arn(self):
@@ -261,25 +262,19 @@ class EventBus(CloudFormationModel):
 
     @property
     def policy(self):
-        if not len(self._permissions):
-            return None
+        if self._statements:
+            policy = {
+                "Version": "2012-10-17",
+                "Statement": [
+                    stmt.describe()
+                    for stmt in self._statements.values()
+                ]
+            }
+            return json.dumps(policy)
+        return None
 
-        policy = {"Version": "2012-10-17", "Statement": []}
-
-        for sid, permission in self._permissions.items():
-            policy["Statement"].append(
-                {
-                    "Sid": sid,
-                    "Effect": "Allow",
-                    "Principal": {
-                        "AWS": "arn:aws:iam::{}:root".format(permission["Principal"])
-                    },
-                    "Action": permission["Action"],
-                    "Resource": self.arn,
-                }
-            )
-
-        return json.dumps(policy)
+    def has_permissions(self):
+        return len(self._statements) > 0
 
     def delete(self, region_name):
         event_backend = events_backends[region_name]
@@ -334,6 +329,64 @@ class EventBus(CloudFormationModel):
         event_backend = events_backends[region_name]
         event_bus_name = resource_name
         event_backend.delete_event_bus(event_bus_name)
+
+    def add_permission(self, statement_id, action, principal):
+        for sid, statement in self._statements.items():
+            if statement.principal == principal:
+                del self._statements[sid]
+
+        statement = EventBusPolicyStatement(
+            sid=statement_id,
+            action=action,
+            principal=principal,
+            resource=self.arn,
+        )
+        self._statements[statement_id] = statement
+
+    def add_policy(self, policy):
+        policy_statements = policy["Statement"]
+        for new_statement in policy_statements:
+            for sid, statement in self._statements.items():
+                if statement.principal == new_statement["Principal"]:
+                    del self._statements[sid]
+
+        for new_statement in policy_statements:
+            sid = new_statement["Sid"]
+            self._statements[sid] = EventBusPolicyStatement.from_dict(new_statement)
+
+    def remove_statement(self, sid):
+        return self._statements.pop(sid, None)
+
+    def remove_statements(self):
+        self._statements.clear()
+
+
+class EventBusPolicyStatement:
+    def __init__(self, sid, principal, action, resource, effect="Allow"):
+        self.sid = sid
+        self.principal = principal
+        self.action = action
+        self.resource = resource
+        self.effect = effect
+
+    def describe(self):
+        return {
+            "Sid": self.sid,
+            "Effect": self.effect,
+            "Principal": self.principal,
+            "Action": self.action,
+            "Resource": self.resource,
+        }
+
+    @classmethod
+    def from_dict(cls, statement_dict):
+        return cls(
+            sid=statement_dict["Sid"],
+            effect=statement_dict["Effect"],
+            principal=statement_dict["Principal"],
+            action=statement_dict["Action"],
+            resource=statement_dict["Resource"],
+        )
 
 
 class Archive(CloudFormationModel):
@@ -1073,49 +1126,58 @@ class EventsBackend(BaseBackend):
     def test_event_pattern(self):
         raise NotImplementedError()
 
-    def put_permission(self, event_bus_name, action, principal, statement_id):
-        if not event_bus_name:
-            event_bus_name = "default"
-
-        event_bus = self.describe_event_bus(event_bus_name)
-
+    def _put_permission_from_params(self, event_bus, action, principal, statement_id):
+        if principal is None or self.ACCOUNT_ID.match(principal) is None:
+            raise JsonRESTError(
+                "InvalidParameterValue", r"Principal must match ^(\d{1,12}|\*)$"
+            )
         if action is None or action != "events:PutEvents":
             raise JsonRESTError(
                 "ValidationException",
                 "Provided value in parameter 'action' is not supported.",
             )
-
-        if principal is None or self.ACCOUNT_ID.match(principal) is None:
-            raise JsonRESTError(
-                "InvalidParameterValue", r"Principal must match ^(\d{1,12}|\*)$"
-            )
-
         if statement_id is None or self.STATEMENT_ID.match(statement_id) is None:
             raise JsonRESTError(
                 "InvalidParameterValue", r"StatementId must match ^[a-zA-Z0-9-_]{1,64}$"
             )
 
-        event_bus._permissions[statement_id] = {
-            "Action": action,
-            "Principal": principal,
-        }
+        principal = {"AWS": f"arn:aws:iam::{principal}:root"}
+        event_bus.add_permission(statement_id, action, principal)
 
-    def remove_permission(self, event_bus_name, statement_id):
+    def put_permission(self, event_bus_name, action, principal, statement_id, policy):
         if not event_bus_name:
             event_bus_name = "default"
 
         event_bus = self.describe_event_bus(event_bus_name)
 
-        if not len(event_bus._permissions):
-            raise JsonRESTError(
-                "ResourceNotFoundException", "EventBus does not have a policy."
-            )
+        if policy:
+            validator = IAMPolicyDocumentValidator(policy_document=policy)
+            validator.validate()
+            policy_doc = json.loads(policy)
+            event_bus.add_policy(policy_doc)
+        else:
+            self._put_permission_from_params(event_bus, action, principal, statement_id)
 
-        if not event_bus._permissions.pop(statement_id, None):
-            raise JsonRESTError(
-                "ResourceNotFoundException",
-                "Statement with the provided id does not exist.",
-            )
+    def remove_permission(self, event_bus_name, statement_id, remove_all_permissions):
+        if not event_bus_name:
+            event_bus_name = "default"
+
+        event_bus = self.describe_event_bus(event_bus_name)
+
+        if remove_all_permissions:
+            event_bus.remove_statements()
+        else:
+            if not event_bus.has_permissions():
+                raise JsonRESTError(
+                    "ResourceNotFoundException", "EventBus does not have a policy."
+                )
+
+            statement = event_bus.remove_statement(statement_id)
+            if not statement:
+                raise JsonRESTError(
+                    "ResourceNotFoundException",
+                    "Statement with the provided id does not exist.",
+                )
 
     def describe_event_bus(self, name):
         if not name:
