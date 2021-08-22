5c24071f43afb32741497ef37b67467f2d488b89
Bert Blommers <bblommers@users.noreply.github.com>
2021-08-22 11:33:16 +0100

    Support Python 3.9 (#3848)

--- setup.py
+++ setup.py
@@ -129,6 +129,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Testing",
     ],
