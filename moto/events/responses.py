41105297a5b41832bcdc3775bc31f2af3ecc3cbc
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-08-22 11:33:12 +0200

    fix returning missing Name attribute for EventBridge connections

--- moto/events/responses.py
+++ moto/events/responses.py
@@ -422,6 +422,7 @@ class EventsHandler(BaseResponse):
                     "ConnectionArn": connection.arn,
                     "ConnectionState": "AUTHORIZED",
                     "CreationTime": connection.creation_time,
+                    "Name": connection.name,
                     "LastModifiedTime": connection.creation_time,
                     "AuthorizationType": connection.authorization_type,
                 }
