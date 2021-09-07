0484a4a67cb046c4531f674c3af25665536a867f
Karri Balk <kbalk@users.noreply.github.com>
2021-09-07 12:43:26 -0400

    Modify the url_bases values for scaffold

--- moto/firehose/urls.py
+++ moto/firehose/urls.py
@@ -2,5 +2,5 @@
 from .responses import FirehoseResponse
 
 
-url_bases = ["https?://firehose\.(.+)\.amazonaws\.com"]
+url_bases = [r"https?://firehose\.(.+)\.amazonaws\.com"]
 url_paths = {"{0}/$": FirehoseResponse.dispatch}
