e30169cfcbea8326489a157622c164236d57357d
Thomas Rausch <thomas@thrau.at>
2021-09-08 16:06:43 +0200

    Add url_bases index for faster backend lookup (#4209)

--- moto/backends.py
+++ moto/backends.py
@@ -1,93 +1,19 @@
 from __future__ import unicode_literals
 
 import importlib
+import moto
 
-BACKENDS = {
-    "acm": ("acm", "acm_backends"),
-    "apigateway": ("apigateway", "apigateway_backends"),
-    "athena": ("athena", "athena_backends"),
-    "applicationautoscaling": (
-        "applicationautoscaling",
-        "applicationautoscaling_backends",
-    ),
-    "autoscaling": ("autoscaling", "autoscaling_backends"),
-    "batch": ("batch", "batch_backends"),
-    "cloudformation": ("cloudformation", "cloudformation_backends"),
-    "cloudwatch": ("cloudwatch", "cloudwatch_backends"),
-    "codecommit": ("codecommit", "codecommit_backends"),
-    "codepipeline": ("codepipeline", "codepipeline_backends"),
-    "cognito-identity": ("cognitoidentity", "cognitoidentity_backends"),
-    "cognito-idp": ("cognitoidp", "cognitoidp_backends"),
-    "config": ("config", "config_backends"),
-    "datapipeline": ("datapipeline", "datapipeline_backends"),
-    "datasync": ("datasync", "datasync_backends"),
-    "dms": ("dms", "dms_backends"),
-    "dynamodb": ("dynamodb", "dynamodb_backends"),
-    "dynamodb2": ("dynamodb2", "dynamodb_backends2"),
-    "dynamodbstreams": ("dynamodbstreams", "dynamodbstreams_backends"),
-    "ec2": ("ec2", "ec2_backends"),
-    "ec2instanceconnect": ("ec2instanceconnect", "ec2instanceconnect_backends"),
-    "ecr": ("ecr", "ecr_backends"),
-    "ecs": ("ecs", "ecs_backends"),
-    "elasticbeanstalk": ("elasticbeanstalk", "eb_backends"),
-    "elastictranscoder": ("elastictranscoder", "elastictranscoder_backends"),
-    "elb": ("elb", "elb_backends"),
-    "elbv2": ("elbv2", "elbv2_backends"),
-    "emr": ("emr", "emr_backends"),
-    "events": ("events", "events_backends"),
-    "glacier": ("glacier", "glacier_backends"),
-    "glue": ("glue", "glue_backends"),
-    "iam": ("iam", "iam_backends"),
-    "instance_metadata": ("instance_metadata", "instance_metadata_backends"),
-    "iot": ("iot", "iot_backends"),
-    "iot-data": ("iotdata", "iotdata_backends"),
-    "kinesis": ("kinesis", "kinesis_backends"),
-    "kms": ("kms", "kms_backends"),
-    "lambda": ("awslambda", "lambda_backends"),
-    "logs": ("logs", "logs_backends"),
-    "managedblockchain": ("managedblockchain", "managedblockchain_backends"),
-    "moto_api": ("core", "moto_api_backends"),
-    "opsworks": ("opsworks", "opsworks_backends"),
-    "organizations": ("organizations", "organizations_backends"),
-    "polly": ("polly", "polly_backends"),
-    "ram": ("ram", "ram_backends"),
-    "rds": ("rds2", "rds2_backends"),
-    "redshift": ("redshift", "redshift_backends"),
-    "resource-groups": ("resourcegroups", "resourcegroups_backends"),
-    "resourcegroupstaggingapi": (
-        "resourcegroupstaggingapi",
-        "resourcegroupstaggingapi_backends",
-    ),
-    "route53": ("route53", "route53_backends"),
-    "s3": ("s3", "s3_backends"),
-    "s3bucket_path": ("s3", "s3_backends"),
-    "sagemaker": ("sagemaker", "sagemaker_backends"),
-    "secretsmanager": ("secretsmanager", "secretsmanager_backends"),
-    "ses": ("ses", "ses_backends"),
-    "sns": ("sns", "sns_backends"),
-    "sqs": ("sqs", "sqs_backends"),
-    "ssm": ("ssm", "ssm_backends"),
-    "stepfunctions": ("stepfunctions", "stepfunction_backends"),
-    "sts": ("sts", "sts_backends"),
-    "swf": ("swf", "swf_backends"),
-    "transcribe": ("transcribe", "transcribe_backends"),
-    "xray": ("xray", "xray_backends"),
-    "kinesisvideo": ("kinesisvideo", "kinesisvideo_backends"),
-    "medialive": ("medialive", "medialive_backends"),
-    "kinesis-video-archived-media": (
-        "kinesisvideoarchivedmedia",
-        "kinesisvideoarchivedmedia_backends",
-    ),
-    "forecast": ("forecast", "forecast_backends"),
-    "support": ("support", "support_backends"),
-    "mediaconnect": ("mediaconnect", "mediaconnect_backends"),
-    "mediapackage": ("mediapackage", "mediapackage_backends"),
-    "mediastore": ("mediastore", "mediastore_backends"),
-    "mediastore-data": ("mediastoredata", "mediastoredata_backends"),
-    "eks": ("eks", "eks_backends"),
-    "efs": ("efs", "efs_backends"),
-    "wafv2": ("wafv2", "wafv2_backends"),
-}
+
+decorators = [
+    d
+    for d in dir(moto)
+    if d.startswith("mock_") and not d.endswith("_deprecated") and not d == "mock_all"
+]
+decorator_functions = [getattr(moto, f) for f in decorators]
+BACKENDS = {f.boto3_name: (f.name, f.backend) for f in decorator_functions}
+BACKENDS["moto_api"] = ("core", "moto_api_backends")
+BACKENDS["instance_metadata"] = ("instance_metadata", "instance_metadata_backends")
+BACKENDS["s3bucket_path"] = ("s3", "s3_backends")
 
 
 def _import_backend(module_name, backends_name):
@@ -108,12 +34,6 @@ def named_backends():
 def get_backend(name):
     module_name, backends_name = BACKENDS[name]
     return _import_backend(module_name, backends_name)
-
-
-def search_backend(predicate):
-    for name, backend in named_backends():
-        if predicate(backend):
-            return name
 
 
 def get_model(name, region_name):
