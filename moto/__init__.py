5081e96ebd1a495c8458496a965a412068c957d5
Moto Admin <admin@getmoto.org>
2021-09-16 09:13:00 +0000

    Increase version number post-release

--- moto/__init__.py
+++ moto/__init__.py
@@ -154,7 +154,7 @@ mock_wafv2 = lazy_load(".wafv2", "mock_wafv2")
 # logging.getLogger('boto').setLevel(logging.CRITICAL)
 
 __title__ = "moto"
-__version__ = "2.2.7.dev"
+__version__ = "2.2.8.dev"
 
 
 try:
