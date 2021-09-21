5b10671af41f9217fac4a353514d551dd4f96987
Bert Blommers <bblommers@users.noreply.github.com>
2021-09-21 22:00:20 +0000

    Rewrite deprecated SWF tests (#3891)

--- tests/test_swf/responses/test_workflow_executions.py
+++ tests/test_swf/responses/test_workflow_executions.py
@@ -1,10 +1,13 @@
 import boto
+import boto3
 from boto.swf.exceptions import SWFResponseError
+from botocore.exceptions import ClientError
 from datetime import datetime, timedelta
 
+import pytest
 import sure  # noqa
 
-from moto import mock_swf_deprecated
+from moto import mock_swf_deprecated, mock_swf
 from moto.core.utils import unix_time
 
 
@@ -26,7 +29,30 @@ def setup_swf_environment():
     return conn
 
 
+def setup_swf_environment_boto3():
+    client = boto3.client("swf", region_name="us-west-1")
+    client.register_domain(
+        name="test-domain",
+        workflowExecutionRetentionPeriodInDays="60",
+        description="A test domain",
+    )
+    client.register_workflow_type(
+        domain="test-domain",
+        name="test-workflow",
+        version="v1.0",
+        defaultTaskList={"name": "queue"},
+        defaultChildPolicy="TERMINATE",
+        defaultTaskStartToCloseTimeout="300",
+        defaultExecutionStartToCloseTimeout="300",
+    )
+    client.register_activity_type(
+        domain="test-domain", name="test-activity", version="v1.1"
+    )
+    return client
+
+
 # StartWorkflowExecution endpoint
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_start_workflow_execution():
     conn = setup_swf_environment()
@@ -37,6 +63,20 @@ def test_start_workflow_execution():
     wf.should.contain("runId")
 
 
+# StartWorkflowExecution endpoint
+@mock_swf
+def test_start_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+
+    wf = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    wf.should.have.key("runId")
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_signal_workflow_execution():
     conn = setup_swf_environment()
@@ -54,6 +94,32 @@ def test_signal_workflow_execution():
     wfe["openCounts"]["openDecisionTasks"].should.equal(2)
 
 
+@mock_swf
+def test_signal_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+    hsh = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    run_id = hsh["runId"]
+
+    wfe = client.signal_workflow_execution(
+        domain="test-domain",
+        signalName="my_signal",
+        workflowId="uid-abcd1234",
+        input="my_input",
+        runId=run_id,
+    )
+
+    wfe = client.describe_workflow_execution(
+        domain="test-domain", execution={"runId": run_id, "workflowId": "uid-abcd1234"}
+    )
+
+    wfe["openCounts"]["openDecisionTasks"].should.equal(2)
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_start_already_started_workflow_execution():
     conn = setup_swf_environment()
@@ -66,6 +132,29 @@ def test_start_already_started_workflow_execution():
     ).should.throw(SWFResponseError)
 
 
+@mock_swf
+def test_start_already_started_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+    client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+
+    with pytest.raises(ClientError) as ex:
+        client.start_workflow_execution(
+            domain="test-domain",
+            workflowId="uid-abcd1234",
+            workflowType={"name": "test-workflow", "version": "v1.0"},
+        )
+    ex.value.response["Error"]["Code"].should.equal(
+        "WorkflowExecutionAlreadyStartedFault"
+    )
+    ex.value.response["Error"]["Message"].should.equal("Already Started")
+    ex.value.response["ResponseMetadata"]["HTTPStatusCode"].should.equal(400)
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_start_workflow_execution_on_deprecated_type():
     conn = setup_swf_environment()
@@ -76,7 +165,28 @@ def test_start_workflow_execution_on_deprecated_type():
     ).should.throw(SWFResponseError)
 
 
+@mock_swf
+def test_start_workflow_execution_on_deprecated_type_boto3():
+    client = setup_swf_environment_boto3()
+    client.deprecate_workflow_type(
+        domain="test-domain", workflowType={"name": "test-workflow", "version": "v1.0"}
+    )
+
+    with pytest.raises(ClientError) as ex:
+        client.start_workflow_execution(
+            domain="test-domain",
+            workflowId="uid-abcd1234",
+            workflowType={"name": "test-workflow", "version": "v1.0"},
+        )
+    ex.value.response["Error"]["Code"].should.equal("TypeDeprecatedFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "WorkflowType=[name=test-workflow, version=v1.0]"
+    )
+    ex.value.response["ResponseMetadata"]["HTTPStatusCode"].should.equal(400)
+
+
 # DescribeWorkflowExecution endpoint
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_describe_workflow_execution():
     conn = setup_swf_environment()
@@ -90,6 +200,25 @@ def test_describe_workflow_execution():
     wfe["executionInfo"]["executionStatus"].should.equal("OPEN")
 
 
+# DescribeWorkflowExecution endpoint
+@mock_swf
+def test_describe_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+    hsh = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    run_id = hsh["runId"]
+
+    wfe = client.describe_workflow_execution(
+        domain="test-domain", execution={"runId": run_id, "workflowId": "uid-abcd1234"}
+    )
+    wfe["executionInfo"]["execution"]["workflowId"].should.equal("uid-abcd1234")
+    wfe["executionInfo"]["executionStatus"].should.equal("OPEN")
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_describe_non_existent_workflow_execution():
     conn = setup_swf_environment()
@@ -99,7 +228,24 @@ def test_describe_non_existent_workflow_execution():
     ).should.throw(SWFResponseError)
 
 
+@mock_swf
+def test_describe_non_existent_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+
+    with pytest.raises(ClientError) as ex:
+        client.describe_workflow_execution(
+            domain="test-domain",
+            execution={"runId": "wrong-run-id", "workflowId": "uid-abcd1234"},
+        )
+    ex.value.response["Error"]["Code"].should.equal("UnknownResourceFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "Unknown execution: WorkflowExecution=[workflowId=uid-abcd1234, runId=wrong-run-id]"
+    )
+    ex.value.response["ResponseMetadata"]["HTTPStatusCode"].should.equal(400)
+
+
 # GetWorkflowExecutionHistory endpoint
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_get_workflow_execution_history():
     conn = setup_swf_environment()
@@ -113,6 +259,25 @@ def test_get_workflow_execution_history():
     types.should.equal(["WorkflowExecutionStarted", "DecisionTaskScheduled"])
 
 
+# GetWorkflowExecutionHistory endpoint
+@mock_swf
+def test_get_workflow_execution_history_boto3():
+    client = setup_swf_environment_boto3()
+    hsh = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    run_id = hsh["runId"]
+
+    resp = client.get_workflow_execution_history(
+        domain="test-domain", execution={"runId": run_id, "workflowId": "uid-abcd1234"},
+    )
+    types = [evt["eventType"] for evt in resp["events"]]
+    types.should.equal(["WorkflowExecutionStarted", "DecisionTaskScheduled"])
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_get_workflow_execution_history_with_reverse_order():
     conn = setup_swf_environment()
@@ -128,6 +293,26 @@ def test_get_workflow_execution_history_with_reverse_order():
     types.should.equal(["DecisionTaskScheduled", "WorkflowExecutionStarted"])
 
 
+@mock_swf
+def test_get_workflow_execution_history_with_reverse_order_boto3():
+    client = setup_swf_environment_boto3()
+    hsh = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    run_id = hsh["runId"]
+
+    resp = client.get_workflow_execution_history(
+        domain="test-domain",
+        execution={"runId": run_id, "workflowId": "uid-abcd1234"},
+        reverseOrder=True,
+    )
+    types = [evt["eventType"] for evt in resp["events"]]
+    types.should.equal(["DecisionTaskScheduled", "WorkflowExecutionStarted"])
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_get_workflow_execution_history_on_non_existent_workflow_execution():
     conn = setup_swf_environment()
@@ -137,7 +322,24 @@ def test_get_workflow_execution_history_on_non_existent_workflow_execution():
     ).should.throw(SWFResponseError)
 
 
+@mock_swf
+def test_get_workflow_execution_history_on_non_existent_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+
+    with pytest.raises(ClientError) as ex:
+        client.get_workflow_execution_history(
+            domain="test-domain",
+            execution={"runId": "wrong-run-id", "workflowId": "wrong-workflow-id"},
+        )
+    ex.value.response["Error"]["Code"].should.equal("UnknownResourceFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "Unknown execution: WorkflowExecution=[workflowId=wrong-workflow-id, runId=wrong-run-id]"
+    )
+    ex.value.response["ResponseMetadata"]["HTTPStatusCode"].should.equal(400)
+
+
 # ListOpenWorkflowExecutions endpoint
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_list_open_workflow_executions():
     conn = setup_swf_environment()
@@ -175,7 +377,52 @@ def test_list_open_workflow_executions():
     open_workflow["executionStatus"].should.equal("OPEN")
 
 
+# ListOpenWorkflowExecutions endpoint
+@mock_swf
+def test_list_open_workflow_executions_boto3():
+    client = setup_swf_environment_boto3()
+    # One open workflow execution
+    client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    # One closed workflow execution to make sure it isn't displayed
+    run_id = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd12345",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )["runId"]
+    client.terminate_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd12345",
+        details="some details",
+        reason="a more complete reason",
+        runId=run_id,
+    )
+
+    yesterday = datetime.utcnow() - timedelta(days=1)
+    oldest_date = unix_time(yesterday)
+    response = client.list_open_workflow_executions(
+        domain="test-domain",
+        startTimeFilter={"oldestDate": oldest_date},
+        executionFilter={"workflowId": "test-workflow"},
+    )
+    execution_infos = response["executionInfos"]
+    len(execution_infos).should.equal(1)
+    open_workflow = execution_infos[0]
+    open_workflow["workflowType"].should.equal(
+        {"version": "v1.0", "name": "test-workflow"}
+    )
+    open_workflow.should.contain("startTimestamp")
+    open_workflow["execution"]["workflowId"].should.equal("uid-abcd1234")
+    open_workflow["execution"].should.contain("runId")
+    open_workflow["cancelRequested"].should.be(False)
+    open_workflow["executionStatus"].should.equal("OPEN")
+
+
 # ListClosedWorkflowExecutions endpoint
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_list_closed_workflow_executions():
     conn = setup_swf_environment()
@@ -213,7 +460,52 @@ def test_list_closed_workflow_executions():
     open_workflow["executionStatus"].should.equal("CLOSED")
 
 
+# ListClosedWorkflowExecutions endpoint
+@mock_swf
+def test_list_closed_workflow_executions_boto3():
+    client = setup_swf_environment_boto3()
+    # Leave one workflow execution open to make sure it isn't displayed
+    client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )
+    # One closed workflow execution
+    run_id = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd12345",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )["runId"]
+    client.terminate_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd12345",
+        details="some details",
+        reason="a more complete reason",
+        runId=run_id,
+    )
+
+    yesterday = datetime.utcnow() - timedelta(days=1)
+    oldest_date = unix_time(yesterday)
+    response = client.list_closed_workflow_executions(
+        domain="test-domain",
+        startTimeFilter={"oldestDate": oldest_date},
+        executionFilter={"workflowId": "test-workflow"},
+    )
+    execution_infos = response["executionInfos"]
+    len(execution_infos).should.equal(1)
+    open_workflow = execution_infos[0]
+    open_workflow["workflowType"].should.equal(
+        {"version": "v1.0", "name": "test-workflow"}
+    )
+    open_workflow.should.contain("startTimestamp")
+    open_workflow["execution"]["workflowId"].should.equal("uid-abcd12345")
+    open_workflow["execution"].should.contain("runId")
+    open_workflow["cancelRequested"].should.be(False)
+    open_workflow["executionStatus"].should.equal("CLOSED")
+
+
 # TerminateWorkflowExecution endpoint
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_terminate_workflow_execution():
     conn = setup_swf_environment()
@@ -239,6 +531,36 @@ def test_terminate_workflow_execution():
     attrs["cause"].should.equal("OPERATOR_INITIATED")
 
 
+# TerminateWorkflowExecution endpoint
+@mock_swf
+def test_terminate_workflow_execution_boto3():
+    client = setup_swf_environment_boto3()
+    run_id = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )["runId"]
+
+    client.terminate_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        details="some details",
+        reason="a more complete reason",
+        runId=run_id,
+    )
+
+    resp = client.get_workflow_execution_history(
+        domain="test-domain", execution={"runId": run_id, "workflowId": "uid-abcd1234"},
+    )
+    evt = resp["events"][-1]
+    evt["eventType"].should.equal("WorkflowExecutionTerminated")
+    attrs = evt["workflowExecutionTerminatedEventAttributes"]
+    attrs["details"].should.equal("some details")
+    attrs["reason"].should.equal("a more complete reason")
+    attrs["cause"].should.equal("OPERATOR_INITIATED")
+
+
+# Has boto3 equivalent
 @mock_swf_deprecated
 def test_terminate_workflow_execution_with_wrong_workflow_or_run_id():
     conn = setup_swf_environment()
@@ -271,4 +593,59 @@ def test_terminate_workflow_execution_with_wrong_workflow_or_run_id():
         "test-domain", "uid-abcd1234", run_id="foo"
     ).should.throw(
         SWFResponseError, "WorkflowExecution=[workflowId=uid-abcd1234, runId="
+    )
+
+
+@mock_swf
+def test_terminate_workflow_execution_with_wrong_workflow_or_run_id_boto3():
+    client = setup_swf_environment_boto3()
+    run_id = client.start_workflow_execution(
+        domain="test-domain",
+        workflowId="uid-abcd1234",
+        workflowType={"name": "test-workflow", "version": "v1.0"},
+    )["runId"]
+
+    # terminate workflow execution
+    client.terminate_workflow_execution(domain="test-domain", workflowId="uid-abcd1234")
+
+    # already closed, with run_id
+    with pytest.raises(ClientError) as ex:
+        client.terminate_workflow_execution(
+            domain="test-domain", workflowId="uid-abcd1234", runId=run_id
+        )
+    ex.value.response["Error"]["Code"].should.equal("UnknownResourceFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "Unknown execution: WorkflowExecution=[workflowId=uid-abcd1234, runId={}]".format(
+            run_id
+        )
+    )
+
+    # already closed, without run_id
+    with pytest.raises(ClientError) as ex:
+        client.terminate_workflow_execution(
+            domain="test-domain", workflowId="uid-abcd1234"
+        )
+    ex.value.response["Error"]["Code"].should.equal("UnknownResourceFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "Unknown execution, workflowId = uid-abcd1234"
+    )
+
+    # wrong workflow id
+    with pytest.raises(ClientError) as ex:
+        client.terminate_workflow_execution(
+            domain="test-domain", workflowId="uid-non-existent"
+        )
+    ex.value.response["Error"]["Code"].should.equal("UnknownResourceFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "Unknown execution, workflowId = uid-non-existent"
+    )
+
+    # wrong run_id
+    with pytest.raises(ClientError) as ex:
+        client.terminate_workflow_execution(
+            domain="test-domain", workflowId="uid-abcd1234", runId="foo"
+        )
+    ex.value.response["Error"]["Code"].should.equal("UnknownResourceFault")
+    ex.value.response["Error"]["Message"].should.equal(
+        "Unknown execution: WorkflowExecution=[workflowId=uid-abcd1234, runId=foo]"
     )
