e2d1375f0f924ad18d5d8b06c562baf9c4a82676
Daniel Roschka <daniel.roschka@yoma-solutions.de>
2021-11-24 10:57:47 +0100

    Loosen version constraint for ecdsa
    This loosens the version constraint for ecdsa the same way as
    python-jose does in current versions to allow the usage of newer ecdsa
    versions.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 _dep_PyYAML = "PyYAML>=5.1"
 _dep_python_jose = "python-jose[cryptography]>=3.1.0,<4.0.0"
 _dep_python_jose_ecdsa_pin = (
-    "ecdsa<0.15"  # https://github.com/spulec/moto/pull/3263#discussion_r477404984
+    "ecdsa!=0.15"  # https://github.com/spulec/moto/pull/3263#discussion_r477404984
 )
 _dep_dataclasses = "dataclasses; python_version < '3.7'"
 _dep_docker = "docker>=2.5.1"
