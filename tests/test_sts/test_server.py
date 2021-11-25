a2c6e00e229357447fee22a11828fc94cf2cdc5a
Chris Evett <chris.evett@gmail.com>
2021-11-25 05:03:57 -0500

    fix: STS xml was malformed (#4637)

--- tests/test_sts/test_server.py
+++ tests/test_sts/test_server.py
@@ -36,3 +36,15 @@ def test_sts_get_caller_identity():
     res.data.should.contain(b"Arn")
     res.data.should.contain(b"UserId")
     res.data.should.contain(b"Account")
+
+
+def test_sts_wellformed_xml():
+    backend = server.create_backend_app("sts")
+    test_client = backend.test_client()
+
+    res = test_client.get("/?Action=GetFederationToken&Name=Bob")
+    res.data.should_not.contain(b"\n")
+    res = test_client.get("/?Action=GetSessionToken")
+    res.data.should_not.contain(b"\n")
+    res = test_client.get("/?Action=GetCallerIdentity")
+    res.data.should_not.contain(b"\n")
