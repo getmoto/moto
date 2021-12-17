ffcc88d8148aae074b7c8b0d37c9cf05fdf5c495
Bert Blommers <info@bertblommers.nl>
2021-12-17 11:13:20 -0100

    Skip tests in ServerMode

--- tests/test_core/test_mock_regions.py
+++ tests/test_core/test_mock_regions.py
@@ -2,11 +2,14 @@ import boto3
 import mock
 import os
 import pytest
-from moto import mock_sns
+from moto import mock_sns, settings
+from unittest import SkipTest
 
 
 @mock_sns
 def test_use_invalid_region():
+    if settings.TEST_SERVER_MODE:
+        raise SkipTest("ServerMode will throw different errors")
     client = boto3.client("sns", region_name="any-region")
     with pytest.raises(KeyError) as exc:
         client.list_platform_applications()
@@ -23,6 +26,8 @@ def test_use_region_from_env():
 @mock_sns
 @mock.patch.dict(os.environ, {"AWS_DEFAULT_REGION": "any-region"})
 def test_use_unknown_region_from_env():
+    if settings.TEST_SERVER_MODE:
+        raise SkipTest("Cannot set environemnt variables in ServerMode")
     client = boto3.client("sns")
     with pytest.raises(KeyError) as exc:
         client.list_platform_applications()
@@ -33,5 +38,7 @@ def test_use_unknown_region_from_env():
 @mock.patch.dict(os.environ, {"AWS_DEFAULT_REGION": "any-region"})
 @mock.patch.dict(os.environ, {"MOTO_ALLOW_NONEXISTENT_REGION": "trUe"})
 def test_use_unknown_region_from_env_but_allow_it():
+    if settings.TEST_SERVER_MODE:
+        raise SkipTest("Cannot set environemnt variables in ServerMode")
     client = boto3.client("sns")
     client.list_platform_applications()["PlatformApplications"].should.equal([])
