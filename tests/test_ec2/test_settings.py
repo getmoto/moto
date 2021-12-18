53fb17d8ea9776d499b4258526ea3286a2f18ea8
Simon Toftegaard Petersen <38880853+SimonToftegaard@users.noreply.github.com>
2021-12-18 17:05:36 +0100

    Added missing region for boto client in test

--- tests/test_ec2/test_settings.py
+++ tests/test_ec2/test_settings.py
@@ -10,20 +10,20 @@ logger = logging.getLogger(__name__)
 
 @mock_ec2
 def test_disable_ebs_encryption_by_default():
-    ec2 = boto3.client("ec2")
+    ec2 = boto3.client("ec2", "eu-central-1")
 
     ec2.enable_ebs_encryption_by_default()
     response = ec2.get_ebs_encryption_by_default()
     response.should.have.key("EbsEncryptionByDefault").equal(True)
 
     ec2.disable_ebs_encryption_by_default()
-    after_disable_response = boto3.client("ec2").get_ebs_encryption_by_default()
+    after_disable_response = ec2.get_ebs_encryption_by_default()
     after_disable_response.should.have.key("EbsEncryptionByDefault").equal(False)
 
 
 @mock_ec2
 def test_enable_ebs_encryption_by_default():
-    ec2 = boto3.client("ec2")
+    ec2 = boto3.client("ec2", region_name="eu-central-1")
     response = ec2.enable_ebs_encryption_by_default()
 
     ec2.get_ebs_encryption_by_default()
@@ -32,7 +32,7 @@ def test_enable_ebs_encryption_by_default():
 
 @mock_ec2
 def test_get_ebs_encryption_by_default():
-    ec2 = boto3.client("ec2")
+    ec2 = boto3.client("ec2", region_name="eu-central-1")
 
     response = ec2.get_ebs_encryption_by_default()
     response.should.have.key("EbsEncryptionByDefault").equal(False)
@@ -40,12 +40,12 @@ def test_get_ebs_encryption_by_default():
 
 @mock_ec2
 def test_enable_ebs_encryption_by_default_region():
-    ec2_eu = boto3.client("ec2", "eu-central-1")
+    ec2_eu = boto3.client("ec2", region_name="eu-central-1")
     ec2_eu.enable_ebs_encryption_by_default()
 
     response = ec2_eu.get_ebs_encryption_by_default()
     response.should.have.key("EbsEncryptionByDefault").equal(True)
 
-    ec2_us = boto3.client("ec2", "us-east-1")
+    ec2_us = boto3.client("ec2", region_name="us-east-1")
     response = ec2_us.get_ebs_encryption_by_default()
     response.should.have.key("EbsEncryptionByDefault").equal(False)
