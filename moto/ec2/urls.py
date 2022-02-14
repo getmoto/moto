e9798a325139f62da81c44b12f6e486b8851890f
Bert Blommers <info@bertblommers.nl>
2022-02-14 21:03:30 -0100

    Undo EC2 change - fix APIGateway bug

--- moto/ec2/urls.py
+++ moto/ec2/urls.py
@@ -3,5 +3,4 @@ from .responses import EC2Response
 
 url_bases = [r"https?://ec2\.(.+)\.amazonaws\.com(|\.cn)"]
 
-response = EC2Response()
-url_paths = {"{0}/": response._dispatch}
+url_paths = {"{0}/": EC2Response.dispatch}
