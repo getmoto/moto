3fca2c3b3126d18d07d743cb47feaccf264ab43e
Erik Lilja <erik.lilja@funnel.io>
2022-03-02 13:00:27 +0100

    [sns] get default region by env var

--- moto/sns/__init__.py
+++ moto/sns/__init__.py
@@ -1,5 +1,7 @@
+import os
 from .models import sns_backends
 from ..core.models import base_decorator
 
-sns_backend = sns_backends["us-east-1"]
+region_name = os.environ.get("AWS_DEFAULT_REGION", "us-east-1")
+sns_backend = sns_backends[region_name]
 mock_sns = base_decorator(sns_backends)
