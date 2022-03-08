4f9522623592825bf048e44d3fdbd84b729d6d9e
Bert Blommers <info@bertblommers.nl>
2022-03-08 12:34:51 -0100

    Add Server test

--- tests/test_rds/test_server.py
+++ tests/test_rds/test_server.py
@@ -1,15 +1,11 @@
+import moto.server as server
 import sure  # noqa # pylint: disable=unused-import
 
-"""
-Test the different server responses
-"""
 
+def test_list_databases():
+    backend = server.create_backend_app("rds")
+    test_client = backend.test_client()
 
-# @mock_rds
-# def test_list_databases():
-#    backend = server.create_backend_app("rds")
-#    test_client = backend.test_client()
-#
-#    res = test_client.get('/?Action=DescribeDBInstances')
-#
-#    res.data.decode("utf-8").should.contain("<DescribeDBInstancesResult>")
+    res = test_client.get("/?Action=DescribeDBInstances")
+
+    res.data.decode("utf-8").should.contain("<DescribeDBInstancesResult>")
