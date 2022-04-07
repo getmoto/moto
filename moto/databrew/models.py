2a6ba0ddd1c90b4cc95e4f25873f904f7c010072
John-Paul Stanford <jp@yagro.com>
2022-04-07 10:45:17 +0100

    New methods to the databrew service for recipes and rulesets. (#4996)

--- moto/databrew/models.py
+++ moto/databrew/models.py
@@ -5,6 +5,7 @@ from moto.core import BaseBackend, BaseModel
 from moto.core.utils import BackendDict
 from moto.utilities.paginator import paginate
 from .exceptions import RecipeAlreadyExistsException, RecipeNotFoundException
+from .exceptions import RulesetAlreadyExistsException, RulesetNotFoundException
 
 
 class DataBrewBackend(BaseBackend):
@@ -15,11 +16,18 @@ class DataBrewBackend(BaseBackend):
             "limit_default": 100,
             "unique_attribute": "name",
         },
+        "list_rulesets": {
+            "input_token": "next_token",
+            "limit_key": "max_results",
+            "limit_default": 100,
+            "unique_attribute": "name",
+        },
     }
 
     def __init__(self, region_name):
         self.region_name = region_name
         self.recipes = OrderedDict()
+        self.rulesets = OrderedDict()
 
     def reset(self):
         """Re-initialize all attributes for this instance."""
@@ -37,6 +45,20 @@ class DataBrewBackend(BaseBackend):
         self.recipes[recipe_name] = recipe
         return recipe
 
+    def update_recipe(self, recipe_name, recipe_description, recipe_steps, tags):
+        if recipe_name not in self.recipes:
+            raise RecipeNotFoundException(recipe_name)
+
+        recipe = self.recipes[recipe_name]
+        if recipe_description is not None:
+            recipe.description = recipe_description
+        if recipe_steps is not None:
+            recipe.steps = recipe_steps
+        if tags is not None:
+            recipe.tags = tags
+
+        return recipe
+
     @paginate(pagination_model=PAGINATION_MODEL)
     def list_recipes(self):
         return [self.recipes[key] for key in self.recipes] if self.recipes else []
@@ -48,6 +70,52 @@ class DataBrewBackend(BaseBackend):
         if recipe_name not in self.recipes:
             raise RecipeNotFoundException(recipe_name)
         return self.recipes[recipe_name]
+
+    def create_ruleset(
+        self, ruleset_name, ruleset_description, ruleset_rules, ruleset_target_arn, tags
+    ):
+        if ruleset_name in self.rulesets:
+            raise RulesetAlreadyExistsException()
+
+        ruleset = FakeRuleset(
+            self.region_name,
+            ruleset_name,
+            ruleset_description,
+            ruleset_rules,
+            ruleset_target_arn,
+            tags,
+        )
+        self.rulesets[ruleset_name] = ruleset
+        return ruleset
+
+    def update_ruleset(self, ruleset_name, ruleset_description, ruleset_rules, tags):
+        if ruleset_name not in self.rulesets:
+            raise RulesetNotFoundException(ruleset_name)
+
+        ruleset = self.rulesets[ruleset_name]
+        if ruleset_description is not None:
+            ruleset.description = ruleset_description
+        if ruleset_rules is not None:
+            ruleset.rules = ruleset_rules
+        if tags is not None:
+            ruleset.tags = tags
+
+        return ruleset
+
+    def get_ruleset(self, ruleset_name):
+        if ruleset_name not in self.rulesets:
+            raise RulesetNotFoundException(ruleset_name)
+        return self.rulesets[ruleset_name]
+
+    @paginate(pagination_model=PAGINATION_MODEL)
+    def list_rulesets(self):
+        return list(self.rulesets.values())
+
+    def delete_ruleset(self, ruleset_name):
+        if ruleset_name not in self.rulesets:
+            raise RulesetNotFoundException(ruleset_name)
+
+        del self.rulesets[ruleset_name]
 
 
 class FakeRecipe(BaseModel):
@@ -66,6 +134,36 @@ class FakeRecipe(BaseModel):
             "Name": self.name,
             "Steps": self.steps,
             "Description": self.description,
+            "CreateTime": self.created_time.isoformat(),
+            "Tags": self.tags or dict(),
+        }
+
+
+class FakeRuleset(BaseModel):
+    def __init__(
+        self,
+        region_name,
+        ruleset_name,
+        ruleset_description,
+        ruleset_rules,
+        ruleset_target_arn,
+        tags,
+    ):
+        self.region_name = region_name
+        self.name = ruleset_name
+        self.description = ruleset_description
+        self.rules = ruleset_rules
+        self.target_arn = ruleset_target_arn
+        self.created_time = datetime.now()
+
+        self.tags = tags
+
+    def as_dict(self):
+        return {
+            "Name": self.name,
+            "Rules": self.rules,
+            "Description": self.description,
+            "TargetArn": self.target_arn,
             "CreateTime": self.created_time.isoformat(),
             "Tags": self.tags or dict(),
         }
