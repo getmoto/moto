300f4ce00fd44d61a6f7f58ad366e42e62f61d47
Robert Schmidtke <robert.schmidtke@trailstonegroup.com>
2022-04-08 09:34:04 +0200

    test NOT operations

--- tests/test_glue/test_datacatalog.py
+++ tests/test_glue/test_datacatalog.py
@@ -452,6 +452,9 @@ def test_get_partitions_expression_int_column():
         "int_col > 1 AND int_col < 3",
         "int_col >= 2 and int_col <> 3",
         "(int_col) = ((2)) (OR) (((int_col))) = (2)",
+        "int_col IS NOT NULL and int_col = 2",
+        "int_col not IN (1, 3)",
+        "int_col NOT BETWEEN 1 AND 1 and int_col NOT BETWEEN 3 AND 3",
     )
 
     for expression in int_col_is_two_expressions:
