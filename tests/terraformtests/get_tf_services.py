32f327658d1e985b26b5ffe7dd929ad30f007223
Bert Blommers <info@bertblommers.nl>
2022-04-13 22:29:01 +0000

    Run all tests

--- tests/terraformtests/get_tf_services.py
+++ tests/terraformtests/get_tf_services.py
@@ -2,4 +2,4 @@ import yaml
 
 with open("tests/terraformtests/terraform-tests.success.txt") as f:
     dct = yaml.load(f, Loader=yaml.FullLoader)
-    print(list(dct.keys())[0:2])
+    print(list(dct.keys()))
