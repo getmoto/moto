b7c539eb873813f4b1a84a71b05c2a885629cbff
Nick Brown <nick.phbrown@gmail.com>
2022-04-27 23:50:40 +0100

    mediaconnect: reverse dodgy changes

--- tests/test_mediaconnect/test_mediaconnect.py
+++ tests/test_mediaconnect/test_mediaconnect.py
@@ -223,9 +223,7 @@ def test_add_flow_vpc_interfaces_fails():
         client.add_flow_vpc_interfaces(FlowArn=flow_arn, VpcInterfaces=[])
     err = err.value.response["Error"]
     err["Code"].should.equal("NotFoundException")
-    err["Message"].should.equal(
-        "flow with arn=unknown-flow not found".format(str(flow_arn))
-    )
+    err["Message"].should.equal("flow with arn=unknown-flow not found")
 
 
 @mock_mediaconnect
@@ -270,9 +268,7 @@ def test_remove_flow_vpc_interface_fails():
         )
     err = err.value.response["Error"]
     err["Code"].should.equal("NotFoundException")
-    err["Message"].should.equal(
-        "flow with arn=unknown-flow not found".format(str(flow_arn))
-    )
+    err["Message"].should.equal("flow with arn=unknown-flow not found")
 
 
 @mock_mediaconnect
@@ -307,9 +303,7 @@ def test_add_flow_outputs_fails():
         client.add_flow_outputs(FlowArn=flow_arn, Outputs=[])
     err = err.value.response["Error"]
     err["Code"].should.equal("NotFoundException")
-    err["Message"].should.equal(
-        "flow with arn=unknown-flow not found".format(str(flow_arn))
-    )
+    err["Message"].should.equal("flow with arn=unknown-flow not found")
 
 
 @mock_mediaconnect
@@ -321,9 +315,7 @@ def test_remove_flow_output_fails():
         client.remove_flow_output(FlowArn=flow_arn, OutputArn=output_arn)
     err = err.value.response["Error"]
     err["Code"].should.equal("NotFoundException")
-    err["Message"].should.equal(
-        "flow with arn=unknown-flow not found".format(str(flow_arn))
-    )
+    err["Message"].should.equal("flow with arn=unknown-flow not found")
 
 
 @mock_mediaconnect
