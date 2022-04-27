d13021c5326b301fd1864ffd8d4de058d9ea4b7a
tzven0 <36624215+tzven0@users.noreply.github.com>
2022-04-27 13:09:07 +0200

    black formatting

--- moto/cloudwatch/models.py
+++ moto/cloudwatch/models.py
@@ -502,7 +502,8 @@ class CloudWatchBackend(BaseBackend):
                     query_period_data = [
                         md
                         for md in period_md
-                        if sorted(md.dimensions) == sorted(dimensions) and md.name == query_name
+                        if sorted(md.dimensions) == sorted(dimensions)
+                        and md.name == query_name
                     ]
 
                 metric_values = [m.value for m in query_period_data]
