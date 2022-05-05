a305a2547185f02439d6694209f6eb1def1da429
Matt Brzezinski <matt.brzezinski@invenia.ca>
2022-05-05 16:18:48 -0500

    RDS Filtering on db-cluster-id

--- moto/rds/responses.py
+++ moto/rds/responses.py
@@ -19,6 +19,7 @@ class RDSResponse(BaseResponse):
             "backup_retention_period": self._get_param("BackupRetentionPeriod"),
             "copy_tags_to_snapshot": self._get_param("CopyTagsToSnapshot"),
             "db_instance_class": self._get_param("DBInstanceClass"),
+            "db_cluster_identifier": self._get_param("DBClusterIdentifier"),
             "db_instance_identifier": self._get_param("DBInstanceIdentifier"),
             "db_name": self._get_param("DBName"),
             "db_parameter_group_name": self._get_param("DBParameterGroupName"),
