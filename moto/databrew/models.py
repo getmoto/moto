8b4d6760b452f70e54a531146994e6c218fe03e2
Steve Church <sc@yagro.com>
2022-05-10 09:09:10 +0100

    run black

--- moto/databrew/models.py
+++ moto/databrew/models.py
@@ -13,7 +13,7 @@ from .exceptions import (
     ValidationException,
     RulesetAlreadyExistsException,
     RulesetNotFoundException,
-    ResourceNotFoundException
+    ResourceNotFoundException,
 )
 
 
