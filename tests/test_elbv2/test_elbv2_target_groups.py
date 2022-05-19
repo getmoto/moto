5aace90385082429740937d205f1a1b92ca4d4d9
Adam <radam9@gmail.com>
2022-05-19 19:35:38 +0200

    ELBv2:create_target_group: fix template variable typo (#5147)

--- tests/test_elbv2/test_elbv2_target_groups.py
+++ tests/test_elbv2/test_elbv2_target_groups.py
@@ -96,7 +96,9 @@ def test_create_target_group_and_listeners():
         UnhealthyThresholdCount=2,
         Matcher={"HttpCode": "200"},
     )
-    target_group_arn = response.get("TargetGroups")[0]["TargetGroupArn"]
+    target_group = response.get("TargetGroups")[0]
+    target_group_arn = target_group.get("TargetGroupArn")
+    target_group.get("HealthCheckProtocol").should.equal("HTTP")
 
     # Check it's in the describe_target_groups response
     response = conn.describe_target_groups()
