e0036483cc0c1ed9bd6de62d6946eb80525ebf3b
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2022-06-01 17:00:51 +1000

    Use fixtures to reduce test duplication

--- tests/test_kms/test_model.py
+++ tests/test_kms/test_model.py
@@ -1,70 +1,52 @@
+import pytest
 import sure  # noqa # pylint: disable=unused-import
 
 from moto.kms.models import KmsBackend
 
+PLAINTEXT = b"text"
+REGION = "us-east-1"
 
-def test_encrypt_key_id():
-    region = "us-east-1"
-    backend = KmsBackend(region)
-    # Create key
-    key = backend.create_key(
-        None, "ENCRYPT_DECRYPT", "SYMMETRIC_DEFAULT", "Test key", None, region
+
+@pytest.fixture
+def backend():
+    return KmsBackend(REGION)
+
+
+@pytest.fixture
+def key(backend):
+    return backend.create_key(
+        None, "ENCRYPT_DECRYPT", "SYMMETRIC_DEFAULT", "Test key", None, REGION
     )
-    # Create alias
-    backend.add_alias(key.id, "alias/test/test")
 
-    # Test encryption
-    ciphertext, arn = backend.encrypt(key.id, b"text", {})
+
+def test_encrypt_key_id(backend, key):
+    ciphertext, arn = backend.encrypt(key.id, PLAINTEXT, {})
 
     ciphertext.shouldnt.be.none
     arn.shouldnt.be.none
 
 
-def test_encrypt_key_arn():
-    region = "us-east-1"
-    backend = KmsBackend(region)
-    # Create key
-    key = backend.create_key(
-        None, "ENCRYPT_DECRYPT", "SYMMETRIC_DEFAULT", "Test key", None, region
-    )
-
-    # Test encryption
-    ciphertext, arn = backend.encrypt(key.arn, b"text", {})
+def test_encrypt_key_arn(backend, key):
+    ciphertext, arn = backend.encrypt(key.arn, PLAINTEXT, {})
 
     ciphertext.shouldnt.be.none
     arn.shouldnt.be.none
 
 
-def test_encrypt_alias_name():
-    region = "us-east-1"
-    backend = KmsBackend(region)
-    # Create key
-    key = backend.create_key(
-        None, "ENCRYPT_DECRYPT", "SYMMETRIC_DEFAULT", "Test key", None, region
-    )
-    # Create alias
+def test_encrypt_alias_name(backend, key):
     backend.add_alias(key.id, "alias/test/test")
 
-    # Test encryption
-    ciphertext, arn = backend.encrypt("alias/test/test", b"text", {})
+    ciphertext, arn = backend.encrypt("alias/test/test", PLAINTEXT, {})
 
     ciphertext.shouldnt.be.none
     arn.shouldnt.be.none
 
 
-def test_encrypt_alias_arn():
-    region = "us-east-1"
-    backend = KmsBackend(region)
-    # Create key
-    key = backend.create_key(
-        None, "ENCRYPT_DECRYPT", "SYMMETRIC_DEFAULT", "Test key", None, region
-    )
-    # Create alias
+def test_encrypt_alias_arn(backend, key):
     backend.add_alias(key.id, "alias/test/test")
 
-    # Test encryption
     ciphertext, arn = backend.encrypt(
-        "arn:aws:kms::000000000000:alias/test/test", b"text", {}
+        "arn:aws:kms::000000000000:alias/test/test", PLAINTEXT, {}
     )
 
     ciphertext.shouldnt.be.none
