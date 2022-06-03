030ebb06326e66eb597c815c143295526d84b835
Tomoya Iwata <iwata.tomoya@classmethod.jp>
2022-06-03 09:18:28 +0900

    add greengrass create_core_definition_version test

--- tests/test_greengrass/test_greengrass_core.py
+++ tests/test_greengrass/test_greengrass_core.py
@@ -31,3 +31,42 @@ def test_create_core_definition():
     res.should.have.key("LatestVersionArn")
     res.should.have.key("Name").equals(core_name)
     res["ResponseMetadata"]["HTTPStatusCode"].should.equal(201)
+
+
+@freezegun.freeze_time("2022-06-01 12:00:00")
+@mock_greengrass
+def test_create_core_definition_version():
+
+    client = boto3.client("greengrass", region_name="ap-northeast-1")
+    v1_cores = [
+        {
+            "CertificateArn": f"arn:aws:iot:ap-northeast-1:{ACCOUNT_ID}:cert/36ed61be9c6271ae8da174e29d0e033c06af149d7b21672f3800fe322044554d",
+            "Id": "123456789",
+            "ThingArn": f"arn:aws:iot:ap-northeast-1:{ACCOUNT_ID}:thing/v1Thing",
+        }
+    ]
+
+    initial_version = {"Cores": v1_cores}
+
+    core_def_res = client.create_core_definition(
+        InitialVersion=initial_version, Name="TestCore"
+    )
+    core_def_id = core_def_res["Id"]
+
+    v2_cores = [
+        {
+            "CertificateArn": f"arn:aws:iot:ap-northeast-1:{ACCOUNT_ID}:cert/277a6a15293c1ed5fa1aa74bae890b1827f80959537bfdcf10f63e661d54ebe1",
+            "Id": "987654321",
+            "ThingArn": f"arn:aws:iot:ap-northeast-1:{ACCOUNT_ID}:thing/v2Thing",
+        }
+    ]
+
+    core_def_ver_res = client.create_core_definition_version(
+        CoreDefinitionId=core_def_id, Cores=v2_cores
+    )
+    core_def_ver_res.should.have.key("Arn")
+    core_def_ver_res.should.have.key("CreationTimestamp").equals(
+        "2022-06-01T12:00:00.000Z"
+    )
+    core_def_ver_res.should.have.key("Id").equals(core_def_id)
+    core_def_ver_res.should.have.key("Version")
