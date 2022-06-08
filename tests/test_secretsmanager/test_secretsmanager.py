9b694904d2ba7040485329e8ab224e4ad1df5ca3
Bert Blommers <bblommers@users.noreply.github.com>
2022-06-08 10:34:46 +0000

    Update tests/test_secretsmanager/test_secretsmanager.py

--- tests/test_secretsmanager/test_secretsmanager.py
+++ tests/test_secretsmanager/test_secretsmanager.py
@@ -36,7 +36,6 @@ def test_secret_arn():
         Name=DEFAULT_SECRET_NAME,
         SecretString="secret_string",
     )
-    
     assert re.match(f"arn:aws:secretsmanager:{region}:{ACCOUNT_ID}:secret:{DEFAULT_SECRET_NAME}-" + r"\w{6}", create_dict['ARN'])
 
 
