47fe052c6fe7511c1eb9b09339d16105c377a72e
Feng He <frankhe.cn@gmail.com>
2022-06-10 03:37:30 +1000

    sso-admin: add PermissionSet actions (#5208)

--- moto/ssoadmin/responses.py
+++ moto/ssoadmin/responses.py
@@ -65,3 +65,71 @@ class SSOAdminResponse(BaseResponse):
             permission_set_arn=permission_set_arn,
         )
         return json.dumps({"AccountAssignments": assignments})
+
+    def create_permission_set(self):
+        name = self._get_param("Name")
+        description = self._get_param("Description")
+        instance_arn = self._get_param("InstanceArn")
+        session_duration = self._get_param("SessionDuration", 3600)
+        relay_state = self._get_param("RelayState")
+        tags = self._get_param("Tags")
+
+        permission_set = self.ssoadmin_backend.create_permission_set(
+            name=name,
+            description=description,
+            instance_arn=instance_arn,
+            session_duration=session_duration,
+            relay_state=relay_state,
+            tags=tags,
+        )
+
+        return json.dumps({"PermissionSet": permission_set})
+
+    def delete_permission_set(self):
+        params = json.loads(self.body)
+        instance_arn = params.get("InstanceArn")
+        permission_set_arn = params.get("PermissionSetArn")
+        self.ssoadmin_backend.delete_permission_set(
+            instance_arn=instance_arn,
+            permission_set_arn=permission_set_arn,
+        )
+
+    def update_permission_set(self):
+        instance_arn = self._get_param("InstanceArn")
+        permission_set_arn = self._get_param("PermissionSetArn")
+        description = self._get_param("Description")
+        session_duration = self._get_param("SessionDuration", 3600)
+        relay_state = self._get_param("RelayState")
+
+        self.ssoadmin_backend.update_permission_set(
+            instance_arn=instance_arn,
+            permission_set_arn=permission_set_arn,
+            description=description,
+            session_duration=session_duration,
+            relay_state=relay_state,
+        )
+
+    def describe_permission_set(self):
+        instance_arn = self._get_param("InstanceArn")
+        permission_set_arn = self._get_param("PermissionSetArn")
+
+        permission_set = self.ssoadmin_backend.describe_permission_set(
+            instance_arn=instance_arn,
+            permission_set_arn=permission_set_arn,
+        )
+        return json.dumps({"PermissionSet": permission_set})
+
+    def list_permission_sets(self):
+        instance_arn = self._get_param("InstanceArn")
+        max_results = self._get_int_param("MaxResults")
+        next_token = self._get_param("NextToken")
+        permission_sets, next_token = self.ssoadmin_backend.list_permission_sets(
+            instance_arn=instance_arn, max_results=max_results, next_token=next_token
+        )
+        permission_set_ids = []
+        for permission_set in permission_sets:
+            permission_set_ids.append(permission_set.permission_set_arn)
+        response = {"PermissionSets": permission_set_ids}
+        if next_token:
+            response["NextToken"] = next_token
+        return json.dumps(response)
