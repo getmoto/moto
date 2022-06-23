383aef28cb280c19017fdc5d4e54dc4c3d0eae27
b0bu <maclighiche@gmail.com>
2022-06-23 14:13:12 +0100

    codebuild - testing multiple class model for codebuild metadata and history

--- moto/codebuild/models.py
+++ moto/codebuild/models.py
@@ -5,33 +5,53 @@ from moto.core import get_account_id
 # from .exceptions import InvalidInputException
 
 
+class CodeBuildProjectHistory(BaseModel):
+
+    def __init__(self, project_name):
+        self.project_build_history = list()
+
+    def list_builds_for_project(self, project_name):
+        # validate stuff
+        return self.codebuild_projects[project_name].project_build_history
+
+class CodeBuildProjectHistoryMetadata(BaseModel):
+
+    def __init__(self, region, project_name, project_source=dict(), artifacts=dict(), environment=dict(), serviceRole="some_role"):
+        current_date = iso_8601_datetime_with_milliseconds(datetime.utcnow())
+        # remove codebuild from these names
+        self.project_metadata = dict()
+        self.project_build_history = list()
+
+
 class CodeBuild(BaseModel):
 
     def __init__(self, region, project_name, project_source=dict(), artifacts=dict(), environment=dict(), serviceRole="some_role"):
         current_date = iso_8601_datetime_with_milliseconds(datetime.utcnow())
-        self.codebuild_project_metadata = dict()
+        # remove codebuild from these names
+        self.project_metadata = dict()
+        self.project_build_history = list()
 
-        self.codebuild_project_metadata["name"] = project_name
-        self.codebuild_project_metadata["arn"] = "arn:aws:codebuild:{0}:{1}:project/{2}".format(
-            region, get_account_id(), self.codebuild_project_metadata["name"]
+        self.project_metadata["name"] = project_name
+        self.project_metadata["arn"] = "arn:aws:codebuild:{0}:{1}:project/{2}".format(
+            region, get_account_id(), self.project_metadata["name"]
         )
-        self.codebuild_project_metadata["encryptionKey"] = "arn:aws:kms:{0}:{1}:alias/aws/s3".format(
+        self.project_metadata["encryptionKey"] = "arn:aws:kms:{0}:{1}:alias/aws/s3".format(
             region, get_account_id()
         )
-        self.codebuild_project_metadata["serviceRole"] = "arn:aws:iam::{0}:role/service-role/{1}".format(
+        self.project_metadata["serviceRole"] = "arn:aws:iam::{0}:role/service-role/{1}".format(
             get_account_id(), serviceRole
         )
-        self.codebuild_project_metadata["lastModifiedDate"] = current_date
-        self.codebuild_project_metadata["created"] = current_date
-        self.codebuild_project_metadata["badge"] = dict()
-        self.codebuild_project_metadata["badge"]["badgeEnabled"] = False         # this false needs to be a json false not a python false
-        self.codebuild_project_metadata["environment"] = environment
-        self.codebuild_project_metadata["artifacts"] = artifacts
-        self.codebuild_project_metadata["source"] = project_source
-        self.codebuild_project_metadata["cache"] = dict()
-        self.codebuild_project_metadata["cache"]["type"] = "NO_CACHE"
-        self.codebuild_project_metadata["timeoutInMinutes"] = ""
-        self.codebuild_project_metadata["queuedTimeoutInMinutes"] = ""
+        self.project_metadata["lastModifiedDate"] = current_date
+        self.project_metadata["created"] = current_date
+        self.project_metadata["badge"] = dict()
+        self.project_metadata["badge"]["badgeEnabled"] = False         # this false needs to be a json false not a python false
+        self.project_metadata["environment"] = environment
+        self.project_metadata["artifacts"] = artifacts
+        self.project_metadata["source"] = project_source
+        self.project_metadata["cache"] = dict()
+        self.project_metadata["cache"]["type"] = "NO_CACHE"
+        self.project_metadata["timeoutInMinutes"] = ""
+        self.project_metadata["queuedTimeoutInMinutes"] = ""
 
 
 class CodeBuildBackend(BaseBackend):
@@ -57,7 +77,7 @@ class CodeBuildBackend(BaseBackend):
             self.region_name, project_name, project_source, artifacts, environment, role
         )
 
-        return self.codebuild_projects[project_name].codebuild_project_metadata
+        return self.codebuild_projects[project_name].project_metadata
 
     def list_projects(self):
         projects = []
@@ -73,11 +93,8 @@ class CodeBuildBackend(BaseBackend):
         pass
 
     def list_builds_for_project(self, project_name):
-        # validate some stuff
-        self.code_build_projects[project_name] = CodeBuild(project_name)
-        # return the ids ; there must be a codebuild project and there must be a history to return anyything but []
-        # create the code build project first
-        return self.code_build_projects[project_name].ids
+        # validate stuff
+        return self.codebuild_projects[project_name].project_build_history
 
 
 codebuild_backends = BackendDict(CodeBuildBackend, "codebuild")
