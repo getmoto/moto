b9c57cb0b984e72c2d124e3be099f8714abf42fe
b0bu <maclighiche@gmail.com>
2022-06-28 17:19:26 +0100

    codebuild - run black formatter

--- moto/codebuild/models.py
+++ moto/codebuild/models.py
@@ -6,11 +6,11 @@ from random import randint
 from dateutil import parser
 import datetime
 import uuid
+
 # from .exceptions import InvalidInputException
 
 
 class CodeBuildProjectMetadata(BaseModel):
-
     def __init__(self, project_name, source_version, artifacts, build_id, service_role):
         current_date = iso_8601_datetime_with_milliseconds(datetime.datetime.utcnow())
         self.build_metadata = dict()
@@ -20,58 +20,80 @@ class CodeBuildProjectMetadata(BaseModel):
             get_account_id(), build_id
         )
 
-        self.build_metadata["buildNumber"] = randint(1,100)
+        self.build_metadata["buildNumber"] = randint(1, 100)
         self.build_metadata["startTime"] = current_date
         self.build_metadata["currentPhase"] = "QUEUED"
         self.build_metadata["buildStatus"] = "IN_PROGRESS"
-        self.build_metadata["sourceVersion"] = source_version if source_version else "refs/heads/main"
+        self.build_metadata["sourceVersion"] = (
+            source_version if source_version else "refs/heads/main"
+        )
         self.build_metadata["projectName"] = project_name
 
-        self.build_metadata["phases"] = [{"phaseType": "SUBMITTED",
-            "phaseStatus": "SUCCEEDED",
-            "startTime": current_date,
-            "endTime": current_date,
-            "durationInSeconds": 0},
-        {"phaseType": "QUEUED",
-            "startTime": current_date}]
+        self.build_metadata["phases"] = [
+            {
+                "phaseType": "SUBMITTED",
+                "phaseStatus": "SUCCEEDED",
+                "startTime": current_date,
+                "endTime": current_date,
+                "durationInSeconds": 0,
+            },
+            {"phaseType": "QUEUED", "startTime": current_date},
+        ]
 
-        self.build_metadata["source"] = {"type": "CODECOMMIT",  # should be different based on what you pass in
+        self.build_metadata["source"] = {
+            "type": "CODECOMMIT",  # should be different based on what you pass in
             "location": "https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/testing",
             "gitCloneDepth": 1,
             "gitSubmodulesConfig": {"fetchSubmodules": False},
             "buildspec": "buildspec/stuff.yaml",  # should present in the codebuild project somewhere
-            "insecureSsl": False}
+            "insecureSsl": False,
+        }
 
         self.build_metadata["secondarySources"] = []
         self.build_metadata["secondarySourceVersions"] = []
-        self.build_metadata["artifacts"] = {"location": ""}
+        self.build_metadata["artifacts"] = artifacts
         self.build_metadata["secondaryArtifacts"] = []
         self.build_metadata["cache"] = {"type": "NO_CACHE"}
 
-        self.build_metadata["environment"] = {"type": "LINUX_CONTAINER",
+        self.build_metadata["environment"] = {
+            "type": "LINUX_CONTAINER",
             "image": "aws/codebuild/amazonlinux2-x86_64-standard:3.0",
             "computeType": "BUILD_GENERAL1_SMALL",
             "environmentVariables": [],
             "privilegedMode": False,
-            "imagePullCredentialsType": "CODEBUILD"}
+            "imagePullCredentialsType": "CODEBUILD",
+        }
 
         self.build_metadata["serviceRole"] = service_role
 
-        self.build_metadata["logs"] = {"deepLink": "https://console.aws.amazon.com/cloudwatch/home?region=eu-west-2#logEvent:group=null;stream=null",
-            "cloudWatchLogsArn": "arn:aws:logs:eu-west-2:{0}:log-group:null:log-stream:null".format(get_account_id()),
+        self.build_metadata["logs"] = {
+            "deepLink": "https://console.aws.amazon.com/cloudwatch/home?region=eu-west-2#logEvent:group=null;stream=null",
+            "cloudWatchLogsArn": "arn:aws:logs:eu-west-2:{0}:log-group:null:log-stream:null".format(
+                get_account_id()
+            ),
             "cloudWatchLogs": {"status": "ENABLED"},
-            "s3Logs": {"status": "DISABLED", "encryptionDisabled": False}}
+            "s3Logs": {"status": "DISABLED", "encryptionDisabled": False},
+        }
 
         self.build_metadata["timeoutInMinutes"] = 45
         self.build_metadata["queuedTimeoutInMinutes"] = 480
         self.build_metadata["buildComplete"] = False
         self.build_metadata["initiator"] = "rootme"
-        self.build_metadata["encryptionKey"] = "arn:aws:kms:eu-west-2:{0}:alias/aws/s3".format(get_account_id())
+        self.build_metadata[
+            "encryptionKey"
+        ] = "arn:aws:kms:eu-west-2:{0}:alias/aws/s3".format(get_account_id())
 
 
 class CodeBuild(BaseModel):
-
-    def __init__(self, region, project_name, project_source=dict(), artifacts=dict(), environment=dict(), serviceRole="some_role"):
+    def __init__(
+        self,
+        region,
+        project_name,
+        project_source,
+        artifacts,
+        environment,
+        serviceRole="some_role",
+    ):
         current_date = iso_8601_datetime_with_milliseconds(datetime.datetime.utcnow())
         self.project_metadata = dict()
 
@@ -79,16 +101,20 @@ class CodeBuild(BaseModel):
         self.project_metadata["arn"] = "arn:aws:codebuild:{0}:{1}:project/{2}".format(
             region, get_account_id(), self.project_metadata["name"]
         )
-        self.project_metadata["encryptionKey"] = "arn:aws:kms:{0}:{1}:alias/aws/s3".format(
-            region, get_account_id()
-        )
-        self.project_metadata["serviceRole"] = "arn:aws:iam::{0}:role/service-role/{1}".format(
+        self.project_metadata[
+            "encryptionKey"
+        ] = "arn:aws:kms:{0}:{1}:alias/aws/s3".format(region, get_account_id())
+        self.project_metadata[
+            "serviceRole"
+        ] = "arn:aws:iam::{0}:role/service-role/{1}".format(
             get_account_id(), serviceRole
         )
         self.project_metadata["lastModifiedDate"] = current_date
         self.project_metadata["created"] = current_date
         self.project_metadata["badge"] = dict()
-        self.project_metadata["badge"]["badgeEnabled"] = False  # this false needs to be a json false not a python false
+        self.project_metadata["badge"][
+            "badgeEnabled"
+        ] = False  # this false needs to be a json false not a python false
         self.project_metadata["environment"] = environment
         self.project_metadata["artifacts"] = artifacts
         self.project_metadata["source"] = project_source
@@ -99,7 +125,7 @@ class CodeBuild(BaseModel):
 
 
 class CodeBuildBackend(BaseBackend):
-    """ find which functions I used in the dev branch stuff """
+    """find which functions I used in the dev branch stuff"""
 
     def __init__(self, region_name, account_id):
         super().__init__(region_name, account_id)
@@ -108,13 +134,20 @@ class CodeBuildBackend(BaseBackend):
         self.build_metadata = dict()
         self.build_metadata_history = defaultdict(list)
 
-    def create_project(self, project_name, project_source, artifacts, environment, service_role):
+    def create_project(
+        self, project_name, project_source, artifacts, environment, service_role
+    ):
         # required in other functions that don't
         self.project_name = project_name
         self.service_role = service_role
 
         self.codebuild_projects[project_name] = CodeBuild(
-            self.region_name, project_name, project_source, artifacts, environment, service_role
+            self.region_name,
+            project_name,
+            project_source,
+            artifacts,
+            environment,
+            service_role,
         )
 
         # empty build history
@@ -165,7 +198,7 @@ class CodeBuildBackend(BaseBackend):
             "POST_BUILD",
             "UPLOAD_ARTIFACTS",
             "FINALIZING",
-            "COMPLETED"
+            "COMPLETED",
         ]
 
         for status in statuses:
@@ -174,7 +207,7 @@ class CodeBuildBackend(BaseBackend):
             phase["phaseStatus"] = "SUCCEEDED"
             phase["startTime"] = current_date
             phase["endTime"] = current_date
-            phase["durationInSeconds"] = randint(10,100)
+            phase["durationInSeconds"] = randint(10, 100)
             phases.append(phase)
 
         return phases
@@ -187,7 +220,8 @@ class CodeBuildBackend(BaseBackend):
                 if build["id"] in ids:
                     build["phases"] = self._set_phases(build["phases"])
                     build["endTime"] = iso_8601_datetime_with_milliseconds(
-                        parser.parse(build["startTime"]) + datetime.timedelta(minutes=randint(1,5))
+                        parser.parse(build["startTime"])
+                        + datetime.timedelta(minutes=randint(1, 5))
                     )
                     build["currentPhase"] = "COMPLETED"
                     build["buildStatus"] = "SUCCEEDED"
@@ -213,15 +247,16 @@ class CodeBuildBackend(BaseBackend):
         self.build_metadata.pop(project_name, None)
         self.codebuild_projects.pop(project_name, None)
 
-    def stop_build(self, id):
+    def stop_build(self, build_id):
 
         for metadata in self.build_metadata_history.values():
             for build in metadata:
-                if build["id"] == id:
+                if build["id"] == build_id:
                     # set completion properties with variable completion time
                     build["phases"] = self._set_phases(build["phases"])
                     build["endTime"] = iso_8601_datetime_with_milliseconds(
-                        parser.parse(build["startTime"]) + datetime.timedelta(minutes=randint(1,5))
+                        parser.parse(build["startTime"])
+                        + datetime.timedelta(minutes=randint(1, 5))
                     )
                     build["currentPhase"] = "COMPLETED"
                     build["buildStatus"] = "STOPPED"
--- moto/codebuild/responses.py
+++ moto/codebuild/responses.py
@@ -1,6 +1,10 @@
 from moto.core.responses import BaseResponse
 from .models import codebuild_backends
-from .exceptions import InvalidInputException, ResourceAlreadyExistsException, ResourceNotFoundException
+from .exceptions import (
+    InvalidInputException,
+    ResourceAlreadyExistsException,
+    ResourceNotFoundException,
+)
 from moto.core import get_account_id
 import json
 import re
@@ -8,25 +12,32 @@ import re
 
 def _validate_required_params_source(source):
     try:
-        assert source["type"] in ["BITBUCKET", "CODECOMMIT", "CODEPIPELINE", "GITHUB", "GITHUB_ENTERPRISE", "NO_SOURCE", "S3"]
+        assert source["type"] in [
+            "BITBUCKET",
+            "CODECOMMIT",
+            "CODEPIPELINE",
+            "GITHUB",
+            "GITHUB_ENTERPRISE",
+            "NO_SOURCE",
+            "S3",
+        ]
     except AssertionError:
-        raise InvalidInputException(
-            "Invalid type provided: Project source type"
-        )
+        raise InvalidInputException("Invalid type provided: Project source type")
     # if type CODECOMMIT ensure https://git-codecommit.<region>.amazonaws.com in location value
     # if type S3 ensure valid bucket name or use mock_s3
     try:
         assert source["location"]
         assert source["location"] != ""
     except (KeyError, AssertionError):
-        raise InvalidInputException(
-            "Project source location is required"
-        )
+        raise InvalidInputException("Project source location is required")
 
 
 def _validate_required_params_service_role(service_role):
     try:
-        assert "arn:aws:iam::{0}:role/service-role/".format(get_account_id()) in service_role
+        assert (
+            "arn:aws:iam::{0}:role/service-role/".format(get_account_id())
+            in service_role
+        )
     except AssertionError:
         raise InvalidInputException(
             "Invalid service role: Service role account ID does not match caller's account"
@@ -37,9 +48,7 @@ def _validate_required_params_artifacts(artifacts):
     try:
         assert artifacts["type"] in ["CODEPIPELINE", "S3", "NO_ARTIFACTS"]
     except AssertionError:
-        raise InvalidInputException(
-            "Invalid type provided: Artifact type"
-        )
+        raise InvalidInputException("Invalid type provided: Artifact type")
     try:
         if artifacts["type"] == "NO_ARTIFACTS":
             try:
@@ -52,20 +61,28 @@ def _validate_required_params_artifacts(artifacts):
             assert artifacts["location"]
             assert artifacts["location"] != ""
     except (KeyError, AssertionError):
-        raise InvalidInputException(
-            "Project source location is required"
-        )
+        raise InvalidInputException("Project source location is required")
 
 
 def _validate_required_params_environment(environment):
     try:
-        assert environment["type"] in ["WINDOWS_CONTAINER", "LINUX_CONTAINER", "LINUX_GPU_CONTAINER", "ARM_CONTAINER"]
+        assert environment["type"] in [
+            "WINDOWS_CONTAINER",
+            "LINUX_CONTAINER",
+            "LINUX_GPU_CONTAINER",
+            "ARM_CONTAINER",
+        ]
     except AssertionError:
         raise InvalidInputException(
             "Invalid type provided: {0}".format(environment["type"])
         )
     try:
-        assert environment["computeType"] in ["BUILD_GENERAL1_SMALL", "BUILD_GENERAL1_MEDIUM", "BUILD_GENERAL1_LARGE", "BUILD_GENERAL1_2XLARGE"]
+        assert environment["computeType"] in [
+            "BUILD_GENERAL1_SMALL",
+            "BUILD_GENERAL1_MEDIUM",
+            "BUILD_GENERAL1_LARGE",
+            "BUILD_GENERAL1_2XLARGE",
+        ]
     except AssertionError:
         raise InvalidInputException(
             "Invalid compute type provided: {0}".format(environment["computeType"])
@@ -83,7 +100,6 @@ def _validate_required_params_project_name(name):
 
 
 class CodeBuildResponse(BaseResponse):
-
     @property
     def codebuild_backend(self):
         return codebuild_backends[self.region]
@@ -92,11 +108,15 @@ class CodeBuildResponse(BaseResponse):
         _validate_required_params_project_name(self._get_param("projectName"))
 
         try:
-            assert self._get_param("projectName") in self.codebuild_backend.codebuild_projects.keys()
+            assert (
+                self._get_param("projectName")
+                in self.codebuild_backend.codebuild_projects.keys()
+            )
         except AssertionError:
             raise ResourceNotFoundException(
-             "The provided project arn:aws:codebuild:{0}:{1}:project/{2} does not exist".format(
-                    self.region, get_account_id(), self._get_param("projectName"))
+                "The provided project arn:aws:codebuild:{0}:{1}:project/{2} does not exist".format(
+                    self.region, get_account_id(), self._get_param("projectName")
+                )
             )
 
         ids = self.codebuild_backend.list_builds_for_project(
@@ -104,7 +124,6 @@ class CodeBuildResponse(BaseResponse):
         )
 
         return json.dumps({"ids": ids})
-
 
     def create_project(self):
         _validate_required_params_source(self._get_param("source"))
@@ -114,15 +133,23 @@ class CodeBuildResponse(BaseResponse):
         _validate_required_params_project_name(self._get_param("name"))
 
         try:
-            assert self._get_param("name") not in self.codebuild_backend.codebuild_projects.keys()
+            assert (
+                self._get_param("name")
+                not in self.codebuild_backend.codebuild_projects.keys()
+            )
         except AssertionError:
             raise ResourceAlreadyExistsException(
                 "Project already exists: arn:aws:codebuild:{0}:{1}:project/{2}".format(
-                    self.region, get_account_id(), self._get_param("name"))
+                    self.region, get_account_id(), self._get_param("name")
+                )
             )
 
         project_metadata = self.codebuild_backend.create_project(
-            self._get_param("name"), self._get_param("source"), self._get_param("artifacts"), self._get_param("environment"), self._get_param("serviceRole")
+            self._get_param("name"),
+            self._get_param("source"),
+            self._get_param("artifacts"),
+            self._get_param("environment"),
+            self._get_param("serviceRole"),
         )
 
         return json.dumps({"project": project_metadata})
@@ -135,31 +162,33 @@ class CodeBuildResponse(BaseResponse):
         _validate_required_params_project_name(self._get_param("projectName"))
 
         try:
-            assert self._get_param("projectName") in self.codebuild_backend.codebuild_projects.keys()
+            assert (
+                self._get_param("projectName")
+                in self.codebuild_backend.codebuild_projects.keys()
+            )
         except AssertionError:
             raise ResourceNotFoundException(
                 "Project cannot be found: arn:aws:codebuild:{0}:{1}:project/{2}".format(
-                    self.region, get_account_id(), self._get_param("projectName"))
+                    self.region, get_account_id(), self._get_param("projectName")
+                )
             )
 
         metadata = self.codebuild_backend.start_build(
-            self._get_param("projectName"), self._get_param("sourceVersion"), self._get_param("artifactsOverride")
+            self._get_param("projectName"),
+            self._get_param("sourceVersion"),
+            self._get_param("artifactsOverride"),
         )
         return json.dumps({"build": metadata})
 
     def batch_get_builds(self):
 
         try:
-            for id in self._get_param("ids"):
-                assert ":" in id
+            for build_id in self._get_param("ids"):
+                assert ":" in build_id
         except AssertionError:
-            raise InvalidInputException(
-                "Invalid build ID provided"
-            )
-    
-        metadata = self.codebuild_backend.batch_get_builds(
-            self._get_param("ids")
-        )
+            raise InvalidInputException("Invalid build ID provided")
+
+        metadata = self.codebuild_backend.batch_get_builds(self._get_param("ids"))
         return json.dumps({"builds": metadata})
 
     def list_builds(self):
@@ -169,9 +198,7 @@ class CodeBuildResponse(BaseResponse):
     def delete_project(self):
         _validate_required_params_project_name(self._get_param("name"))
 
-        self.codebuild_backend.delete_project(
-            self._get_param("name")
-        )
+        self.codebuild_backend.delete_project(self._get_param("name"))
         return
 
     def stop_build(self):
@@ -179,11 +206,7 @@ class CodeBuildResponse(BaseResponse):
         try:
             assert ":" in self._get_param("id")
         except AssertionError:
-            raise InvalidInputException(
-                "Invalid build ID provided"
-            )
+            raise InvalidInputException("Invalid build ID provided")
 
-        metadata = self.codebuild_backend.stop_build(
-            self._get_param("id")
-        )
+        metadata = self.codebuild_backend.stop_build(self._get_param("id"))
         return json.dumps({"build": metadata})
