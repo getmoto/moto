1e429a2c8cb58d7a6c7908088a734f8a6dc8c0b6
b0bu <maclighiche@gmail.com>
2022-06-29 13:39:51 +0100

    codebuild - adding more tests

--- tests/test_codebuild/test_codebuild.py
+++ tests/test_codebuild/test_codebuild.py
@@ -2,8 +2,8 @@ import boto3
 import sure  # noqa # pylint: disable=unused-import
 from moto import mock_codebuild
 from moto.core import ACCOUNT_ID
-#from botocore.exceptions import ClientError
-#import pytest
+from botocore.exceptions import ClientError, ParamValidationError
+import pytest
 from pprint import pprint as pp
 
 
@@ -11,7 +11,7 @@ from pprint import pprint as pp
 def test_codebuild_create_project_s3_artifacts():
     client = boto3.client("codebuild", region_name="eu-central-1")
 
-    name="my_project"
+    name = "some_project"
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -53,7 +53,7 @@ def test_codebuild_create_project_s3_artifacts():
 def test_codebuild_create_project_no_artifacts():
     client = boto3.client("codebuild", region_name="eu-central-1")
 
-    name="my_project"
+    name = "some_project"
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -78,7 +78,7 @@ def test_codebuild_create_project_no_artifacts():
         {"computeType": "BUILD_GENERAL1_SMALL",
         "image": "contents_not_validated",
         "type": "LINUX_CONTAINER"}
-        )
+    )
 
     response["project"]["source"].should.equal(
          {"location": "bucketname/path/file.zip", "type": "S3"}
@@ -88,7 +88,28 @@ def test_codebuild_create_project_no_artifacts():
         {"type": "NO_ARTIFACTS"}
     )
 
-# two projects with the same name?
+@mock_codebuild
+def test_codebuild_create_project_when_exists():
+    client = boto3.client("codebuild", region_name="eu-central-1")
+
+    name = "some_project"
+    source = dict()
+    source["type"] = "S3"
+    # repository location for S3
+    source["location"] = "bucketname/path/file.zip"
+    artifacts = {"type": "NO_ARTIFACTS"}
+
+    environment = dict()
+    environment["type"] = "LINUX_CONTAINER"
+    environment["image"] = "contents_not_validated"
+    environment["computeType"] = "BUILD_GENERAL1_SMALL"
+    service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
+
+    client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+    
+    with pytest.raises(ClientError) as err:
+        client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+        err.response["Error"]["Code"].should.equal("ResourceAlreadyExistsException")
 
 @mock_codebuild
 def test_codebuild_list_projects():
@@ -122,7 +143,7 @@ def test_codebuild_list_projects():
 def test_codebuild_list_builds_for_project_no_history():
     client = boto3.client("codebuild", region_name="eu-central-1")
 
-    name="my_project"
+    name = "some_project"
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -146,7 +167,7 @@ def test_codebuild_list_builds_for_project_no_history():
 def test_codebuild_list_builds_for_project_with_history():
     client = boto3.client("codebuild", region_name="eu-central-1")
 
-    name="my_project"
+    name = "some_project"
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -165,6 +186,34 @@ def test_codebuild_list_builds_for_project_with_history():
 
     response["ids"].should_not.be.empty
 
+# project never started
+@mock_codebuild
+def test_codebuild_list_builds_for_project_no_history():
+    client = boto3.client("codebuild", region_name="eu-central-1")
+
+    name = "some_project"
+    source = dict()
+    source["type"] = "S3"
+    # repository location for S3
+    source["location"] = "bucketname/path/file.zip"
+    artifacts = {"type": "NO_ARTIFACTS"}
+
+    environment = dict()
+    environment["type"] = "LINUX_CONTAINER"
+    environment["image"] = "contents_not_validated"
+    environment["computeType"] = "BUILD_GENERAL1_SMALL"
+    service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
+  
+    client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+
+    response = client.list_builds_for_project(projectName=name)
+    response.should_not.be.none
+    response["ids"].should.be.empty
+
+    with pytest.raises(ParamValidationError) as err:
+        client.batch_get_builds(ids=response["ids"])
+        err.response["Error"]["Code"].should.equal("ParamValidationError")
+
 
 # # mock start_build() here with hsitroy single projects single build history WILL FAIL WITH non-existent project name
 @mock_codebuild
@@ -172,7 +221,7 @@ def test_codebuild_start_build_no_overrides():
 
     client = boto3.client("codebuild", region_name="eu-central-1")
     
-    name="my_project"
+    name = "some_project"
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -194,11 +243,36 @@ def test_codebuild_start_build_no_overrides():
     # must test for default artifacts here 
 
 @mock_codebuild
+def test_codebuild_start_build_multiple_times():
+    client = boto3.client("codebuild", region_name="eu-central-1")
+
+    name = "some_project"
+    source = dict()
+    source["type"] = "S3"
+    # repository location for S3
+    source["location"] = "bucketname/path/file.zip"
+    artifacts = {"type": "NO_ARTIFACTS"}
+
+    environment = dict()
+    environment["type"] = "LINUX_CONTAINER"
+    environment["image"] = "contents_not_validated"
+    environment["computeType"] = "BUILD_GENERAL1_SMALL"
+    service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
+
+    client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+
+    client.start_build(projectName=name)
+    client.start_build(projectName=name)
+    client.start_build(projectName=name)
+
+    len(client.list_builds()["ids"]).should.equal(3)
+
+@mock_codebuild
 def test_codebuild_start_build_with_overrides():
 
     client = boto3.client("codebuild", region_name="eu-central-1")
     
-    name="my_project"
+    name = "some_project"
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -228,7 +302,36 @@ def test_codebuild_start_build_with_overrides():
 def test_codebuild_batch_get_builds_1_project():
     client = boto3.client("codebuild", region_name="eu-central-1")
 
-    name="my_project"
+    name = "some_project"
+    source = dict()
+    source["type"] = "S3"
+    # repository location for S3
+    source["location"] = "bucketname/path/file.zip"
+    artifacts = {"type": "NO_ARTIFACTS"}
+
+    environment = dict()
+    environment["type"] = "LINUX_CONTAINER"
+    environment["image"] = "contents_not_validated"
+    environment["computeType"] = "BUILD_GENERAL1_SMALL"
+    service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
+    
+    client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+    client.start_build(projectName=name)
+
+    history = client.list_builds_for_project(projectName=name)
+    response = client.batch_get_builds(ids=history["ids"])
+
+    response.should_not.be.none
+    response["builds"].should_not.be.none
+    response["builds"][0]["currentPhase"].should.equal("COMPLETED")
+    response["builds"][0]["buildNumber"].should.be.a(int)
+    response["builds"][0]["phases"].should_not.be.none
+    len(response["builds"][0]["phases"]).should.equal(11)
+
+@mock_codebuild
+def test_codebuild_batch_get_builds_2_projects():
+    client = boto3.client("codebuild", region_name="eu-central-1")
+
     source = dict()
     source["type"] = "S3"
     # repository location for S3
@@ -241,18 +344,80 @@ def test_codebuild_batch_get_builds_1_project():
     environment["computeType"] = "BUILD_GENERAL1_SMALL"
     service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
 
-    branch_override = "fix/testing"
-    artifacts_override = {"type": "NO_ARTIFACTS"}
+    client.create_project(name="project-1", source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+    client.start_build(projectName="project-1")
+
+    client.create_project(name="project-2", source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+    client.start_build(projectName="project-2")
+
+    response = client.list_builds()
+    response["ids"].should_not.be.empty
+
+    for build_id in response["ids"]:
+        try:
+            build_id.should.contain("project-1")
+        except AssertionError:
+            build_id.should.contain("project-2")
+
+    for metadata in client.batch_get_builds(ids=response["ids"])["builds"]:
+        metadata.should_not.be.none
+        try:
+            metadata["id"].should.contain("project-1")
+        except AssertionError:
+            metadata["id"].should.contain("project-2")
+
+
+@mock_codebuild
+def test_codebuild_delete_project():
+    client = boto3.client("codebuild", region_name="eu-central-1")
+
+    name = "some_project"
+    source = dict()
+    source["type"] = "S3"
+    # repository location for S3
+    source["location"] = "bucketname/path/file.zip"
+    artifacts = {"type": "NO_ARTIFACTS"}
+
+    environment = dict()
+    environment["type"] = "LINUX_CONTAINER"
+    environment["image"] = "contents_not_validated"
+    environment["computeType"] = "BUILD_GENERAL1_SMALL"
+    service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
+
+    client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
+    client.start_build(projectName=name)
+
+    response = client.list_builds_for_project(projectName=name)
+    response["ids"].should_not.be.empty
+
+    client.delete_project(name=name)
+
+    with pytest.raises(ClientError) as err:
+        client.list_builds_for_project(projectName=name)
+        err.response["Error"]["Code"].should.equal("ResourceNotFoundException")
+
+
+@mock_codebuild
+def test_codebuild_stop_build():
+    client = boto3.client("codebuild", region_name="eu-central-1")
+
+    name = "some_project"
+    source = dict()
+    source["type"] = "S3"
+    # repository location for S3
+    source["location"] = "bucketname/path/file.zip"
+    artifacts = {"type": "NO_ARTIFACTS"}
+
+    environment = dict()
+    environment["type"] = "LINUX_CONTAINER"
+    environment["image"] = "contents_not_validated"
+    environment["computeType"] = "BUILD_GENERAL1_SMALL"
+    service_role = "arn:aws:iam::123456789012:role/service-role/my-codebuild-service-role"
     
     client.create_project(name=name, source=source, artifacts=artifacts, environment=environment, serviceRole=service_role)
-    client.start_build(projectName=name, sourceVersion=branch_override, artifactsOverride=artifacts_override)
+    client.start_build(projectName=name)
 
-    history = client.list_builds_for_project(projectName=name)
-    response = client.batch_get_builds(ids=history["ids"])
+    builds = client.list_builds()
 
-    response.should_not.be.none
-    response["builds"].should_not.be.none
-    response["builds"][0]["currentPhase"].should.equal("COMPLETED")
-    response["builds"][0]["buildNumber"].should.be.a(int)
-    response["builds"][0]["phases"].should_not.be.none
-    len(response["builds"][0]["phases"]).should.equal(11)
+    response = client.stop_build(id=builds["ids"][0])
+    response["build"]["buildStatus"].should.equal("STOPPED")
