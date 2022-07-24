90389d56e511f411b817bd5ec3f7735949b73960
macnev2013 <macnev2013@gmail.com>
2022-07-24 17:09:17 +0530

    removed code duplicacy

--- moto/ec2/models/availability_zones_and_regions.py
+++ moto/ec2/models/availability_zones_and_regions.py
@@ -10,7 +10,7 @@ class Region(object):
 
 
 class Zone(object):
-    def __init__(self, name, region_name, zone_id, zone_type):
+    def __init__(self, name, region_name, zone_id, zone_type="availability-zone"):
         self.name = name
         self.region_name = region_name
         self.zone_id = zone_id
@@ -70,19 +70,16 @@ class RegionsAndZonesBackend:
                 region_name="af-south-1",
                 name="af-south-1a",
                 zone_id="afs1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="af-south-1",
                 name="af-south-1b",
                 zone_id="afs1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="af-south-1",
                 name="af-south-1c",
                 zone_id="afs1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "ap-south-1": [
@@ -90,13 +87,11 @@ class RegionsAndZonesBackend:
                 region_name="ap-south-1",
                 name="ap-south-1a",
                 zone_id="aps1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-south-1",
                 name="ap-south-1b",
                 zone_id="aps1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "eu-west-3": [
@@ -104,19 +99,16 @@ class RegionsAndZonesBackend:
                 region_name="eu-west-3",
                 name="eu-west-3a",
                 zone_id="euw3-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-west-3",
                 name="eu-west-3b",
                 zone_id="euw3-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-west-3",
                 name="eu-west-3c",
                 zone_id="euw3-az3",
-                zone_type="availability-zone",
             ),
         ],
         "eu-north-1": [
@@ -124,19 +116,16 @@ class RegionsAndZonesBackend:
                 region_name="eu-north-1",
                 name="eu-north-1a",
                 zone_id="eun1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-north-1",
                 name="eu-north-1b",
                 zone_id="eun1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-north-1",
                 name="eu-north-1c",
                 zone_id="eun1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "eu-west-2": [
@@ -144,19 +133,16 @@ class RegionsAndZonesBackend:
                 region_name="eu-west-2",
                 name="eu-west-2a",
                 zone_id="euw2-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-west-2",
                 name="eu-west-2b",
                 zone_id="euw2-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-west-2",
                 name="eu-west-2c",
                 zone_id="euw2-az1",
-                zone_type="availability-zone",
             ),
         ],
         "eu-west-1": [
@@ -164,19 +150,16 @@ class RegionsAndZonesBackend:
                 region_name="eu-west-1",
                 name="eu-west-1a",
                 zone_id="euw1-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-west-1",
                 name="eu-west-1b",
                 zone_id="euw1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-west-1",
                 name="eu-west-1c",
                 zone_id="euw1-az2",
-                zone_type="availability-zone",
             ),
         ],
         "ap-northeast-3": [
@@ -184,19 +167,16 @@ class RegionsAndZonesBackend:
                 region_name="ap-northeast-3",
                 name="ap-northeast-3a",
                 zone_id="apne3-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-3",
                 name="ap-northeast-3b",
                 zone_id="apne3-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-3",
                 name="ap-northeast-3c",
                 zone_id="apne3-az3",
-                zone_type="availability-zone",
             ),
         ],
         "ap-northeast-2": [
@@ -204,25 +184,21 @@ class RegionsAndZonesBackend:
                 region_name="ap-northeast-2",
                 name="ap-northeast-2a",
                 zone_id="apne2-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-2",
                 name="ap-northeast-2b",
                 zone_id="apne2-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-2",
                 name="ap-northeast-2c",
                 zone_id="apne2-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-2",
                 name="ap-northeast-2d",
                 zone_id="apne2-az4",
-                zone_type="availability-zone",
             ),
         ],
         "ap-northeast-1": [
@@ -230,19 +206,16 @@ class RegionsAndZonesBackend:
                 region_name="ap-northeast-1",
                 name="ap-northeast-1a",
                 zone_id="apne1-az4",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-1",
                 name="ap-northeast-1c",
                 zone_id="apne1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-northeast-1",
                 name="ap-northeast-1d",
                 zone_id="apne1-az2",
-                zone_type="availability-zone",
             ),
         ],
         "ap-east-1": [
@@ -250,19 +223,16 @@ class RegionsAndZonesBackend:
                 region_name="ap-east-1",
                 name="ap-east-1a",
                 zone_id="ape1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-east-1",
                 name="ap-east-1b",
                 zone_id="ape1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-east-1",
                 name="ap-east-1c",
                 zone_id="ape1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "sa-east-1": [
@@ -270,13 +240,11 @@ class RegionsAndZonesBackend:
                 region_name="sa-east-1",
                 name="sa-east-1a",
                 zone_id="sae1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="sa-east-1",
                 name="sa-east-1c",
                 zone_id="sae1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "ca-central-1": [
@@ -284,13 +252,11 @@ class RegionsAndZonesBackend:
                 region_name="ca-central-1",
                 name="ca-central-1a",
                 zone_id="cac1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ca-central-1",
                 name="ca-central-1b",
                 zone_id="cac1-az2",
-                zone_type="availability-zone",
             ),
         ],
         "ap-southeast-1": [
@@ -298,19 +264,16 @@ class RegionsAndZonesBackend:
                 region_name="ap-southeast-1",
                 name="ap-southeast-1a",
                 zone_id="apse1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-southeast-1",
                 name="ap-southeast-1b",
                 zone_id="apse1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-southeast-1",
                 name="ap-southeast-1c",
                 zone_id="apse1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "ap-southeast-2": [
@@ -318,19 +281,16 @@ class RegionsAndZonesBackend:
                 region_name="ap-southeast-2",
                 name="ap-southeast-2a",
                 zone_id="apse2-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-southeast-2",
                 name="ap-southeast-2b",
                 zone_id="apse2-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-southeast-2",
                 name="ap-southeast-2c",
                 zone_id="apse2-az2",
-                zone_type="availability-zone",
             ),
         ],
         "ap-southeast-3": [
@@ -338,19 +298,16 @@ class RegionsAndZonesBackend:
                 region_name="ap-southeast-3",
                 name="ap-southeast-3a",
                 zone_id="apse3-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-southeast-3",
                 name="ap-southeast-3b",
                 zone_id="apse3-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="ap-southeast-3",
                 name="ap-southeast-3c",
                 zone_id="apse3-az3",
-                zone_type="availability-zone",
             ),
         ],
         "eu-central-1": [
@@ -358,19 +315,16 @@ class RegionsAndZonesBackend:
                 region_name="eu-central-1",
                 name="eu-central-1a",
                 zone_id="euc1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-central-1",
                 name="eu-central-1b",
                 zone_id="euc1-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-central-1",
                 name="eu-central-1c",
                 zone_id="euc1-az1",
-                zone_type="availability-zone",
             ),
         ],
         "eu-south-1": [
@@ -378,19 +332,16 @@ class RegionsAndZonesBackend:
                 region_name="eu-south-1",
                 name="eu-south-1a",
                 zone_id="eus1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-south-1",
                 name="eu-south-1b",
                 zone_id="eus1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="eu-south-1",
                 name="eu-south-1c",
                 zone_id="eus1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "us-east-1": [
@@ -398,37 +349,31 @@ class RegionsAndZonesBackend:
                 region_name="us-east-1",
                 name="us-east-1a",
                 zone_id="use1-az6",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-1",
                 name="us-east-1b",
                 zone_id="use1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-1",
                 name="us-east-1c",
                 zone_id="use1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-1",
                 name="us-east-1d",
                 zone_id="use1-az4",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-1",
                 name="us-east-1e",
                 zone_id="use1-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-1",
                 name="us-east-1f",
                 zone_id="use1-az5",
-                zone_type="availability-zone",
             ),
         ],
         "us-east-2": [
@@ -436,19 +381,16 @@ class RegionsAndZonesBackend:
                 region_name="us-east-2",
                 name="us-east-2a",
                 zone_id="use2-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-2",
                 name="us-east-2b",
                 zone_id="use2-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-east-2",
                 name="us-east-2c",
                 zone_id="use2-az3",
-                zone_type="availability-zone",
             ),
         ],
         "us-west-1": [
@@ -456,13 +398,11 @@ class RegionsAndZonesBackend:
                 region_name="us-west-1",
                 name="us-west-1a",
                 zone_id="usw1-az3",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-west-1",
                 name="us-west-1b",
                 zone_id="usw1-az1",
-                zone_type="availability-zone",
             ),
         ],
         "us-west-2": [
@@ -470,19 +410,16 @@ class RegionsAndZonesBackend:
                 region_name="us-west-2",
                 name="us-west-2a",
                 zone_id="usw2-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-west-2",
                 name="us-west-2b",
                 zone_id="usw2-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-west-2",
                 name="us-west-2c",
                 zone_id="usw2-az3",
-                zone_type="availability-zone",
             ),
         ],
         "me-south-1": [
@@ -490,19 +427,16 @@ class RegionsAndZonesBackend:
                 region_name="me-south-1",
                 name="me-south-1a",
                 zone_id="mes1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="me-south-1",
                 name="me-south-1b",
                 zone_id="mes1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="me-south-1",
                 name="me-south-1c",
                 zone_id="mes1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "cn-north-1": [
@@ -510,13 +444,11 @@ class RegionsAndZonesBackend:
                 region_name="cn-north-1",
                 name="cn-north-1a",
                 zone_id="cnn1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="cn-north-1",
                 name="cn-north-1b",
                 zone_id="cnn1-az2",
-                zone_type="availability-zone",
             ),
         ],
         "cn-northwest-1": [
@@ -524,19 +456,16 @@ class RegionsAndZonesBackend:
                 region_name="cn-northwest-1",
                 name="cn-northwest-1a",
                 zone_id="cnnw1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="cn-northwest-1",
                 name="cn-northwest-1b",
                 zone_id="cnnw1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="cn-northwest-1",
                 name="cn-northwest-1c",
                 zone_id="cnnw1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "us-gov-west-1": [
@@ -544,19 +473,16 @@ class RegionsAndZonesBackend:
                 region_name="us-gov-west-1",
                 name="us-gov-west-1a",
                 zone_id="usgw1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-gov-west-1",
                 name="us-gov-west-1b",
                 zone_id="usgw1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-gov-west-1",
                 name="us-gov-west-1c",
                 zone_id="usgw1-az3",
-                zone_type="availability-zone",
             ),
         ],
         "us-gov-east-1": [
@@ -564,19 +490,16 @@ class RegionsAndZonesBackend:
                 region_name="us-gov-east-1",
                 name="us-gov-east-1a",
                 zone_id="usge1-az1",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-gov-east-1",
                 name="us-gov-east-1b",
                 zone_id="usge1-az2",
-                zone_type="availability-zone",
             ),
             Zone(
                 region_name="us-gov-east-1",
                 name="us-gov-east-1c",
                 zone_id="usge1-az3",
-                zone_type="availability-zone",
             ),
         ],
     }
