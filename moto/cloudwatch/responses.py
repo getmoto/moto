765805159440148d553501e748afd57a25102e4b
steffyP <steffyP@users.noreply.github.com>
2022-07-26 00:32:41 +0200

    parse 'ActionsEnabled' as boolean for describe-alarms (#5313)

--- moto/cloudwatch/responses.py
+++ moto/cloudwatch/responses.py
@@ -76,7 +76,7 @@ class CloudWatchResponse(BaseResponse):
         dimensions = self._get_list_prefix("Dimensions.member")
         alarm_actions = self._get_multi_param("AlarmActions.member")
         ok_actions = self._get_multi_param("OKActions.member")
-        actions_enabled = self._get_param("ActionsEnabled")
+        actions_enabled = self._get_bool_param("ActionsEnabled")
         insufficient_data_actions = self._get_multi_param(
             "InsufficientDataActions.member"
         )
@@ -354,7 +354,7 @@ DESCRIBE_ALARMS_TEMPLATE = """<DescribeAlarmsResponse xmlns="http://monitoring.a
         <{{tag_name}}>
             {% for alarm in alarms %}
             <member>
-                <ActionsEnabled>{{ alarm.actions_enabled }}</ActionsEnabled>
+                <ActionsEnabled>{{ "true" if alarm.actions_enabled else "false" }}</ActionsEnabled>
                 <AlarmActions>
                     {% for action in alarm.alarm_actions %}
                     <member>{{ action }}</member>
@@ -480,7 +480,7 @@ DESCRIBE_METRIC_ALARMS_TEMPLATE = """<DescribeAlarmsForMetricResponse xmlns="htt
         <MetricAlarms>
             {% for alarm in alarms %}
             <member>
-                <ActionsEnabled>{{ alarm.actions_enabled }}</ActionsEnabled>
+                <ActionsEnabled>{{ "true" if alarm.actions_enabled else "false" }}</ActionsEnabled>
                 <AlarmActions>
                     {% for action in alarm.alarm_actions %}
                     <member>{{ action }}</member>
