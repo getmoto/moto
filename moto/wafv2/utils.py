872b9ddbdb7158c9324d66aeb6d59556e09835ce
Bert Blommers <bblommers@users.noreply.github.com>
2022-09-10 13:30:45 +0000

    WAFv2: Extend coverage (#5460)

--- moto/wafv2/utils.py
+++ moto/wafv2/utils.py
@@ -1,6 +1,3 @@
-from moto.core.utils import pascal_to_camelcase, camelcase_to_underscores
-
-
 def make_arn_for_wacl(name, account_id, region_name, wacl_id, scope):
     """https://docs.aws.amazon.com/waf/latest/developerguide/how-aws-waf-works.html - explains --scope (cloudfront vs regional)"""
 
@@ -9,10 +6,3 @@ def make_arn_for_wacl(name, account_id, region_name, wacl_id, scope):
     elif scope == "CLOUDFRONT":
         scope = "global"
     return f"arn:aws:wafv2:{region_name}:{account_id}:{scope}/webacl/{name}/{wacl_id}"
-
-
-def pascal_to_underscores_dict(original_dict):
-    outdict = {}
-    for k, v in original_dict.items():
-        outdict[camelcase_to_underscores(pascal_to_camelcase(k))] = v
-    return outdict
