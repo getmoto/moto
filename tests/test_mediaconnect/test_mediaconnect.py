3adf4ff08af584adb56ca54c81dddd99a22296b5
donfiguerres <lyndonfiguerres@gmail.com>
2022-10-07 18:03:17 +0800

    MediaConnect: Implement update_flow_output (#5538)

--- tests/test_mediaconnect/test_mediaconnect.py
+++ tests/test_mediaconnect/test_mediaconnect.py
@@ -87,7 +87,7 @@ def test_create_flow_succeeds():
     )
     response["Flow"]["Name"].should.equal("test-Flow-1")
     response["Flow"]["Status"].should.equal("STANDBY")
-    response["Flow"]["Outputs"][0].should.equal({"Name": "Output-1"})
+    response["Flow"]["Outputs"][0]["Name"].should.equal("Output-1")
     response["Flow"]["Outputs"][1]["ListenerAddress"].should.equal("1.0.0.0")
     response["Flow"]["Outputs"][2]["ListenerAddress"].should.equal("2.0.0.0")
     response["Flow"]["Source"]["IngestIp"].should.equal("127.0.0.0")
@@ -339,6 +339,38 @@ def test_add_flow_outputs_fails():
     flow_arn = "unknown-flow"
     with pytest.raises(ClientError) as err:
         client.add_flow_outputs(FlowArn=flow_arn, Outputs=[])
+    err = err.value.response["Error"]
+    err["Code"].should.equal("NotFoundException")
+    err["Message"].should.equal("flow with arn=unknown-flow not found")
+
+
+@mock_mediaconnect
+def test_update_flow_output_succeeds():
+    client = boto3.client("mediaconnect", region_name=region)
+    channel_config = _create_flow_config("test-Flow-1")
+
+    create_response = client.create_flow(**channel_config)
+    create_response["ResponseMetadata"]["HTTPStatusCode"].should.equal(200)
+    create_response["Flow"]["Status"].should.equal("STANDBY")
+    flow_arn = create_response["Flow"]["FlowArn"]
+    output_arn = create_response["Flow"]["Outputs"][0]["OutputArn"]
+
+    update_response = client.update_flow_output(
+        FlowArn=flow_arn, OutputArn=output_arn, Description="new description"
+    )
+    update_response["Output"]["Description"].should.equal("new description")
+
+
+@mock_mediaconnect
+def test_update_flow_output_fails():
+    client = boto3.client("mediaconnect", region_name=region)
+    flow_arn = "unknown-flow"
+    with pytest.raises(ClientError) as err:
+        client.update_flow_output(
+            FlowArn=flow_arn,
+            OutputArn="some-arn",
+            Description="new description",
+        )
     err = err.value.response["Error"]
     err["Code"].should.equal("NotFoundException")
     err["Message"].should.equal("flow with arn=unknown-flow not found")
