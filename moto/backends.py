a470ea748b1c409a507d24c8a26836b92faec381
Bert Blommers <bblommers@users.noreply.github.com>
2022-10-23 12:19:13 +0000

    TechDebt - MyPy Budgets (#5591)

--- moto/backends.py
+++ moto/backends.py
@@ -1,6 +1,8 @@
 import importlib
 import moto
 import sys
+from moto.core.utils import BackendDict
+from typing import Iterable, Tuple
 
 
 decorators = [d for d in dir(moto) if d.startswith("mock_") and not d == "mock_all"]
@@ -12,44 +14,35 @@ BACKENDS["instance_metadata"] = ("instance_metadata", "instance_metadata_backend
 BACKENDS["s3bucket_path"] = ("s3", "s3_backends")
 
 
-def _import_backend(module_name, backends_name):
+def _import_backend(module_name: str, backends_name: str) -> BackendDict:
     module = importlib.import_module("moto." + module_name)
     return getattr(module, backends_name)
 
 
-def backends():
+def backends() -> Iterable[BackendDict]:
     for module_name, backends_name in BACKENDS.values():
         yield _import_backend(module_name, backends_name)
 
 
-def service_backends():
+def service_backends() -> Iterable[BackendDict]:
     services = [(f.name, f.backend) for f in decorator_functions]
     for module_name, backends_name in sorted(set(services)):
         yield _import_backend(module_name, backends_name)
 
 
-def loaded_backends():
+def loaded_backends() -> Iterable[Tuple[str, BackendDict]]:
     loaded_modules = sys.modules.keys()
-    loaded_modules = [m for m in loaded_modules if m.startswith("moto.")]
+    moto_modules = [m for m in loaded_modules if m.startswith("moto.")]
     imported_backends = [
         name
         for name, (module_name, _) in BACKENDS.items()
-        if f"moto.{module_name}" in loaded_modules
+        if f"moto.{module_name}" in moto_modules
     ]
     for name in imported_backends:
         module_name, backends_name = BACKENDS[name]
         yield name, _import_backend(module_name, backends_name)
 
 
-def get_backend(name):
+def get_backend(name: str) -> BackendDict:
     module_name, backends_name = BACKENDS[name]
     return _import_backend(module_name, backends_name)
-
-
-def get_model(name, region_name):
-    for backends_ in backends():
-        for region, backend in backends_.items():
-            if region == region_name:
-                models = getattr(backend.__class__, "__models__", {})
-                if name in models:
-                    return list(getattr(backend, models[name])())
