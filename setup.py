80db33cb44c77331b3b53bc7cba2caf56e61c5fb
Bert Blommers <bblommers@users.noreply.github.com>
2022-10-30 10:18:06 -0100

    Admin: Enable support for Py 3.11 (#5619)

--- setup.py
+++ setup.py
@@ -151,6 +151,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Testing",
     ],
