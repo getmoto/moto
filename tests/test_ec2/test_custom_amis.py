f516dfed0b5f5e40e061cb4cd6489b9b6209b47d
raf <therafcio9@gmail.com>
2022-11-22 09:20:02 +0000

    chore: lint

--- tests/test_ec2/test_custom_amis.py
+++ tests/test_ec2/test_custom_amis.py
@@ -44,8 +44,9 @@ from moto import mock_ec2  # noqa: E402
 @mock_ec2
 def test_custom_amis_with_MOTO_AMIS_PATH():
 
+    # clean up the file with custom amis, it's already been loaded
+    os.remove(ami_path)
+
     ec2_client = boto3.client("ec2", region_name="us-east-1")
     images = ec2_client.describe_images()["Images"]
     assert len(images) == 1
-
-    os.remove(ami_path)
