4273fb74b0fc5567622d1f25094238f616eb493c
sist <15859432+stiebels@users.noreply.github.com>
2022-12-22 10:17:29 +0100

    Corrected kwarg names

--- moto/sagemaker/utils.py
+++ moto/sagemaker/utils.py
@@ -5,8 +5,8 @@ import json
 def load_pipeline_definition_from_s3(pipeline_definition_s3_location, account_id):
     s3_backend = s3_backends[account_id]["global"]
     result = s3_backend.get_object(
-        Bucket=pipeline_definition_s3_location["Bucket"],
-        Key=pipeline_definition_s3_location["ObjectKey"],
+        bucket_name=pipeline_definition_s3_location["Bucket"],
+        key_name=pipeline_definition_s3_location["ObjectKey"],
     )
     pipeline_definition = json.loads(result["Body"].read())
     return pipeline_definition
