fba67e5ea1ae2a3cbb90c11854d9a312d3b94a35
opavliuk <40970635+opavlyuk@users.noreply.github.com>
2023-01-23 18:19:14 +0100

    CognitoIDP: fix REFRESH_TOKEN_AUTH flow in admin_initiate_auth()

--- moto/cognitoidp/models.py
+++ moto/cognitoidp/models.py
@@ -1429,7 +1429,7 @@ class CognitoIdpBackend(BaseBackend):
                 }
 
             return self._log_user_in(user_pool, client, username)
-        elif auth_flow is AuthFlow.REFRESH_TOKEN:
+        elif auth_flow in (AuthFlow.REFRESH_TOKEN, AuthFlow.REFRESH_TOKEN_AUTH):
             refresh_token: str = auth_parameters.get("REFRESH_TOKEN")  # type: ignore[assignment]
             (
                 access_token,
