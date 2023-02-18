f7af4990be79e9a5326b2bf40c819567bdf29d05
Dan Aronson <dan.aronson@orca.security>
2023-02-18 18:26:57 +0200

    remove redundant line

--- moto/__init__.py
+++ moto/__init__.py
@@ -168,7 +168,6 @@ mock_xray_client = lazy_load(".xray", "mock_xray_client")
 mock_wafv2 = lazy_load(".wafv2", "mock_wafv2")
 mock_textract = lazy_load(".textract", "mock_textract")
 mock_identitystore = lazy_load(".identitystore", "mock_identitystore")
-mock_identitystore = lazy_load(".identitystore", "mock_identitystore", boto3_name="identitystore")
 
 
 class MockAll(ContextDecorator):
