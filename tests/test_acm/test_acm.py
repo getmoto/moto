fd66843cf79c7b320af0caa319f6e4ecfd4b142c
Pepe Fagoaga <pepe@verica.io>
2023-02-26 21:45:54 +0100

    ACM: Include more info to the list_certificates (#5973)

--- tests/test_acm/test_acm.py
+++ tests/test_acm/test_acm.py
@@ -14,7 +14,6 @@ from moto import mock_acm, mock_elb, settings
 from moto.core import DEFAULT_ACCOUNT_ID as ACCOUNT_ID
 from unittest import SkipTest, mock
 
-
 RESOURCE_FOLDER = os.path.join(os.path.dirname(__file__), "resources")
 
 
@@ -94,12 +93,18 @@ def test_list_certificates():
     issued_arn = _import_cert(client)
     pending_arn = client.request_certificate(DomainName="google.com")["CertificateArn"]
 
-    moto_arn = {"CertificateArn": issued_arn, "DomainName": SERVER_COMMON_NAME}
-    google_arn = {"CertificateArn": pending_arn, "DomainName": "google.com"}
-
     certs = client.list_certificates()["CertificateSummaryList"]
-    certs.should.contain(moto_arn)
-    certs.should.contain(google_arn)
+    [c["CertificateArn"] for c in certs].should.contain(issued_arn)
+    [c["CertificateArn"] for c in certs].should.contain(pending_arn)
+    for cert in certs:
+        cert.should.have.key("CertificateArn")
+        cert.should.have.key("DomainName")
+        cert.should.have.key("Status")
+        cert.should.have.key("Type")
+        cert.should.have.key("KeyAlgorithm")
+        cert.should.have.key("RenewalEligibility")
+        cert.should.have.key("NotBefore")
+        cert.should.have.key("NotAfter")
 
     resp = client.list_certificates(CertificateStatuses=["EXPIRED", "INACTIVE"])
     len(resp["CertificateSummaryList"]).should.equal(0)
@@ -107,20 +112,20 @@ def test_list_certificates():
     certs = client.list_certificates(CertificateStatuses=["PENDING_VALIDATION"])[
         "CertificateSummaryList"
     ]
-    certs.should.contain(google_arn)
-    certs.shouldnt.contain(moto_arn)
+    [c["CertificateArn"] for c in certs].shouldnt.contain(issued_arn)
+    [c["CertificateArn"] for c in certs].should.contain(pending_arn)
 
     certs = client.list_certificates(CertificateStatuses=["ISSUED"])[
         "CertificateSummaryList"
     ]
-    certs.shouldnt.contain(google_arn)
-    certs.should.contain(moto_arn)
+    [c["CertificateArn"] for c in certs].should.contain(issued_arn)
+    [c["CertificateArn"] for c in certs].shouldnt.contain(pending_arn)
 
     certs = client.list_certificates(
         CertificateStatuses=["ISSUED", "PENDING_VALIDATION"]
     )["CertificateSummaryList"]
-    certs.should.contain(google_arn)
-    certs.should.contain(moto_arn)
+    [c["CertificateArn"] for c in certs].should.contain(issued_arn)
+    [c["CertificateArn"] for c in certs].should.contain(pending_arn)
 
 
 @mock_acm
