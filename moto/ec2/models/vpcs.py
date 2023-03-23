ad4122fe7ac499680770f8caedf1edca80fa44ac
Jordan Sanders <jordan@jordansanders.com>
2023-03-23 12:12:06 -0500

    Implement stack deletion for vpcs
    This also removes `test_delete_stack_with_resource_missing_delete_attr`
    because the test cases the assertions relied on (VPCs) are
    no longer valid. Also, this functionality is better tested as part of
    the CloudFormation test suite instead of the ec2 test suite.
    
    I followed the pattern of ignoring types on these cloudformation
    classmethods.

--- moto/ec2/models/vpcs.py
+++ moto/ec2/models/vpcs.py
@@ -239,6 +239,18 @@ class VPC(TaggedEC2Resource, CloudFormationModel):
 
         return vpc
 
+    @classmethod
+    def delete_from_cloudformation_json(  # type: ignore[misc]
+        cls,
+        resource_name: str,
+        cloudformation_json: Dict[str, Any],
+        account_id: str,
+        region_name: str,
+    ) -> None:
+        from ..models import ec2_backends
+
+        ec2_backends[account_id][region_name].delete_vpc(resource_name)
+
     @property
     def physical_resource_id(self) -> str:
         return self.id
--- tests/test_ec2/test_ec2_cloudformation.py
+++ tests/test_ec2/test_ec2_cloudformation.py
@@ -84,28 +84,20 @@ def test_vpc_single_instance_in_subnet():
 
 @mock_cloudformation
 @mock_ec2
-def test_delete_stack_with_resource_missing_delete_attr():
+def test_delete_stack_with_vpc():
     cf = boto3.client("cloudformation", region_name="us-east-1")
     ec2 = boto3.client("ec2", region_name="us-east-1")
     name = str(uuid4())[0:6]
 
     cf.create_stack(StackName=name, TemplateBody=json.dumps(template_vpc))
-    cf.describe_stacks(StackName=name)["Stacks"].should.have.length_of(1)
-
     resources = cf.list_stack_resources(StackName=name)["StackResourceSummaries"]
     vpc_id = resources[0]["PhysicalResourceId"]
 
-    cf.delete_stack(
-        StackName=name
-    )  # should succeed, despite the fact that the resource itself cannot be deleted
-    with pytest.raises(ClientError) as exc:
-        cf.describe_stacks(StackName=name)
-    err = exc.value.response["Error"]
-    err.should.have.key("Code").equals("ValidationError")
-    err.should.have.key("Message").equals(f"Stack with id {name} does not exist")
-
-    # We still have our VPC, as the VPC-object does not have a delete-method yet
     ec2.describe_vpcs(VpcIds=[vpc_id])["Vpcs"].should.have.length_of(1)
+
+    cf.delete_stack(StackName=name)
+    with pytest.raises(ec2.exceptions.ClientError):
+        ec2.describe_vpcs(VpcIds=[vpc_id])
 
 
 @mock_ec2
