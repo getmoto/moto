94db0c6fa4895811003d9c6901c7bbe6d8036189
Bert Blommers <bblommers@users.noreply.github.com>
2023-04-17 18:01:08 +0000

    Techdebt: MyPy Rekognition (#6219)

--- moto/rekognition/responses.py
+++ moto/rekognition/responses.py
@@ -1,22 +1,21 @@
-"""Handles incoming rekognition requests, invokes methods, returns responses."""
 import json
 
+from moto.core.common_types import TYPE_RESPONSE
 from moto.core.responses import BaseResponse
-from .models import rekognition_backends
+from .models import rekognition_backends, RekognitionBackend
 
 
 class RekognitionResponse(BaseResponse):
     """Handler for Rekognition requests and responses."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(service_name="rekognition")
 
     @property
-    def rekognition_backend(self):
-        """Return backend instance specific for this region."""
+    def rekognition_backend(self) -> RekognitionBackend:
         return rekognition_backends[self.current_account][self.region]
 
-    def get_face_search(self):
+    def get_face_search(self) -> str:
         (
             job_status,
             status_message,
@@ -37,7 +36,7 @@ class RekognitionResponse(BaseResponse):
             )
         )
 
-    def get_text_detection(self):
+    def get_text_detection(self) -> str:
         (
             job_status,
             status_message,
@@ -58,19 +57,16 @@ class RekognitionResponse(BaseResponse):
             )
         )
 
-    def start_face_search(self):
+    def start_face_search(self) -> TYPE_RESPONSE:
         headers = {"Content-Type": "application/x-amz-json-1.1"}
         job_id = self.rekognition_backend.start_face_search()
         response = ('{"JobId":"' + job_id + '"}').encode()
 
         return 200, headers, response
 
-    def start_text_detection(self):
+    def start_text_detection(self) -> TYPE_RESPONSE:
         headers = {"Content-Type": "application/x-amz-json-1.1"}
         job_id = self.rekognition_backend.start_text_detection()
         response = ('{"JobId":"' + job_id + '"}').encode()
 
         return 200, headers, response
-
-
-# add templates from here
