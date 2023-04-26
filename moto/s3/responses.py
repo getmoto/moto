fff1c3c6571e2d4b69b3b25b732e29474cda6a03
Benjamin Simon <benjh.simon@gmail.com>
2023-04-26 22:48:50 +0200

    re-add missing type annotation

--- moto/s3/responses.py
+++ moto/s3/responses.py
@@ -1610,7 +1610,7 @@ class S3Response(BaseResponse):
             else:
                 raise MissingKey(key=src_key)
 
-            new_key = self.backend.get_object(bucket_name, key_name)
+            new_key: FakeKey = self.backend.get_object(bucket_name, key_name)
 
             if acl is not None:
                 new_key.set_acl(acl)
