038b6fd6487df08a4005e2804a9a85d35f536aab
raf <therafcio9@gmail.com>
2023-04-30 12:11:58 +0100

    tests: contactlists return 200 if empty, contacts do not

--- tests/test_sesv2/test_server.py
+++ tests/test_sesv2/test_server.py
@@ -7,7 +7,7 @@ def test_sesv2_list():
     backend = server.create_backend_app("sesv2")
     test_client = backend.test_client()
 
-    resp = test_client.get("/v2/email/contact-lists/test/contacts")
+    resp = test_client.get("/v2/email/contact-lists")
 
     assert resp.status_code == 200
-    assert resp.data == b'{"Contacts": []}'
+    assert resp.data == b'{"ContactLists": []}'
