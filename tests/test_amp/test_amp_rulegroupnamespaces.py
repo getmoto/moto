22c4b2d0c04f1e410b830699de431c085138c04f
Bert Blommers <info@bertblommers.nl>
2023-06-03 20:04:00 +0000

    Techdebt: Replace sure with regular asserts in AMP tests

--- tests/test_amp/test_amp_rulegroupnamespaces.py
+++ tests/test_amp/test_amp_rulegroupnamespaces.py
@@ -1,7 +1,6 @@
 """Unit tests for amp-supported APIs."""
 import boto3
 import pytest
-import sure  # noqa # pylint: disable=unused-import
 
 from botocore.exceptions import ClientError
 from moto import mock_amp
@@ -18,9 +17,9 @@ def test_create_rule_groups_namespace():
         data=b"asdf", name="my first rule group", workspaceId=workspace_id
     )
 
-    resp.should.have.key("arn")
-    resp.should.have.key("name").equals("my first rule group")
-    resp.should.have.key("status")
+    assert "arn" in resp
+    assert resp["name"] == "my first rule group"
+    assert "status" in resp
 
 
 @mock_amp
@@ -36,8 +35,8 @@ def test_delete_rule_groups_namespace():
     with pytest.raises(ClientError) as exc:
         client.describe_rule_groups_namespace(name="myname", workspaceId=workspace_id)
     err = exc.value.response["Error"]
-    err["Code"].should.equal("ResourceNotFoundException")
-    err["Message"].should.equal("RuleGroupNamespace not found")
+    assert err["Code"] == "ResourceNotFoundException"
+    assert err["Message"] == "RuleGroupNamespace not found"
 
 
 @mock_amp
@@ -52,15 +51,15 @@ def test_describe_rule_groups_namespace():
     resp = client.describe_rule_groups_namespace(
         name="myname", workspaceId=workspace_id
     )
-    resp.should.have.key("ruleGroupsNamespace")
+    assert "ruleGroupsNamespace" in resp
     ns = resp["ruleGroupsNamespace"]
 
-    ns.should.have.key("arn")
-    ns.should.have.key("createdAt")
-    ns.should.have.key("data").equals(b"asdf")
-    ns.should.have.key("modifiedAt")
-    ns.should.have.key("name").equals("myname")
-    ns.should.have.key("status")
+    assert "arn" in ns
+    assert "createdAt" in ns
+    assert ns["data"] == b"asdf"
+    assert "modifiedAt" in ns
+    assert ns["name"] == "myname"
+    assert "status" in ns
 
 
 @mock_amp
@@ -79,12 +78,12 @@ def test_put_rule_groups_namespace():
     resp = client.describe_rule_groups_namespace(
         name="myname", workspaceId=workspace_id
     )
-    resp.should.have.key("ruleGroupsNamespace")
+    assert "ruleGroupsNamespace" in resp
     ns = resp["ruleGroupsNamespace"]
 
-    ns.should.have.key("arn")
-    ns.should.have.key("createdAt")
-    ns.should.have.key("data").equals(b"updated")
+    assert "arn" in ns
+    assert "createdAt" in ns
+    assert ns["data"] == b"updated"
 
 
 @mock_amp
@@ -97,18 +96,18 @@ def test_list_rule_groups_namespaces():
         )
 
     resp = client.list_rule_groups_namespaces(workspaceId=w_id)
-    resp.should.have.key("ruleGroupsNamespaces").length_of(15)
-    resp.shouldnt.have.key("nextToken")
+    assert len(resp["ruleGroupsNamespaces"]) == 15
+    assert "nextToken" not in resp
 
     resp = client.list_rule_groups_namespaces(workspaceId=w_id, name="ns1")
-    resp.should.have.key("ruleGroupsNamespaces").length_of(6)
+    assert len(resp["ruleGroupsNamespaces"]) == 6
     names = [ns["name"] for ns in resp["ruleGroupsNamespaces"]]
-    set(names).should.equal({"ns10", "ns13", "ns1", "ns12", "ns11", "ns14"})
+    assert set(names) == {"ns10", "ns13", "ns1", "ns12", "ns11", "ns14"}
 
     resp = client.list_rule_groups_namespaces(workspaceId=w_id, name="ns10")
-    resp.should.have.key("ruleGroupsNamespaces").length_of(1)
+    assert len(resp["ruleGroupsNamespaces"]) == 1
     names = [ns["name"] for ns in resp["ruleGroupsNamespaces"]]
-    set(names).should.equal({"ns10"})
+    assert set(names) == {"ns10"}
 
 
 @mock_amp
@@ -122,26 +121,26 @@ def test_list_rule_groups_namespaces__paginated():
 
     # default pagesize is 100
     page1 = client.list_rule_groups_namespaces(workspaceId=w_id)
-    page1.should.have.key("ruleGroupsNamespaces").length_of(100)
-    page1.should.have.key("nextToken")
+    assert len(page1["ruleGroupsNamespaces"]) == 100
+    assert "nextToken" in page1
 
     # We can ask for a smaller pagesize
     page2 = client.list_rule_groups_namespaces(
         workspaceId=w_id, maxResults=15, nextToken=page1["nextToken"]
     )
-    page2.should.have.key("ruleGroupsNamespaces").length_of(15)
-    page2.should.have.key("nextToken")
+    assert len(page2["ruleGroupsNamespaces"]) == 15
+    assert "nextToken" in page2
 
     page3 = client.list_rule_groups_namespaces(
         workspaceId=w_id, maxResults=15, nextToken=page2["nextToken"]
     )
-    page3.should.have.key("ruleGroupsNamespaces").length_of(10)
-    page3.shouldnt.have.key("nextToken")
+    assert len(page3["ruleGroupsNamespaces"]) == 10
+    assert "nextToken" not in page3
 
     # We could request all of them in one go
     full_page = client.list_rule_groups_namespaces(workspaceId=w_id, maxResults=150)
-    full_page.should.have.key("ruleGroupsNamespaces").length_of(125)
-    full_page.shouldnt.have.key("nextToken")
+    assert len(full_page["ruleGroupsNamespaces"]) == 125
+    assert "nextToken" not in full_page
 
 
 @mock_amp
@@ -157,17 +156,18 @@ def test_tag_resource():
 
     client.tag_resource(resourceArn=arn, tags={"t1": "v1", "t2": "v2"})
 
-    client.list_tags_for_resource(resourceArn=arn)["tags"].should.equal(
-        {"t": "v", "t1": "v1", "t2": "v2"}
-    )
-    client.describe_rule_groups_namespace(workspaceId=w_id, name="ns")[
+    assert get_tags(arn, client) == {"t": "v", "t1": "v1", "t2": "v2"}
+    ns = client.describe_rule_groups_namespace(workspaceId=w_id, name="ns")[
         "ruleGroupsNamespace"
-    ]["tags"].should.equal({"t": "v", "t1": "v1", "t2": "v2"})
+    ]
+    assert ns["tags"] == {"t": "v", "t1": "v1", "t2": "v2"}
 
     client.untag_resource(resourceArn=arn, tagKeys=["t1"])
-    client.list_tags_for_resource(resourceArn=arn)["tags"].should.equal(
-        {"t": "v", "t2": "v2"}
-    )
+    assert get_tags(arn, client) == {"t": "v", "t2": "v2"}
 
     client.untag_resource(resourceArn=arn, tagKeys=["t", "t2"])
-    client.list_tags_for_resource(resourceArn=arn)["tags"].should.equal({})
+    assert get_tags(arn, client) == {}
+
+
+def get_tags(arn, client):
+    return client.list_tags_for_resource(resourceArn=arn)["tags"]
