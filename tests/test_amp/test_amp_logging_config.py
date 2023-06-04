073b49411351fd6144ee598f0c1518ca29390b87
Bert Blommers <bblommers@users.noreply.github.com>
2023-06-04 09:50:10 +0000

    Techdebt: Replace sure with regular asserts in AMP tests (#6364)

--- tests/test_amp/test_amp_logging_config.py
+++ tests/test_amp/test_amp_logging_config.py
@@ -13,21 +13,21 @@ class TestAmpLoggingConfig(unittest.TestCase):
 
     def test_describe_logging(self):
         resp = self.client.describe_logging_configuration(workspaceId=self.workspace_id)
-        resp.should.have.key("loggingConfiguration").equals({})
+        assert resp["loggingConfiguration"] == {}
 
     def test_create_logging(self):
         resp = self.client.create_logging_configuration(
             workspaceId=self.workspace_id, logGroupArn="log/arn"
         )
-        resp.should.have.key("status").equals({"statusCode": "ACTIVE"})
+        assert resp["status"] == {"statusCode": "ACTIVE"}
 
         resp = self.client.describe_logging_configuration(
             workspaceId=self.workspace_id
         )["loggingConfiguration"]
-        resp.should.have.key("createdAt")
-        resp.should.have.key("logGroupArn").equals("log/arn")
-        resp.should.have.key("status").equals({"statusCode": "ACTIVE"})
-        resp.should.have.key("workspace").equals(self.workspace_id)
+        assert "createdAt" in resp
+        assert resp["logGroupArn"] == "log/arn"
+        assert resp["status"] == {"statusCode": "ACTIVE"}
+        assert resp["workspace"] == self.workspace_id
 
     def test_update_logging(self):
         self.client.create_logging_configuration(
@@ -37,13 +37,13 @@ class TestAmpLoggingConfig(unittest.TestCase):
         resp = self.client.update_logging_configuration(
             workspaceId=self.workspace_id, logGroupArn="log/arn2"
         )
-        resp.should.have.key("status").equals({"statusCode": "ACTIVE"})
+        assert resp["status"] == {"statusCode": "ACTIVE"}
 
         resp = self.client.describe_logging_configuration(
             workspaceId=self.workspace_id
         )["loggingConfiguration"]
-        resp.should.have.key("modifiedAt")
-        resp.should.have.key("logGroupArn").equals("log/arn2")
+        assert "modifiedAt" in resp
+        assert resp["logGroupArn"] == "log/arn2"
 
     def test_delete_logging(self):
         resp = self.client.create_logging_configuration(
@@ -53,4 +53,4 @@ class TestAmpLoggingConfig(unittest.TestCase):
         self.client.delete_logging_configuration(workspaceId=self.workspace_id)
 
         resp = self.client.describe_logging_configuration(workspaceId=self.workspace_id)
-        resp.should.have.key("loggingConfiguration").equals({})
+        assert resp["loggingConfiguration"] == {}
