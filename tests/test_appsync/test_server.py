56895eae1736264c4b97d99936c0a44f4a30bbb9
Bert Blommers <bblommers@users.noreply.github.com>
2023-06-09 10:10:11 +0000

    Techdebt: Replace sure with regular asserts in AppSync tests (#6381)

--- tests/test_appsync/test_server.py
+++ tests/test_appsync/test_server.py
@@ -1,5 +1,4 @@
 import json
-import sure  # noqa # pylint: disable=unused-import
 
 import moto.server as server
 
@@ -11,5 +10,5 @@ def test_appsync_list_tags_for_resource():
     resp = test_client.get(
         "/v1/tags/arn%3Aaws%3Aappsync%3Aus-east-1%3A123456789012%3Aapis%2Ff405dd93-855e-451d-ab00-7325b8e439c6?tagKeys=Description"
     )
-    resp.status_code.should.equal(200)
-    json.loads(resp.data).should.equals({"tags": {}})
+    assert resp.status_code == 200
+    assert json.loads(resp.data) == {"tags": {}}
