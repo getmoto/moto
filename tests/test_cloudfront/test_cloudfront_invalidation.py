71b6cd3c164187437c6b29a6f9a90152777c7b25
Daniel Gomes-Sebastiao <daniel.gomes@adscale.co.nz>
2023-06-10 14:55:09 +1200

    Cloudfront: correct create_invalidation API response

--- tests/test_cloudfront/test_cloudfront_invalidation.py
+++ tests/test_cloudfront/test_cloudfront_invalidation.py
@@ -5,7 +5,36 @@ import sure  # noqa # pylint: disable=unused-import
 
 
 @mock_cloudfront
-def test_create_invalidation():
+def test_create_invalidation_with_single_path():
+    client = boto3.client("cloudfront", region_name="us-west-1")
+    config = scaffold.example_distribution_config("ref")
+    resp = client.create_distribution(DistributionConfig=config)
+    dist_id = resp["Distribution"]["Id"]
+
+    resp = client.create_invalidation(
+        DistributionId=dist_id,
+        InvalidationBatch={
+            "Paths": {"Quantity": 1, "Items": ["/path1"]},
+            "CallerReference": "ref2",
+        },
+    )
+
+    resp.should.have.key("Location")
+    resp.should.have.key("Invalidation")
+
+    resp["Invalidation"].should.have.key("Id")
+    resp["Invalidation"].should.have.key("Status").equals("COMPLETED")
+    resp["Invalidation"].should.have.key("CreateTime")
+    resp["Invalidation"].should.have.key("InvalidationBatch").equals(
+        {
+            "Paths": {"Quantity": 1, "Items": ["/path1"]},
+            "CallerReference": "ref2",
+        }
+    )
+
+
+@mock_cloudfront
+def test_create_invalidation_with_multiple_paths():
     client = boto3.client("cloudfront", region_name="us-west-1")
     config = scaffold.example_distribution_config("ref")
     resp = client.create_distribution(DistributionConfig=config)
