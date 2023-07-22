d05eae7feddf536bdf5986f73febf5dcb30dfe8e
Brian Pandola <bpandola@gmail.com>
2023-07-22 03:55:57 -0700

    Fix code scanning alerts (#6551)
    * Escape period in URL regex
    
    * Fix capital letter range
    
    * Fix unintentional range and duplicate period in regex
    
    * Formatting

--- moto/backend_index.py
+++ moto/backend_index.py
@@ -110,9 +110,12 @@ backend_url_patterns = [
     ),
     (
         "meteringmarketplace",
-        re.compile("https?://metering.marketplace.(.+).amazonaws.com"),
+        re.compile("https?://metering.marketplace\\.(.+)\\.amazonaws.com"),
     ),
-    ("meteringmarketplace", re.compile("https?://aws-marketplace.(.+).amazonaws.com")),
+    (
+        "meteringmarketplace",
+        re.compile("https?://aws-marketplace\\.(.+)\\.amazonaws.com"),
+    ),
     ("mq", re.compile("https?://mq\\.(.+)\\.amazonaws\\.com")),
     ("opsworks", re.compile("https?://opsworks\\.us-east-1\\.amazonaws.com")),
     ("organizations", re.compile("https?://organizations\\.(.+)\\.amazonaws\\.com")),
