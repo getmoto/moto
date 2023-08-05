089ce697d0ee40bc32cc7a2942cfaee74db3a9e3
Karri Balk <kbalk@users.noreply.github.com>
2023-08-05 17:37:51 -0400

    Partial replacement #2 of sure with asserts in S3

--- tests/test_s3/test_s3_object_attributes.py
+++ tests/test_s3/test_s3_object_attributes.py
@@ -1,16 +1,18 @@
+from uuid import uuid4
+
 import boto3
 import pytest
+
 from moto import mock_s3
-from uuid import uuid4
 
 
 @mock_s3
 class TestS3ObjectAttributes:
     def setup_method(self, *args) -> None:  # pylint: disable=unused-argument
         self.bucket_name = str(uuid4())
-        self.s3 = boto3.resource("s3", region_name="us-east-1")
+        self.s3_resource = boto3.resource("s3", region_name="us-east-1")
         self.client = boto3.client("s3", region_name="us-east-1")
-        self.bucket = self.s3.Bucket(self.bucket_name)
+        self.bucket = self.s3_resource.Bucket(self.bucket_name)
         self.bucket.create()
 
         self.key = self.bucket.put_object(Key="mykey", Body=b"somedata")
--- tests/test_s3/test_s3_utils.py
+++ tests/test_s3/test_s3_utils.py
@@ -1,5 +1,6 @@
+from unittest.mock import patch
+
 import pytest
-from sure import expect
 from moto.s3.utils import (
     bucket_name_from_url,
     _VersionedKeyStore,
@@ -9,66 +10,63 @@ from moto.s3.utils import (
     compute_checksum,
     cors_matches_origin,
 )
-from unittest.mock import patch
 
 
 def test_base_url():
-    expect(bucket_name_from_url("https://s3.amazonaws.com/")).should.equal(None)
+    assert bucket_name_from_url("https://s3.amazonaws.com/") is None
 
 
 def test_localhost_bucket():
-    expect(bucket_name_from_url("https://wfoobar.localhost:5000/abc")).should.equal(
-        "wfoobar"
-    )
+    assert bucket_name_from_url("https://wfoobar.localhost:5000/abc") == "wfoobar"
 
 
 def test_localhost_without_bucket():
-    expect(bucket_name_from_url("https://www.localhost:5000/def")).should.equal(None)
+    assert bucket_name_from_url("https://www.localhost:5000/def") is None
 
 
 def test_force_ignore_subdomain_for_bucketnames():
     with patch("moto.s3.utils.S3_IGNORE_SUBDOMAIN_BUCKETNAME", True):
-        expect(
+        assert (
             bucket_name_from_url("https://subdomain.localhost:5000/abc/resource")
-        ).should.equal(None)
+            is None
+        )
 
 
 def test_versioned_key_store():
-    d = _VersionedKeyStore()
+    key_store = _VersionedKeyStore()
 
-    d.should.have.length_of(0)
+    assert not key_store
 
-    d["key"] = [1]
+    key_store["key"] = [1]
+    assert len(key_store) == 1
 
-    d.should.have.length_of(1)
+    key_store["key"] = 2
+    assert len(key_store) == 1
 
-    d["key"] = 2
-    d.should.have.length_of(1)
-
-    d.should.have.key("key").being.equal(2)
-
-    d.get.when.called_with("key").should.return_value(2)
-    d.get.when.called_with("badkey").should.return_value(None)
-    d.get.when.called_with("badkey", "HELLO").should.return_value("HELLO")
+    assert key_store["key"] == 2
+    assert key_store.get("key") == 2
+    assert key_store.get("badkey") is None
+    assert key_store.get("badkey", "HELLO") == "HELLO"
 
     # Tests key[
-    d.shouldnt.have.key("badkey")
-    d.__getitem__.when.called_with("badkey").should.throw(KeyError)
+    assert "badkey" not in key_store
+    with pytest.raises(KeyError):
+        _ = key_store["badkey"]
 
-    d.getlist("key").should.have.length_of(2)
-    d.getlist("key").should.be.equal([[1], 2])
-    d.getlist("badkey").should.be.none
+    assert len(key_store.getlist("key")) == 2
+    assert key_store.getlist("key") == [[1], 2]
+    assert key_store.getlist("badkey") is None
 
-    d.setlist("key", 1)
-    d.getlist("key").should.be.equal([1])
+    key_store.setlist("key", 1)
+    assert key_store.getlist("key") == [1]
 
-    d.setlist("key", (1, 2))
-    d.getlist("key").shouldnt.be.equal((1, 2))
-    d.getlist("key").should.be.equal([1, 2])
+    key_store.setlist("key", (1, 2))
+    assert key_store.getlist("key") != (1, 2)
+    assert key_store.getlist("key") == [1, 2]
 
-    d.setlist("key", [[1], [2]])
-    d["key"].should.have.length_of(1)
-    d.getlist("key").should.be.equal([[1], [2]])
+    key_store.setlist("key", [[1], [2]])
+    assert len(key_store["key"]) == 1
+    assert key_store.getlist("key") == [[1], [2]]
 
 
 def test_parse_region_from_url():
@@ -81,7 +79,7 @@ def test_parse_region_from_url():
         "https://s3.us-west-2.amazonaws.com/bucket",
         "https://bucket.s3-us-west-2.amazonaws.com",
     ]:
-        parse_region_from_url(url).should.equal(expected)
+        assert parse_region_from_url(url) == expected
 
     expected = "us-east-1"
     for url in [
@@ -90,7 +88,7 @@ def test_parse_region_from_url():
         "https://s3.amazonaws.com/bucket",
         "https://bucket.s3.amazonaws.com",
     ]:
-        parse_region_from_url(url).should.equal(expected)
+        assert parse_region_from_url(url) == expected
 
 
 @pytest.mark.parametrize(
@@ -105,7 +103,7 @@ def test_parse_region_from_url():
     ],
 )
 def test_clean_key_name(key, expected):
-    clean_key_name(key).should.equal(expected)
+    assert clean_key_name(key) == expected
 
 
 @pytest.mark.parametrize(
@@ -120,32 +118,32 @@ def test_clean_key_name(key, expected):
     ],
 )
 def test_undo_clean_key_name(key, expected):
-    undo_clean_key_name(key).should.equal(expected)
+    assert undo_clean_key_name(key) == expected
 
 
 def test_checksum_sha256():
     checksum = b"h9FJy0JMA4dlbyEdJYn7Wx4WIpkhMJ6YWIQZzMqKc2I="
-    compute_checksum(b"somedata", "SHA256").should.equal(checksum)
+    assert compute_checksum(b"somedata", "SHA256") == checksum
     # Unknown algorithms fallback to SHA256 for now
-    compute_checksum(b"somedata", algorithm="unknown").should.equal(checksum)
+    assert compute_checksum(b"somedata", algorithm="unknown") == checksum
 
 
 def test_checksum_sha1():
-    compute_checksum(b"somedata", "SHA1").should.equal(b"76oxGuRIpzdMEiBhv+2VLZQOnjc=")
+    assert compute_checksum(b"somedata", "SHA1") == b"76oxGuRIpzdMEiBhv+2VLZQOnjc="
 
 
 def test_checksum_crc32():
-    compute_checksum(b"somedata", "CRC32").should.equal(b"Uwy90A==")
+    assert compute_checksum(b"somedata", "CRC32") == b"Uwy90A=="
 
 
 def test_checksum_crc32c():
     try:
         import crc32c  # noqa # pylint: disable=unused-import
 
-        compute_checksum(b"somedata", "CRC32C").should.equal(b"dB9qBQ==")
+        assert compute_checksum(b"somedata", "CRC32C") == b"dB9qBQ=="
     except:  # noqa: E722 Do not use bare except
         # Optional library Can't be found - just revert to CRC32
-        compute_checksum(b"somedata", "CRC32C").should.equal(b"Uwy90A==")
+        assert compute_checksum(b"somedata", "CRC32C") == b"Uwy90A=="
 
 
 def test_cors_utils():
