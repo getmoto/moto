3727a0c0d60890dc8f34043cbdc0a105b3a32d1a
Joel McCoy <nbajoel@gmail.com>
2023-08-13 02:46:00 +0000

    one more type fix

--- moto/identitystore/models.py
+++ moto/identitystore/models.py
@@ -234,7 +234,7 @@ class IdentityStoreBackend(BaseBackend):
     @paginate(pagination_model=PAGINATION_MODEL)  # type: ignore
     def list_groups(
         self, identity_store_id: str, filters: List[Dict[str, str]]
-    ) -> List[dict[str, str]]:
+    ) -> List[Dict[str, str]]:
         identity_store = self.__get_identity_store(identity_store_id)
 
         if filters:
