82363e49e9fc7055376829e8daa617d63498fd54
Bert Blommers <bblommers@users.noreply.github.com>
2023-08-13 20:17:54 +0000

    Techdebt: Replace sure with regular assertions in MediaStore (#6653)

--- tests/test_mediastoredata/test_server.py
+++ tests/test_mediastoredata/test_server.py
@@ -1,4 +1,4 @@
-import sure  # noqa # pylint: disable=unused-import
+import json
 
 import moto.server as server
 from moto import mock_mediastoredata
@@ -12,5 +12,5 @@ Test the different server responses
 def test_mediastore_lists_containers():
     backend = server.create_backend_app("mediastore-data")
     test_client = backend.test_client()
-    response = test_client.get("/").data
-    response.should.contain(b'"Items": []')
+    body = test_client.get("/").data.decode("utf-8")
+    assert json.loads(body) == {"Items": []}
