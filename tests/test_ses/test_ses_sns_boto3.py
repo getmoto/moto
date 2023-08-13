a866d1b040e1cf128719d2003055e756d0c87fd1
Karri Balk <kbalk@users.noreply.github.com>
2023-08-13 12:34:02 -0400

    Missed an 'import sure' in ses

--- tests/test_ses/test_ses_sns_boto3.py
+++ tests/test_ses/test_ses_sns_boto3.py
@@ -2,7 +2,6 @@ import json
 
 import boto3
 
-import sure  # noqa # pylint: disable=unused-import
 from moto import mock_ses, mock_sns, mock_sqs
 from moto.ses.models import SESFeedback
 from moto.core import DEFAULT_ACCOUNT_ID as ACCOUNT_ID
