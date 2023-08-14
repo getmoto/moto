b9c848fd9cd67ed830dd66f0bd142eeca66cd2dc
kbalk <7536198+kbalk@users.noreply.github.com>
2023-08-14 05:54:27 -0400

    Techdebt: Replace sure with regular assertions in WAFv2 (#6657)

--- tests/test_wafv2/test_wafv2_rules.py
+++ tests/test_wafv2/test_wafv2_rules.py
@@ -6,4 +6,4 @@ from moto import mock_wafv2
 def test_list_rule_groups():
     client = boto3.client("wafv2", region_name="us-east-2")
     resp = client.list_rule_groups(Scope="CLOUDFRONT")
-    resp.should.have.key("RuleGroups").equals([])
+    assert resp["RuleGroups"] == []
