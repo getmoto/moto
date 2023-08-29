6031d71bad9cf752f0c2493efaf3a2e382d95ed8
Jean-Frederic Mainville <jfmainville@outlook.com>
2023-08-29 14:51:40 -0400

    refactor: add the CacheCluster pydantic BaseModel configuration

--- moto/elasticache/models.py
+++ moto/elasticache/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List, Optional, Dict, Any
 
 from moto.core import BaseBackend, BackendDict, BaseModel
@@ -28,6 +29,80 @@ class User(BaseModel):
         self.usergroupids: List[str] = []
         self.region = region
         self.arn = f"arn:aws:elasticache:{self.region}:{account_id}:user:{self.id}"
+
+
+class CacheCluster(BaseModel):
+    def __init__(
+            self,
+            cache_cluster_id: str,
+            replication_group_id: Optional[str],
+            az_mode: Optional[str],
+            preferred_availability_zone: Optional[str],
+            num_cache_nodes: Optional[int],
+            cache_node_type: Optional[str],
+            engine: Optional[str],
+            engine_version: Optional[str],
+            cache_parameter_group_name: Optional[str],
+            cache_subnet_group_name: Optional[str],
+            transit_encryption_enabled: Optional[bool],
+            network_type: Optional[str],
+            ip_discovery: Optional[str],
+            snapshot_name: Optional[str],
+            preferred_maintenance_window: Optional[str],
+            port: Optional[int],
+            notification_topic_arn: Optional[str],
+            auto_minor_version_upgrade: Optional[bool],
+            snapshot_retention_limit: Optional[int],
+            snapshot_window: Optional[str],
+            auth_token: Optional[str],
+            outpost_mode: Optional[str],
+            preferred_outpost_arn: Optional[str],
+            preferred_availability_zones: Optional[List[str]],
+            cache_security_group_names: Optional[List[str]],
+            security_group_ids: Optional[List[str]],
+            tags: Optional[Dict[str, str]],
+            snapshot_arns: Optional[List[str]],
+            preferred_outpost_arns: Optional[List[str]],
+            log_delivery_configurations: List[Dict[str, Any]],
+    ):
+        if tags is None:
+            tags = dict()
+
+        self.cache_cluster_id = cache_cluster_id
+        self.replication_group_id = replication_group_id
+        self.az_mode = az_mode
+        self.preferred_availability_zone = preferred_availability_zone
+        self.preferred_availability_zones = preferred_availability_zones
+        self.engine = engine or "redis"
+        self.engine_version = engine_version
+        if self.engine == "redis":
+            self.num_cache_nodes = 1
+        else:
+            self.num_cache_nodes = num_cache_nodes or 1
+        self.cache_node_type = cache_node_type
+        self.cache_parameter_group_name = cache_parameter_group_name
+        self.cache_subnet_group_name = cache_subnet_group_name
+        self.cache_security_group_names = cache_security_group_names
+        self.security_group_ids = security_group_ids
+        self.tags = tags
+        self.snapshot_arns = snapshot_arns
+        self.snapshot_name = snapshot_name
+        self.preferred_maintenance_window = preferred_maintenance_window
+        self.port = port or 6379
+        self.notification_topic_arn = notification_topic_arn
+        self.auto_minor_version_upgrade = auto_minor_version_upgrade
+        self.snapshot_retention_limit = snapshot_retention_limit
+        self.snapshot_window = snapshot_window
+        self.auth_token = auth_token
+        self.outpost_mode = outpost_mode
+        self.preferred_outpost_arn = preferred_outpost_arn
+        self.preferred_outpost_arns = preferred_outpost_arns
+        self.log_delivery_configurations = log_delivery_configurations
+        self.transit_encryption_enabled = transit_encryption_enabled
+        self.network_type = network_type
+        self.ip_discovery = ip_discovery
+        self.cache_cluster_create_time = datetime.utcnow()
+        self.cache_cluster_status = "available"
 
 
 class ElastiCacheBackend(BaseBackend):
