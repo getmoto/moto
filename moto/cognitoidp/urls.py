14d981ca717306450dbf836ae9c59efde67631ac
Daniel Fangl <daniel.fangl@gmail.com>
2023-08-31 11:48:15 +0200

    fix wrong refactoring

--- moto/cognitoidp/urls.py
+++ moto/cognitoidp/urls.py
@@ -4,7 +4,7 @@ url_bases = [r"https?://cognito-idp\.(.+)\.amazonaws.com"]
 
 url_paths = {
     "{0}/$": CognitoIdpResponse.dispatch,
-    "{0}/(?P<user_pool_id>[^/]+)/.well-known/jwks.json$": CognitoIdpJsonWebKeyResponse.dispatch(
+    "{0}/(?P<user_pool_id>[^/]+)/.well-known/jwks.json$": CognitoIdpJsonWebKeyResponse.method_dispatch(
         CognitoIdpJsonWebKeyResponse.serve_json_web_key
     ),
 }
--- moto/instance_metadata/urls.py
+++ moto/instance_metadata/urls.py
@@ -3,7 +3,7 @@ from .responses import InstanceMetadataResponse
 url_bases = ["http://169.254.169.254"]
 
 url_paths = {
-    "{0}/(?P<path>.+)": InstanceMetadataResponse.dispatch(
+    "{0}/(?P<path>.+)": InstanceMetadataResponse.method_dispatch(
         InstanceMetadataResponse.metadata_response
     )
 }
