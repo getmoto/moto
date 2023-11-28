8ad0cf69589f09eb1c0700a71a1dfdbf1bcfb33b
Alan F. <fuentes.j.alan@gmail.com>
2023-11-27 23:14:06 -0500

    fix: sns tests linting error

--- tests/test_sns/test_sns.py
+++ tests/test_sns/test_sns.py
@@ -1,7 +1,5 @@
 """Unit tests for sns-supported APIs."""
-import boto3
 
-from moto import mock_sns
 
 # See our Development Tips on writing tests for hints on how to write good tests:
 # http://docs.getmoto.org/en/latest/docs/contributing/development_tips/tests.html
