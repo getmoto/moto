ecf3de7851d30c37863f6f7a2f4b0fa41839c672
afuentes-19 <62731482+afuentes-19@users.noreply.github.com>
2023-11-28 17:29:57 -0500

    Add ACM support for ResourceGroupsTaggingAPI (#7010)

---
+++ tests/test_sns/test_sns.py
@@ -0,0 +1,5 @@
+"""Unit tests for sns-supported APIs."""
+
+
+# See our Development Tips on writing tests for hints on how to write good tests:
+# http://docs.getmoto.org/en/latest/docs/contributing/development_tips/tests.html
