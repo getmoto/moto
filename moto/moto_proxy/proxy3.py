bee9e7252fcf649b732b85eb44fbfd4d9189a249
Stephen Morton <git@tungol.org>
2023-12-04 23:45:11 -0800

    botocore-stubs fixed typing for AWSPreparedRequest

--- moto/moto_proxy/proxy3.py
+++ moto/moto_proxy/proxy3.py
@@ -68,7 +68,7 @@ class MotoRequestHandler:
         handler = self.get_handler_for_host(host=host, path=path)
         full_url = host + path
         request = AWSPreparedRequest(
-            method, full_url, headers, body, stream_output=False  # type: ignore[arg-type]
+            method, full_url, headers, body, stream_output=False
         )
         request.form_data = form_data  # type: ignore[attr-defined]
         return handler(request, full_url, headers)
