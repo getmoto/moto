cdd9cd8fdd7174180aaf1b300506f8909c8616f6
Joel McCoy <nbajoel@gmail.com>
2024-01-04 09:27:48 -0600

    Implements sso-admin AWS Managed Policies  (#7184)

--- moto/ssoadmin/exceptions.py
+++ moto/ssoadmin/exceptions.py
@@ -3,9 +3,30 @@ from moto.core.exceptions import JsonRESTError
 
 
 class ResourceNotFoundException(JsonRESTError):
-    def __init__(self, message: str = "Account not found") -> None:
+    code = 400
+
+    def __init__(self, message: str = "") -> None:
         super().__init__(
             error_type="ResourceNotFoundException",
             message=message,
-            code="ResourceNotFoundException",
+        )
+
+
+class ConflictException(JsonRESTError):
+    code = 400
+
+    def __init__(self, message: str = "") -> None:
+        super().__init__(
+            error_type="ConflictException",
+            message=message,
+        )
+
+
+class ServiceQuotaExceededException(JsonRESTError):
+    code = 400
+
+    def __init__(self, message: str = "") -> None:
+        super().__init__(
+            error_type="ServiceQuotaExceededException",
+            message=message,
         )
