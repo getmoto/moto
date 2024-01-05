57bc607307c76f277b3b39da3282be9a2007fe12
Bert Blommers <info@bertblommers.nl>
2024-01-05 23:40:46 +0000

    Techdebt: Simplify MQ routing (#7190)

--- moto/mq/urls.py
+++ moto/mq/urls.py
@@ -7,25 +7,13 @@ url_bases = [
 
 
 url_paths = {
-    "{0}/v1/brokers/(?P<broker_id>[^/]+)$": MQResponse.method_dispatch(
-        MQResponse.broker
-    ),
-    "{0}/v1/brokers/(?P<broker_id>[^/]+)/reboot$": MQResponse.method_dispatch(
-        MQResponse.reboot
-    ),
-    "{0}/v1/brokers/(?P<broker_id>[^/]+)/users$": MQResponse.method_dispatch(
-        MQResponse.users
-    ),
-    "{0}/v1/brokers/(?P<broker_id>[^/]+)/users/(?P<user_name>[^/]+)$": MQResponse.method_dispatch(
-        MQResponse.user
-    ),
-    "{0}/v1/brokers$": MQResponse.method_dispatch(MQResponse.brokers),
-    "{0}/v1/configurations$": MQResponse.method_dispatch(MQResponse.configurations),
-    "{0}/v1/configurations/(?P<config_id>[^/]+)$": MQResponse.method_dispatch(
-        MQResponse.configuration
-    ),
-    "{0}/v1/configurations/(?P<config_id>[^/]+)/revisions/(?P<revision_id>[^/]+)$": MQResponse.method_dispatch(
-        MQResponse.configuration_revision
-    ),
-    "{0}/v1/tags/(?P<resource_arn>[^/]+)$": MQResponse.method_dispatch(MQResponse.tags),
+    "{0}/v1/brokers/(?P<broker_id>[^/]+)$": MQResponse.dispatch,
+    "{0}/v1/brokers/(?P<broker_id>[^/]+)/reboot$": MQResponse.dispatch,
+    "{0}/v1/brokers/(?P<broker_id>[^/]+)/users$": MQResponse.dispatch,
+    "{0}/v1/brokers/(?P<broker_id>[^/]+)/users/(?P<user_name>[^/]+)$": MQResponse.dispatch,
+    "{0}/v1/brokers$": MQResponse.dispatch,
+    "{0}/v1/configurations$": MQResponse.dispatch,
+    "{0}/v1/configurations/(?P<config_id>[^/]+)$": MQResponse.dispatch,
+    "{0}/v1/configurations/(?P<config_id>[^/]+)/revisions/(?P<revision_id>[^/]+)$": MQResponse.dispatch,
+    "{0}/v1/tags/(?P<resource_arn>[^/]+)$": MQResponse.dispatch,
 }
