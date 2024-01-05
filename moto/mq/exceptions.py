590ddca37aec854717aeb3b9967a3652dc028d90
Bert Blommers <info@bertblommers.nl>
2024-01-05 19:53:59 -0100

    Techdebt: Simplify MQ routing

--- moto/mq/exceptions.py
+++ moto/mq/exceptions.py
@@ -1,5 +1,4 @@
 import json
-from typing import Any
 
 from moto.core.exceptions import JsonRESTError
 
@@ -11,58 +10,38 @@ class MQError(JsonRESTError):
 class UnknownBroker(MQError):
     def __init__(self, broker_id: str):
         super().__init__("NotFoundException", "Can't find requested broker")
-        self.broker_id = broker_id
-
-    def get_body(
-        self, *args: Any, **kwargs: Any
-    ) -> str:  # pylint: disable=unused-argument
         body = {
             "errorAttribute": "broker-id",
-            "message": f"Can't find requested broker [{self.broker_id}]. Make sure your broker exists.",
+            "message": f"Can't find requested broker [{broker_id}]. Make sure your broker exists.",
         }
-        return json.dumps(body)
+        self.description = json.dumps(body)
 
 
 class UnknownConfiguration(MQError):
     def __init__(self, config_id: str):
         super().__init__("NotFoundException", "Can't find requested configuration")
-        self.config_id = config_id
-
-    def get_body(
-        self, *args: Any, **kwargs: Any
-    ) -> str:  # pylint: disable=unused-argument
         body = {
             "errorAttribute": "configuration_id",
-            "message": f"Can't find requested configuration [{self.config_id}]. Make sure your configuration exists.",
+            "message": f"Can't find requested configuration [{config_id}]. Make sure your configuration exists.",
         }
-        return json.dumps(body)
+        self.description = json.dumps(body)
 
 
 class UnknownUser(MQError):
     def __init__(self, username: str):
         super().__init__("NotFoundException", "Can't find requested user")
-        self.username = username
-
-    def get_body(
-        self, *args: Any, **kwargs: Any
-    ) -> str:  # pylint: disable=unused-argument
         body = {
             "errorAttribute": "username",
-            "message": f"Can't find requested user [{self.username}]. Make sure your user exists.",
+            "message": f"Can't find requested user [{username}]. Make sure your user exists.",
         }
-        return json.dumps(body)
+        self.description = json.dumps(body)
 
 
 class UnknownEngineType(MQError):
     def __init__(self, engine_type: str):
         super().__init__("BadRequestException", "")
-        self.engine_type = engine_type
-
-    def get_body(
-        self, *args: Any, **kwargs: Any
-    ) -> str:  # pylint: disable=unused-argument
         body = {
             "errorAttribute": "engineType",
-            "message": f"Broker engine type [{self.engine_type}] is invalid. Valid values are: [ACTIVEMQ]",
+            "message": f"Broker engine type [{engine_type}] is invalid. Valid values are: [ACTIVEMQ]",
         }
-        return json.dumps(body)
+        self.description = json.dumps(body)
