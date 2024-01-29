cc912e09ef610b3ebe28c9ecaacc255b6f6a3e77
Anders Kaseorg <andersk@mit.edu>
2024-01-28 16:01:00 -0800

    Mark mock_aws for explicit reexport
    This is required for mypy to recognize this as exported with
    --no-implicit-reexport (implied by --strict).
    
    https://mypy.readthedocs.io/en/stable/command_line.html#cmdoption-mypy-no-implicit-reexport
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- moto/__init__.py
+++ moto/__init__.py
@@ -1,4 +1,4 @@
-from moto.core.decorator import mock_aws  # noqa  # pylint: disable=unused-import
+from moto.core.decorator import mock_aws as mock_aws
 
 __title__ = "moto"
 __version__ = "5.0.1.dev"
