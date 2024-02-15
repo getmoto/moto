6935558d918b472128804f975d307db6f8f1289e
Bert Blommers <info@bertblommers.nl>
2024-02-15 19:56:17 -0100

    SES: Correct Exception format + Java tests

--- moto/ses/exceptions.py
+++ moto/ses/exceptions.py
@@ -1,100 +1,82 @@
+from typing import Any
+
 from moto.core.exceptions import RESTError
 
 
-class MessageRejectedError(RESTError):
+class SesError(RESTError):
     code = 400
 
+    def __init__(self, error_type: str, message: str, *args: Any, **kwargs: Any):
+        kwargs.setdefault("template", "wrapped_single_error")
+        super().__init__(error_type, message, *args, **kwargs)
+
+
+class MessageRejectedError(SesError):
     def __init__(self, message: str):
         super().__init__("MessageRejected", message)
 
 
-class ConfigurationSetDoesNotExist(RESTError):
-    code = 400
-
+class ConfigurationSetDoesNotExist(SesError):
     def __init__(self, message: str):
         super().__init__("ConfigurationSetDoesNotExist", message)
 
 
-class ConfigurationSetAlreadyExists(RESTError):
-    code = 400
-
+class ConfigurationSetAlreadyExists(SesError):
     def __init__(self, message: str):
         super().__init__("ConfigurationSetAlreadyExists", message)
 
 
-class EventDestinationAlreadyExists(RESTError):
-    code = 400
-
+class EventDestinationAlreadyExists(SesError):
     def __init__(self, message: str):
         super().__init__("EventDestinationAlreadyExists", message)
 
 
-class TemplateNameAlreadyExists(RESTError):
-    code = 400
-
+class TemplateNameAlreadyExists(SesError):
     def __init__(self, message: str):
         super().__init__("TemplateNameAlreadyExists", message)
 
 
-class ValidationError(RESTError):
-    code = 400
-
+class ValidationError(SesError):
     def __init__(self, message: str):
         super().__init__("ValidationError", message)
 
 
-class InvalidParameterValue(RESTError):
-    code = 400
-
+class InvalidParameterValue(SesError):
     def __init__(self, message: str):
         super().__init__("InvalidParameterValue", message)
 
 
-class InvalidRenderingParameterException(RESTError):
-    code = 400
-
+class InvalidRenderingParameterException(SesError):
     def __init__(self, message: str):
         super().__init__("InvalidRenderingParameterException", message)
 
 
-class TemplateDoesNotExist(RESTError):
-    code = 400
-
+class TemplateDoesNotExist(SesError):
     def __init__(self, message: str):
         super().__init__("TemplateDoesNotExist", message)
 
 
-class RuleSetNameAlreadyExists(RESTError):
-    code = 400
-
+class RuleSetNameAlreadyExists(SesError):
     def __init__(self, message: str):
         super().__init__("RuleSetNameAlreadyExists", message)
 
 
-class RuleAlreadyExists(RESTError):
-    code = 400
-
+class RuleAlreadyExists(SesError):
     def __init__(self, message: str):
         super().__init__("RuleAlreadyExists", message)
 
 
-class RuleSetDoesNotExist(RESTError):
-    code = 400
-
+class RuleSetDoesNotExist(SesError):
     def __init__(self, message: str):
         super().__init__("RuleSetDoesNotExist", message)
 
 
-class RuleDoesNotExist(RESTError):
-    code = 400
-
+class RuleDoesNotExist(SesError):
     def __init__(self, message: str):
         super().__init__("RuleDoesNotExist", message)
 
 
-class MissingRenderingAttributeException(RESTError):
-    code = 400
-
+class MissingRenderingAttributeException(SesError):
     def __init__(self, var: str):
         super().__init__(
             "MissingRenderingAttributeException",
