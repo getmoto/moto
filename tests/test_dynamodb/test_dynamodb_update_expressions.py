7c87bddeae2e222f29def0aa73735baac1d32eb6
Bert Blommers <info@bertblommers.nl>
2024-02-19 20:54:02 +0000

    DynamoDB: Fix calculation when adding/subtracting decimals (#7365)

--- tests/test_dynamodb/test_dynamodb_update_expressions.py
+++ tests/test_dynamodb/test_dynamodb_update_expressions.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 import boto3
 import pytest
 
@@ -40,3 +42,50 @@ def test_update_different_map_elements_in_single_request(table_name=None):
         ExpressionAttributeValues={":MyCount": 5},
     )
     assert table.get_item(Key={"pk": "example_id"})["Item"]["MyTotalCount"] == 5
+
+
+@pytest.mark.aws_verified
+@dynamodb_aws_verified()
+def test_update_item_add_float(table_name=None):
+    table = boto3.resource("dynamodb", "us-east-1").Table(table_name)
+
+    # DECIMAL - DECIMAL
+    table.put_item(Item={"pk": "foo", "amount": Decimal(100), "nr": 5})
+    table.update_item(
+        Key={"pk": "foo"},
+        UpdateExpression="ADD amount :delta",
+        ExpressionAttributeValues={":delta": -Decimal("88.3")},
+    )
+    assert table.scan()["Items"][0]["amount"] == Decimal("11.7")
+
+    # DECIMAL + DECIMAL
+    table.update_item(
+        Key={"pk": "foo"},
+        UpdateExpression="ADD amount :delta",
+        ExpressionAttributeValues={":delta": Decimal("25.41")},
+    )
+    assert table.scan()["Items"][0]["amount"] == Decimal("37.11")
+
+    # DECIMAL + INT
+    table.update_item(
+        Key={"pk": "foo"},
+        UpdateExpression="ADD amount :delta",
+        ExpressionAttributeValues={":delta": 6},
+    )
+    assert table.scan()["Items"][0]["amount"] == Decimal("43.11")
+
+    # INT + INT
+    table.update_item(
+        Key={"pk": "foo"},
+        UpdateExpression="ADD nr :delta",
+        ExpressionAttributeValues={":delta": 1},
+    )
+    assert table.scan()["Items"][0]["nr"] == Decimal("6")
+
+    # INT + DECIMAL
+    table.update_item(
+        Key={"pk": "foo"},
+        UpdateExpression="ADD nr :delta",
+        ExpressionAttributeValues={":delta": Decimal("25.41")},
+    )
+    assert table.scan()["Items"][0]["nr"] == Decimal("31.41")
