28fd19bf8df43d26349f67fcc8fbf044da9840bd
Samuel Atkins-Turkish <samuel@fundapps.co>
2024-02-22 13:28:26 +0000

    fix: updated new should contain whole updated list if any item changed
    passes test for seemingly existing issue unrelated to this branch

--- moto/dynamodb/responses.py
+++ moto/dynamodb/responses.py
@@ -964,12 +964,14 @@ class DynamoHandler(BaseResponse):
                 if len(changed) != len(original):
                     return changed
                 else:
-                    return [
+                    any_element_has_changed = any(
                         self._build_updated_new_attributes(
                             original[index], changed[index]
                         )
+                        != original[index]
                         for index in range(len(changed))
-                    ]
+                    )
+                    return changed if any_element_has_changed else original
             else:
                 return changed
 
