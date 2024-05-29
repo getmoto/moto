b19afe832dcad1fe77d4ffe5b75d34982dfab80a
Bert Blommers <info@bertblommers.nl>
2024-05-29 20:45:09 +0000

    IAM: create_policy() should return tags in correct format (#7728)

---
+++ other_langs/terraform/iam/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+
+  tags = {
+    t1 = "v1"
+  }
+}
