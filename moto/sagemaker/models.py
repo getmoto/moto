25c9ac557e5ca883436f1aa0aa371c59e78f83ab
jamarcelin <jamarcelin24@gmail.com>
2024-07-24 17:24:08 -0400

    added sagemaker data quality definition support

--- moto/sagemaker/models.py
+++ moto/sagemaker/models.py
@@ -85,6 +85,12 @@ PAGINATION_MODEL = {
         "limit_default": 100,
         "unique_attribute": "cluster_node_arn",
     },
+    "list_data_quality_job_definitions": {
+        "input_token": "next_token",
+        "limit_key": "max_results",
+        "limit_default": 100,
+        "unique_attribute": "arn",
+    }
 }
 
 METRIC_INFO_TYPE = Dict[str, Union[str, int, float, datetime]]
@@ -3964,6 +3970,118 @@ class SageMakerModelBackend(BaseBackend):
                 nodes_list, key=lambda x: x.launch_time, reverse=reverse
             )
         return nodes_list
+
+    def create_data_quality_job_definition(
+        self,
+        account_id: str,
+        job_definition_name: str,
+        tags: List[Dict[str, str]] = [],
+        role_arn: str = "",
+        job_resources: Optional[Dict[str, Any]] = None,
+        stopping_condition: Optional[Dict[str, Any]] = None,
+        environment: Optional[Dict[str, str]] = None,
+        network_config: Optional[Dict[str, Any]] = None,
+        data_quality_baseline_config: Optional[Dict[str, Any]] = None,
+        data_quality_app_specification: Optional[Dict[str, Any]] = None,
+        data_quality_job_input: Optional[Dict[str, Any]] = None,
+        data_quality_job_output_config: Optional[Dict[str, Any]] = None,
+    ) -> dict[str, str]:
+        job_definition = FakeDataQualityJobDefinition(
+            account_id=account_id,
+            region_name=self.region_name,
+            job_definition_name=job_definition_name,
+            tags=tags,
+            role_arn=role_arn,
+            job_resources=job_resources,
+            stopping_condition=stopping_condition,
+            environment=environment,
+            network_config=network_config,
+            data_quality_baseline_config=data_quality_baseline_config,
+            data_quality_app_specification=data_quality_app_specification,
+            data_quality_job_input=data_quality_job_input,
+            data_quality_job_output_config=data_quality_job_output_config,
+        )
+        self.data_quality_job_definitions[job_definition_name] = job_definition
+        return job_definition.response_create
+
+    @paginate(pagination_model=PAGINATION_MODEL)
+    def list_data_quality_job_definitions(self) -> List[Dict[str, str]]:
+        return [job.summary_object for job in self.data_quality_job_definitions.values()]
+
+    def describe_data_quality_job_definition(self, job_definition_name: str) -> Dict[str, Any]:
+        job_definition = self.data_quality_job_definitions.get(
+            job_definition_name)
+        if job_definition is None:
+            raise ResourceNotFound(
+                f"Job definition {job_definition_name} not found")
+        return job_definition.response_object
+
+    def delete_data_quality_job_definition(self, job_definition_name: str) -> None:
+        if job_definition_name in self.data_quality_job_definitions:
+            del self.data_quality_job_definitions[job_definition_name]
+        else:
+            raise ResourceNotFound(
+                f"Job definition {job_definition_name} not found")
+
+
+class FakeDataQualityJobDefinition(BaseObject):
+    def __init__(
+        self,
+        account_id: str,
+        region_name: str,
+        job_definition_name: str,
+        tags: List[Dict[str, str]] = [],
+        role_arn: str = "",
+        job_resources: Optional[Dict[str, Any]] = None,
+        stopping_condition: Optional[Dict[str, Any]] = None,
+        environment: Optional[Dict[str, str]] = None,
+        network_config: Optional[Dict[str, Any]] = None,
+        data_quality_baseline_config: Optional[Dict[str, Any]] = None,
+        data_quality_app_specification: Optional[Dict[str, Any]] = None,
+        data_quality_job_input: Optional[Dict[str, Any]] = None,
+        data_quality_job_output_config: Optional[Dict[str, Any]] = None,
+    ):
+        self.job_definition_name = job_definition_name
+        self.job_definition_arn = FakeDataQualityJobDefinition.arn_formatter(
+            job_definition_name, account_id, region_name
+        )
+        self.tags = tags
+        self.role_arn = role_arn
+        self.job_resources = job_resources or {}
+        self.stopping_condition = stopping_condition or {}
+        self.environment = environment or {}
+        self.network_config = network_config or {}
+        self.data_quality_baseline_config = data_quality_baseline_config or {}
+        self.data_quality_app_specification = data_quality_app_specification or {}
+        self.data_quality_job_input = data_quality_job_input or {}
+        self.data_quality_job_output_config = data_quality_job_output_config or {}
+        self.creation_time = self.last_modified_time = datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+    @property
+    def response_object(self) -> Dict[str, str]:
+        response_object = self.gen_response_object()
+        return {
+            k: v for k, v in response_object.items() if v is not None and v != [None]
+        }
+
+    @property
+    def response_create(self) -> Dict[str, str]:
+        return {"JobDefinitionArn": self.job_definition_arn}
+
+    @staticmethod
+    def arn_formatter(name: str, account_id: str, region: str) -> str:
+        return arn_formatter("data-quality-job-definition", name, account_id, region)
+
+    @property
+    def summary_object(self) -> Dict[str, str]:
+        return {
+            "MonitoringJobDefinitionName": self.job_definition_name,
+            "MonitoringJobDefinitionArn": self.job_definition_arn,
+            "CreationTime": self.creation_time,
+            "EndpointName": "EndpointName",
+        }
 
 
 class FakeExperiment(BaseObject):
