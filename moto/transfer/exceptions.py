3c6674e711e43ea5909da27f63b7227cf4a8262b
armichaud <email@alexandermichaud.tech>
2024-07-25 10:53:39 -0400

    need to key users with server_id

--- moto/transfer/exceptions.py
+++ moto/transfer/exceptions.py
@@ -1,3 +1,12 @@
 """Exceptions raised by the transfer service."""
 from moto.core.exceptions import JsonRESTError
 
+class TransferError(JsonRESTError):
+    code = 400
+
+class UserNotFound(TransferError):
+    def __init__(self, user_name: str) -> None:
+        super().__init__(
+            "UserNotFound",
+            f"{user_name} does not match any user associated with an FTP-enabled server."
+        )
--- moto/transfer/models.py
+++ moto/transfer/models.py
@@ -1,6 +1,6 @@
 """TransferBackend class with methods for supported APIs."""
 
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 from moto.core.base_backend import BaseBackend, BackendDict
 from moto.core.common_models import BaseModel
 
@@ -11,7 +11,7 @@ class TransferBackend(BaseBackend):
 
     def __init__(self, region_name, account_id):
         super().__init__(region_name, account_id)
-        self.users: List[User] = []
+        self.users: Dict[str, User] = []
 
     def create_user(
         self, 
@@ -25,7 +25,7 @@ class TransferBackend(BaseBackend):
         ssh_public_key_body: Optional[str], 
         tags: Optional[List[Dict[str,str]]], 
         user_name: str
-    ):
+    ) -> Tuple[str, str]:
         ssh_public_keys: List[SshPublicKey] = [
             {
                 'DateImported': datetime.now().strftime('%Y%m%d%H%M%S'),
@@ -34,27 +34,37 @@ class TransferBackend(BaseBackend):
             }
         ]
         user = User(
-            HomeDirectory=home_directory
-            HomeDirectoryType=home_directory_type
-            Policy=policy
-            PosixProfile=posix_profile
-            Role=role
-            SshPublicKeys=ssh_public_keys
-            Tags=tags
+            HomeDirectory=home_directory,
+            HomeDirectoryType=home_directory_type,
+            Policy=policy,
+            PosixProfile=posix_profile,
+            Role=role,
+            SshPublicKeys=ssh_public_keys,
+            Tags=tags,
             UserName=user_name
         )
+        self.users[user_name] = user
         return server_id, user_name
     
-    def describe_user(self, server_id, user_name):
-        # implement here
+    def describe_user(self, server_id: str, user_name: str) -> Tuple[str, User]:
+        user = self.get(user_name)
         return server_id, user
     
-    def delete_user(self, server_id, user_name):
+    def delete_user(
+        self, 
+        server_id: str, 
+        user_name: str
+    ) -> None:
         # implement here
         return 
     
-    def import_ssh_public_key(self, server_id, ssh_public_key_body, user_name):
-        # implement here
+    def import_ssh_public_key(
+        self, 
+        server_id: str, 
+        ssh_public_key_body: str, 
+        user_name: str
+    ) -> Tuple[str, str, str]:
+        ssh_public_key_id = "TODO"
         return server_id, ssh_public_key_id, user_name
     
 
