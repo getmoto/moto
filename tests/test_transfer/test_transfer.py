95468ba6afa5e520c4f8824867e0d82883109ae2
armichaud <email@alexandermichaud.tech>
2024-07-27 22:45:44 -0400

    Ready to run tests

--- tests/test_transfer/test_transfer.py
+++ tests/test_transfer/test_transfer.py
@@ -1,36 +1,76 @@
 """Unit tests for transfer-supported APIs."""
+
 import boto3
+import pytest
 
 from moto import mock_aws
+from moto.transfer.exceptions import UserNotFound
 
-# See our Development Tips on writing tests for hints on how to write good tests:
-# http://docs.getmoto.org/en/latest/docs/contributing/development_tips/tests.html
 
-@mock_aws
-def test_create_user():
-    client = boto3.client("transfer", region_name="ap-southeast-1")
-    resp = client.create_user()
-
-    raise Exception("NotYetImplemented")
+@pytest.fixture(name="client")
+def fixture_transfer_client():
+    with mock_aws():
+        yield boto3.client("transfer", region_name="us-east-1")
 
 
 @mock_aws
-def test_describe_user():
-    client = boto3.client("transfer", region_name="ap-southeast-1")
-    resp = client.describe_user()
+def test_create_and_describe_user(client):
+    connection = client.create_user(
+        HomeDirectory="/Users/mock_user",
+        HomeDirectoryType="PATH",
+        HomeDirectoryMappings=[{ "Entry": "/directory1", "Target": "/bucket_name/home/mydirectory" }],
+        Policy="MockPolicy",
+        PosixProfile={
+            'Uid': 0,
+            'Gid': 1,
+        },
+        Role="TransferAdmin",
+        ServerId="1234",
+        SshPublicKeyBody="ED25519",
+        Tags=[{'Owner': 'MotoUser1337'}],
+        UserName="test_user"
+    )
+    user_name = connection["userName"]
+    server_id = connection["serverId"]
 
-    raise Exception("NotYetImplemented")
+    assert user_name == "test_user"
+    assert server_id == "1234"
+
+    connection = client.describe_user(
+        user_name=user_name,
+        server_id=server_id
+    )
+
+    assert connection["serverId"] == server_id
+    user = connection["user"]
+    assert user["HomeDirectory"] == "/Users/mock_user"
+    assert user["HomeDirectoryType"] == "PATH"
+    assert user["HomeDirectoryMappings"]["Entry"] == "/directory1"
+    assert user["HomeDirectoryMappings"]["Target"] == "/bucket_name/home/mydirectory"
+    assert user["Policy"] == "MockPolicy"
+    assert user["PosixProfile"]['Uid'] == 0
+    assert user["PosixProfile"]['Gid'] == 1
+    assert user["Role"] == "TransferAdmin"
+    assert user["ServerId"] == "1234"
+    assert user["SshPublicKeys"][0]["SshPublicKeyBody"] == "ED25519"
+    assert user["Tags"]["Owner"] == "MotoUser1337"
+    assert user["UserName"] == "test_user"
+    
+    client.delete_user(
+        user_name=user_name,
+        server_id=server_id
+    )
+    
+    with pytest.raises(UserNotFound):
+        client.describe_user(
+            user_name=user_name,
+            server_id=server_id
+        )
+       
 
 
 @mock_aws
-def test_delete_user():
-    client = boto3.client("transfer", region_name="eu-west-1")
-    resp = client.delete_user()
-
-    raise Exception("NotYetImplemented")
-
-
-@mock_aws
+@pytest.mark.skip(reason="TODO")
 def test_import_ssh_public_key():
     client = boto3.client("transfer", region_name="us-east-2")
     resp = client.import_ssh_public_key()
@@ -39,8 +79,9 @@ def test_import_ssh_public_key():
 
 
 @mock_aws
+@pytest.mark.skip(reason="TODO")
 def test_delete_ssh_public_key():
-    client = boto3.client("transfer", region_name="ap-southeast-1")
+    client = boto3.client("transfer", region_name="us-east-1")
     resp = client.delete_ssh_public_key()
 
     raise Exception("NotYetImplemented")
