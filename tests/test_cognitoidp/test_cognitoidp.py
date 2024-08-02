08b4782a5e49f3dd890911bc392f257504d74696
Daniel Ballard <daniel.ballard@hotmail.com>
2024-08-02 17:42:51 -0400

    challenge software_token_mfa for initiate_auth of USER_PASSWORD_AUTH

--- tests/test_cognitoidp/test_cognitoidp.py
+++ tests/test_cognitoidp/test_cognitoidp.py
@@ -4158,6 +4158,48 @@ def test_initiate_auth_USER_PASSWORD_AUTH():
 
 
 @mock_aws
+def test_initiate_auth_USER_PASSWORD_AUTH_when_software_token_mfa_enabled():
+    conn = boto3.client("cognito-idp", "us-west-2")
+
+    result = user_authentication_flow(conn)
+
+    user_pool_id = result["user_pool_id"]
+    username = result["username"]
+    password = result["password"]
+    client_id = result["client_id"]
+    secret_hash = result["secret_hash"]
+
+    result = conn.admin_get_user(UserPoolId=user_pool_id, Username=username)
+    assert result["PreferredMfaSetting"] == "SOFTWARE_TOKEN_MFA"
+
+    result = conn.initiate_auth(
+        ClientId=client_id,
+        AuthFlow="USER_PASSWORD_AUTH",
+        AuthParameters={"USERNAME": username, "PASSWORD": password},
+    )
+
+    assert result["ChallengeName"] == "SOFTWARE_TOKEN_MFA"
+    assert result["ChallengeParameters"] == {}
+    assert result["Session"] is not None
+
+    result = conn.respond_to_auth_challenge(
+        ClientId=client_id,
+        ChallengeName="SOFTWARE_TOKEN_MFA",
+        Session=result["Session"],
+        ChallengeResponses={
+            "SOFTWARE_TOKEN_MFA_CODE": "123456",
+            "USERNAME": username,
+            "SECRET_HASH": secret_hash,
+        },
+    )
+
+    assert result["AuthenticationResult"]["IdToken"] != ""
+    assert result["AuthenticationResult"]["AccessToken"] != ""
+    assert result["AuthenticationResult"]["RefreshToken"] != ""
+    assert result["AuthenticationResult"]["TokenType"] == "Bearer"
+
+
+@mock_aws
 def test_initiate_auth_USER_PASSWORD_AUTH_when_sms_mfa_enabled():
     conn = boto3.client("cognito-idp", "us-west-2")
 
