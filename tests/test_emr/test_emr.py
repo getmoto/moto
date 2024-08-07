150d8b59a0223b4b2cc1ef6dea16907772c7fe62
armichaud <email@alexandermichaud.tech>
2024-08-07 10:50:57 -0400

    tests

--- tests/test_emr/test_emr.py
+++ tests/test_emr/test_emr.py
@@ -1,32 +1,36 @@
 """Unit tests for emr-supported APIs."""
 
 import boto3
+import pytest
 
 from moto import mock_aws
 
 # See our Development Tips on writing tests for hints on how to write good tests:
 # http://docs.getmoto.org/en/latest/docs/contributing/development_tips/tests.html
 
+@pytest.fixture(name="client")
+def fixture_emr_client():
+    with mock_aws():
+        yield boto3.client("emr", region_name="us-east-2")
 
 @mock_aws
-def test_get_block_public_access_configuration():
-    client = boto3.client("emr", region_name="us-east-2")
-    resp = client.get_block_public_access_configuration()
+def test_put_and_get_block_public_access_configuration(client):
+    client.put_block_public_access_configuration(
+        BlockPublicAccessConfiguration={
+            'BlockPublicSecurityGroupRules': True,
+            'PermittedPublicSecurityGroupRuleRanges': [
+                {
+                    'MinRange': 10,
+                    'MaxRange': 100
+                },
+            ]
+        }
+    )
 
-    raise Exception("NotYetImplemented")
-
-
-@mock_aws
-def test_get_block_public_access_configuration():
-    client = boto3.client("emr", region_name="ap-southeast-1")
-    resp = client.get_block_public_access_configuration()
-
-    raise Exception("NotYetImplemented")
-
-
-@mock_aws
-def test_put_block_public_access_configuration():
-    client = boto3.client("emr", region_name="eu-west-1")
-    resp = client.put_block_public_access_configuration()
-
-    raise Exception("NotYetImplemented")
+    connection = client.get_block_public_access_configuration()
+    assert connection["BlockPublicAccessConfiguration"]["BlockPublicSecurityGroupRules"] == True
+    assert connection["BlockPublicAccessConfiguration"]["PermittedPublicSecurityGroupRuleRanges"][0]["MinRange"] == 10
+    assert connection["BlockPublicAccessConfiguration"]["PermittedPublicSecurityGroupRuleRanges"][0]["MinRange"] == 100
+    assert isinstance(connection["BlockPublicAccessConfigurationMetadata"]["CreatedByArn"], str)
+    assert isinstance(connection["BlockPublicAccessConfigurationMetadata"]["CreationDateTime"], int)
+     
