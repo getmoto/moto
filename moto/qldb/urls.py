172f5a69b147ca7f5f8adfca04bcc036afde3062
Bert Blommers <info@bertblommers.nl>
2024-08-15 09:08:35 +0000

    Fix QLDB URL's

--- moto/qldb/urls.py
+++ moto/qldb/urls.py
@@ -7,5 +7,8 @@ url_bases = [
 ]
 
 url_paths = {
-    "{0}/.*$": QLDBResponse.dispatch,
+    "{0}/ledgers$": QLDBResponse.dispatch,
+    "{0}/ledgers/(?P<ledger_name>[^/]+)$": QLDBResponse.dispatch,
+    "{0}/tags/(?P<resource_arn>[^/]+)$": QLDBResponse.dispatch,
+    "{0}/tags/(?P<resource_arn>[^/]+)/mock_ledger$": QLDBResponse.dispatch,
 }
