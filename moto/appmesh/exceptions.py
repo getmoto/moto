231210a693022ce74af01f28811413c0911af746
armichaud <email@alexandermichaud.tech>
2024-08-16 09:03:54 -0400

    basic CRUD

--- moto/appmesh/exceptions.py
+++ moto/appmesh/exceptions.py
@@ -1,1 +1,12 @@
 """Exceptions raised by the appmesh service."""
+from moto.core.exceptions import JsonRESTError
+
+class MeshError(JsonRESTError):
+    code = 400
+
+class MeshNotFoundError(MeshError):
+    def __init__(self, mesh_name: str) -> None:
+        super().__init__(
+            "MeshNotFound",
+            f"There are no meshes with the name {mesh_name}.",
+        )
--- moto/appmesh/models.py
+++ moto/appmesh/models.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Any, Dict, List, Literal, Optional, Tuple, Union
 
+from moto.appmesh.exceptions import MeshNotFoundError
 from moto.core.base_backend import BackendDict, BaseBackend
 
 
@@ -13,6 +14,7 @@ class Mesh:
     metadata: Dict[str, Optional[Union[str, int, datetime]]]
     spec: Dict[str, Optional[Dict[str, Optional[str]]]]
     status: Dict[Literal["status"], str]
+    tags: List[Dict[str, str]]
 
     def to_dict(self) -> Dict[str, Any]:  # type ignore[misc]
         service_discovery = self.spec.get("service_discovery") or {}
@@ -43,31 +45,65 @@ class AppMeshBackend(BaseBackend):
 
     def __init__(self, region_name: str, account_id: str) -> None:
         super().__init__(region_name, account_id)
-
-    # add methods from here
+        self.meshes: Dict[str, Mesh] = dict()
 
     def create_mesh(
         self,
-        client_token: str,
+        client_token: Optional[str],
         mesh_name: str,
         spec: Dict[str, Dict[str, str]],
         tags: List[Dict[str, str]],
     ) -> Mesh:
-        # implement here
+        service_discovery = spec.get("serviceDiscovery") or {}
+        now = datetime.now()
+        mesh = Mesh(
+            mesh_name=mesh_name,
+            spec={
+                "egress_filter": spec.get("egressFilter"),
+                "service_discovery": { 
+                    "ip_preference": service_discovery.get("ipPreference")
+                }
+            },
+            status="ACTIVE",
+            metadata={
+                "arn": "TODO",
+                "created_at": now,
+                "last_updated_at": now,
+                "mesh_owner": "TODO",
+                "uid": "TODO",
+                "version": 1
+            },
+            tags=tags
+        )
+        self.meshes[mesh_name] = mesh
         return mesh
 
     def update_mesh(
-        self, client_token: str, mesh_name: str, spec: Dict[str, Dict[str, str]]
+        self, 
+        client_token: Optional[str], 
+        mesh_name: str, 
+        spec: Optional[Dict[str, Dict[str, str]]]
     ) -> Mesh:
-        # implement here
-        return mesh
+        if mesh_name not in self.meshes:
+            raise MeshNotFoundError(mesh_name)
+        if spec:
+            self.meshes[mesh_name].spec["egress_filter"] = spec.get("egressFilter")
+            self.meshes[mesh_name].spec["service_discovery"] = { 
+                "ip_preference": (spec.get("serviceDiscovery") or {}).get("ipPreference")
+            }
+        return self.meshes[mesh_name]
 
     def describe_mesh(self, mesh_name: str, mesh_owner: str) -> Mesh:
-        # implement here
-        return mesh
+        if mesh_name not in self.meshes:
+            raise MeshNotFoundError(mesh_name)
+        return self.meshes[mesh_name]
 
     def delete_mesh(self, mesh_name: str) -> Mesh:
-        # implement here
+        if mesh_name not in self.meshes:
+            raise MeshNotFoundError(mesh_name)
+        self.meshes[mesh_name].status["status"] = "DELETED"
+        mesh = self.meshes[mesh_name].copy()
+        del self.meshes[mesh_name]
         return mesh
 
     def list_tags_for_resource(
