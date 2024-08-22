getmoto/moto

commit 2a8bb4a6fc06ad91ad1b234ccb11c1199b0898f2

Athena: delete_work_group() and default configuration (#7946)

moto/athena/models.py
@@ -62,6 +62,20 @@ class WorkGroup(TaggableResourceMixin, BaseModel):
         self.description = description
         self.configuration = configuration
 
+        if "EnableMinimumEncryptionConfiguration" not in self.configuration:
+            self.configuration["EnableMinimumEncryptionConfiguration"] = False
+        if "EnforceWorkGroupConfiguration" not in self.configuration:
+            self.configuration["EnforceWorkGroupConfiguration"] = True
+        if "EngineVersion" not in self.configuration:
+            self.configuration["EngineVersion"] = {
+                "EffectiveEngineVersion": "Athena engine " "version 3",
+                "SelectedEngineVersion": "AUTO",
+            }
+        if "PublishCloudWatchMetricsEnabled" not in self.configuration:
+            self.configuration["PublishCloudWatchMetricsEnabled"] = False
+        if "RequesterPaysEnabled" not in self.configuration:
+            self.configuration["RequesterPaysEnabled"] = False
+
 
 class DataCatalog(TaggableResourceMixin, BaseModel):
     def __init__(
@@ -179,7 +193,13 @@ class AthenaBackend(BaseBackend):
 
         # Initialise with the primary workgroup
         self.create_work_group(
-            name="primary", description="", configuration=dict(), tags=[]
+            name="primary",
+            description="",
+            configuration={
+                "ResultConfiguration": {},
+                "EnforceWorkGroupConfiguration": False,
+            },
+            tags=[],
         )
 
     def create_work_group(
@@ -217,6 +237,9 @@ class AthenaBackend(BaseBackend):
             "Description": wg.description,
             "CreationTime": time.time(),
         }
+
+    def delete_work_group(self, name: str) -> None:
+        self.work_groups.pop(name, None)
 
     def start_query_execution(
         self,

moto/athena/responses.py
@@ -41,6 +41,11 @@ class AthenaResponse(BaseResponse):
         name = self._get_param("WorkGroup")
         return json.dumps({"WorkGroup": self.athena_backend.get_work_group(name)})
 
+    def delete_work_group(self) -> str:
+        name = self._get_param("WorkGroup")
+        self.athena_backend.delete_work_group(name)
+        return "{}"
+
     def start_query_execution(self) -> Union[Tuple[str, Dict[str, int]], str]:
         query = self._get_param("QueryString")
         context = self._get_param("QueryExecutionContext")

tests/test_athena/test_athena.py
@@ -5,6 +5,7 @@ from botocore.exceptions import ClientError
 from moto import mock_aws, settings
 from moto.athena.models import QueryResults, athena_backends
 from moto.core import DEFAULT_ACCOUNT_ID
+from tests import aws_verified
 
 
 @mock_aws
@@ -57,32 +58,56 @@ def test_create_work_group():
     assert work_group["State"] == "ENABLED"
 
 
-@mock_aws
+@aws_verified
+@pytest.mark.aws_verified
 def test_get_primary_workgroup():
     client = boto3.client("athena", region_name="us-east-1")
-    assert len(client.list_work_groups()["WorkGroups"]) == 1
+    assert len(client.list_work_groups()["WorkGroups"]) >= 1
 
     primary = client.get_work_group(WorkGroup="primary")["WorkGroup"]
     assert primary["Name"] == "primary"
-    assert primary["Configuration"] == {}
+    assert primary["Configuration"] == {
+        "ResultConfiguration": {},
+        "EnforceWorkGroupConfiguration": False,
+        "PublishCloudWatchMetricsEnabled": False,
+        "RequesterPaysEnabled": False,
+        "EngineVersion": {
+            "SelectedEngineVersion": "AUTO",
+            "EffectiveEngineVersion": "Athena engine version 3",
+        },
+        "EnableMinimumEncryptionConfiguration": False,
+    }
 
 
-@mock_aws
+@aws_verified
+@pytest.mark.aws_verified
 def test_create_and_get_workgroup():
     client = boto3.client("athena", region_name="us-east-1")
 
-    create_basic_workgroup(client=client, name="athena_workgroup")
+    wg_name = "athena_workgroup"
+    create_basic_workgroup(client=client, name=wg_name)
 
-    work_group = client.get_work_group(WorkGroup="athena_workgroup")["WorkGroup"]
-    del work_group["CreationTime"]  # Were not testing creationtime atm
-    assert work_group == {
-        "Name": "athena_workgroup",
-        "State": "ENABLED",
-        "Configuration": {
-            "ResultConfiguration": {"OutputLocation": "s3://bucket-name/prefix/"}
-        },
-        "Description": "Test work group",
-    }
+    try:
+        work_group = client.get_work_group(WorkGroup=wg_name)["WorkGroup"]
+        del work_group["CreationTime"]  # We're not testing creationtime atm
+        assert work_group == {
+            "Name": wg_name,
+            "State": "ENABLED",
+            "Configuration": {
+                "ResultConfiguration": {"OutputLocation": "s3://bucket-name/prefix/"},
+                "EnforceWorkGroupConfiguration": True,
+                "PublishCloudWatchMetricsEnabled": False,
+                "RequesterPaysEnabled": False,
+                "EngineVersion": {
+                    "SelectedEngineVersion": "AUTO",
+                    "EffectiveEngineVersion": "Athena engine version 3",
+                },
+                "EnableMinimumEncryptionConfiguration": False,
+            },
+            "Description": "Test work group",
+        }
+    finally:
+        client.delete_work_group(WorkGroup=wg_name)
 
 
 @mock_aws

commit 0683119c0bdd18213277c36c8e6908cef7200cfc

Resolved issue with Configuration being a required parameter for Athena Workgroups.
Bug introduced by pull request https://github.com/getmoto/moto/pull/7946 which force Configuration to become a required parameter for Athena Workgroups when it is not. This change allow for the work in the pull request mentioned to function without making Configuration required.

moto/athena/models.py
@@ -62,6 +62,9 @@ class WorkGroup(TaggableResourceMixin, BaseModel):
         self.description = description
         self.configuration = configuration
 
+        if not configuration:
+             self.configuration = {}
+
         if "EnableMinimumEncryptionConfiguration" not in self.configuration:
             self.configuration["EnableMinimumEncryptionConfiguration"] = False
         if "EnforceWorkGroupConfiguration" not in self.configuration: