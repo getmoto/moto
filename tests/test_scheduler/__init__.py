9df8ed8cbafbfbb46d8d2fd656874f51f9a39488
Vinícius Aguiar <vaguiararqdevsoftware@proton.me>
2024-09-29 13:33:01 -0300

    test: fix helper func perhaps different environments deals with timestamps differently

--- tests/test_scheduler/__init__.py
+++ tests/test_scheduler/__init__.py
@@ -7,22 +7,22 @@ from moto.core.utils import utcnow
 def pytest_parametrize_test_create_get_schedule__with_start_date() -> (
     List[Tuple[datetime, datetime]]
 ):
-    now = utcnow().replace(microsecond=0)
-    timedelta_kwargs = {"days": 1, "seconds": 1, "minutes": 1, "hours": 1, "weeks": 1}
-    return_ = [(now, now)]
+    now = utcnow()
+    timedelta_kwargs = {"days": 1, "seconds": 30, "minutes": 1, "hours": 1, "weeks": 1}
+    return_ = []
     for k, v in timedelta_kwargs.items():
         to_append = now + timedelta(**{k: v})
-        return_.append((to_append, to_append))
+        return_.append((to_append, to_append.replace(microsecond=0)))
     else:
         to_append = now.replace(year=now.year + 1)
-        return_.append((to_append, to_append))
+        return_.append((to_append, to_append.replace(microsecond=0)))
     return return_
 
 
 def pytest_parametrize_test_create_schedule__exception_with_start_date() -> (
     List[datetime]
 ):
-    now = utcnow().replace(microsecond=0)
+    now = utcnow()
     timedelta_kwargs = {"days": 1, "minutes": 6, "hours": 1, "weeks": 1}
     return_ = []
     for k, v in timedelta_kwargs.items():
