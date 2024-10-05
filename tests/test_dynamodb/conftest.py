70d8cd7561355c34c1e1480a684be601c7209b4f
Siddhesh0305 <siddheshmasurkar2003@gmail.com>
2024-10-05 19:29:50 +0530

    Updated conftest.py

--- tests/test_dynamodb/conftest.py
+++ tests/test_dynamodb/conftest.py
@@ -1,17 +1,13 @@
 import os
-
 import boto3
 import pytest
-
-from moto import mock_aws
+from moto import mock_dynamodb2
 from moto.core import DEFAULT_ACCOUNT_ID
-from moto.dynamodb.models import Table
-
 
 # pylint: disable=redefined-outer-name
 @pytest.fixture(scope="function")
 def aws_credentials():
-    """Mocked AWS Credentials for moto."""
+    """Mocked AWS Credentials for Moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
@@ -21,53 +17,68 @@ def aws_credentials():
 
 @pytest.fixture(scope="function")
 def ddb_client(aws_credentials):
-    with mock_aws():
-        yield boto3.client("dynamodb")
+    """Create a mocked DynamoDB client."""
+    with mock_dynamodb2():
+        yield boto3.client("dynamodb", region_name="us-east-1")
 
 
 @pytest.fixture(scope="function")
 def ddb_resource(aws_credentials):
-    with mock_aws():
-        yield boto3.resource("dynamodb")
+    """Create a mocked DynamoDB resource."""
+    with mock_dynamodb2():
+        yield boto3.resource("dynamodb", region_name="us-east-1")
 
 
 @pytest.fixture
 def users_table_name():
+    """Name of the DynamoDB table."""
     return "users"
 
 
 @pytest.fixture
 def create_user_table(ddb_client, users_table_name):
+    """Create the 'users' table in DynamoDB with some initial data."""
+    # Create a table
     ddb_client.create_table(
         TableName=users_table_name,
         KeySchema=[{"AttributeName": "username", "KeyType": "HASH"}],
         AttributeDefinitions=[{"AttributeName": "username", "AttributeType": "S"}],
         ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
     )
+
+    # Insert sample items
     ddb_client.put_item(
-        TableName="users", Item={"username": {"S": "user1"}, "binaryfoo": {"B": b"bar"}}
+        TableName=users_table_name,
+        Item={"username": {"S": "user1"}, "binaryfoo": {"B": b"bar"}}
     )
     ddb_client.put_item(
-        TableName="users", Item={"username": {"S": "user2"}, "foo": {"S": "bar"}}
+        TableName=users_table_name,
+        Item={"username": {"S": "user2"}, "foo": {"S": "bar"}}
     )
     ddb_client.put_item(
-        TableName="users", Item={"username": {"S": "user3"}, "foo": {"S": "bar"}}
+        TableName=users_table_name,
+        Item={"username": {"S": "user3"}, "foo": {"S": "bar"}}
     )
     return ddb_client
 
 
 @pytest.fixture
 def table():
-    return Table(
-        "Forums",
-        account_id=DEFAULT_ACCOUNT_ID,
-        region="us-east-1",
-        schema=[
+    """Manually define a table using moto's Table object (for advanced use cases)."""
+    return {
+        "TableName": "Forums",
+        "AccountId": DEFAULT_ACCOUNT_ID,
+        "Region": "us-east-1",
+        "KeySchema": [
             {"KeyType": "HASH", "AttributeName": "forum_name"},
             {"KeyType": "RANGE", "AttributeName": "subject"},
         ],
-        attr=[
+        "AttributeDefinitions": [
             {"AttributeType": "S", "AttributeName": "forum_name"},
             {"AttributeType": "S", "AttributeName": "subject"},
         ],
-    )
+        "ProvisionedThroughput": {
+            "ReadCapacityUnits": 5,
+            "WriteCapacityUnits": 5
+        }
+    }
