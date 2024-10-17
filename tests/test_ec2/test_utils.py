b20489d52376d7ff3665309c8c19290c841bf6da
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-10-17 11:34:07 +0300

    Add SHA1 and SHA256 algorithms for EC2 fingerprints

--- tests/test_ec2/test_utils.py
+++ tests/test_ec2/test_utils.py
@@ -2,6 +2,10 @@ import ipaddress
 from copy import deepcopy
 from unittest.mock import patch
 
+import pytest
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from pytest import raises
 
 from moto.ec2 import utils
@@ -13,9 +17,8 @@ def test_random_key_pair():
     key_pair = utils.random_rsa_key_pair()
     check_private_key(key_pair["material"], "rsa")
 
-    # AWS uses MD5 fingerprints, which are 47 characters long, *not* SHA1
-    # fingerprints with 59 characters.
-    assert len(key_pair["fingerprint"]) == 47
+    # AWS uses SHA1 for created by Amazon fingerprints, which are 59 characters long
+    assert len(key_pair["fingerprint"]) == 59
 
     key_pair = utils.random_ed25519_key_pair()
     check_private_key(key_pair["material"], "ed25519")
@@ -62,3 +65,24 @@ def test_gen_moto_amis():
     # with drop=False, it should raise KeyError because of the missing key
     with raises(KeyError, match="'Public'"):
         utils.gen_moto_amis(images, drop_images_missing_keys=False)
+
+
+@pytest.mark.parametrize("is_created, fingerprint_len", [(True, 59), (False, 47)])
+def test_public_key_fingerprint__rsa(is_created: bool, fingerprint_len: int):
+    private_key = rsa.generate_private_key(
+        public_exponent=65537, key_size=2048, backend=default_backend()
+    )
+    public_key = private_key.public_key()
+
+    fingerprint = utils.public_key_fingerprint(public_key, is_created=is_created)
+
+    assert len(fingerprint) == fingerprint_len
+
+
+def test_public_key_fingerprint__ed25519():
+    private_key = Ed25519PrivateKey.generate()
+    public_key = private_key.public_key()
+
+    fingerprint = utils.public_key_fingerprint(public_key)
+
+    assert len(fingerprint) == 95
