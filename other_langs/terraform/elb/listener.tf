8ff0b08665948d5e4625eecb75f901b1b1daac3b
Bert Blommers <info@bertblommers.nl>
2024-11-02 08:54:48 -0100

    ELBv2: Describe/Modify Listener Attributes

---
+++ other_langs/terraform/elb/listener.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
