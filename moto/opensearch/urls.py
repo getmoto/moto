2b15daa833d32ee1143a9dd1f364eada429072c0
Zachary Karpinski <zkarpinski@protonmail.com>
2024-12-09 13:16:06 -0500

    added opensearch urlpath

--- moto/opensearch/urls.py
+++ moto/opensearch/urls.py
@@ -8,4 +8,7 @@ url_bases = [r"https?://es\.(.+)\.amazonaws\.com"]
 response = OpenSearchServiceResponse()
 
 
-url_paths = {"{0}/.*$": response.dispatch}
+url_paths = {
+    "{0}/.*$": response.dispatch,
+    "{0}/2021-01-01/opensearch/domain-info$": response.dispatch,
+}
