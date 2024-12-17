c22754c2e6cb9c2ff67b7ab9f8ce06ef0d4f1fcd
Sleep Kuntz <sleep@kuntz.co>
2024-12-17 11:53:41 -0600

    Add basic addition support to cloudwatch.get_metric_data expressions

--- tests/test_cloudwatch/test_cloudwatch_expression_parser.py
+++ tests/test_cloudwatch/test_cloudwatch_expression_parser.py
@@ -1,3 +1,5 @@
+import datetime
+
 from moto.cloudwatch.metric_data_expression_parser import parse_expression
 
 
@@ -38,3 +40,26 @@ def test_complex_expression():
     ]
     res = parse_expression("totalBytes/10", result_from_previous_queries)
     assert res == ([], [])
+
+
+def test_addition_expression():
+    t3 = datetime.datetime.now(datetime.timezone.utc)
+    t2 = t3 - datetime.timedelta(minutes=1)
+    t1 = t2 - datetime.timedelta(minutes=1)
+
+    results_from_previous_queries = [
+        {
+            "id": "first",
+            "label": "first",
+            "vals": [10.0, 15.0, 30.0],
+            "timestamps": [t1, t2, t3],
+        },
+        {
+            "id": "second",
+            "label": "second",
+            "vals": [25.0, 5.0, 3.0],
+            "timestamps": [t1, t2, t3],
+        },
+    ]
+    res = parse_expression("first + second", results_from_previous_queries)
+    assert res == ([35.0, 20.0, 33.0], [t1, t2, t3])
