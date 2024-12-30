51313cec944f18e8dcee3cf17a0ab055aa10e2e7
Bert Blommers <info@bertblommers.nl>
2024-12-30 09:42:10 -0100

    DynamoDB: KeyConditionExpression can have additional parentheses (#8444)

--- moto/dynamodb/parsing/key_condition_expression.py
+++ moto/dynamodb/parsing/key_condition_expression.py
@@ -164,6 +164,10 @@ def parse_expression(
             # (hash_key = :id) and (sortkey = :sk)
             #                     ^
             if current_stage in [EXPRESSION_STAGES.INITIAL_STAGE]:
+                if not current_phrase:
+                    # hashkey = :id and (begins_with(sortkey, :sk))
+                    #                   ^
+                    continue
                 if current_phrase not in ["begins_with", ""]:
                     raise MockValidationException(
                         f"Invalid KeyConditionExpression: Invalid function name; function: {current_phrase}"
