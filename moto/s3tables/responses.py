641b3aa13a77b851c7cf0275cc720d4d478e21af
Felix Scherz <felixwscherz@gmail.com>
2025-01-05 11:49:46 +0100

    fix: clean up path variables

--- moto/s3tables/responses.py
+++ moto/s3tables/responses.py
@@ -62,7 +62,7 @@ class S3TablesResponse(BaseResponse):
         return 200, self.default_response_headers, json.dumps(body)
 
     def get_table_bucket(self) -> TYPE_RESPONSE:
-        table_bucket_arn = unquote(self.path.split("/")[-1])
+        table_bucket_arn = unquote(self.path.rsplit("/")[-1]).lstrip("/")
         bucket = self.s3tables_backend.get_table_bucket(
             table_bucket_arn=table_bucket_arn,
         )
@@ -81,7 +81,7 @@ class S3TablesResponse(BaseResponse):
         )
 
     def delete_table_bucket(self) -> TYPE_RESPONSE:
-        table_bucket_arn = unquote(self.path.split("/")[-1])
+        table_bucket_arn = unquote(self.path.rsplit("/")[-1]).lstrip("/")
         self.s3tables_backend.delete_table_bucket(
             table_bucket_arn=table_bucket_arn,
         )
