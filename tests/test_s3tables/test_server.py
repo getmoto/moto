0c3088b1b176865571b3952004eea9556e49c8ee
Felix Scherz <felixwscherz@gmail.com>
2025-01-11 15:47:31 +0100

    test: add server test for create_namespace

--- tests/test_s3tables/test_server.py
+++ tests/test_s3tables/test_server.py
@@ -48,3 +48,16 @@ def test_s3tables_delete_bucket():
 
     resp = test_client.delete(f"/buckets/{quoted_arn}")
     assert resp.status_code == 200
+
+
+def test_s3tables_create_namespace():
+    backend = server.create_backend_app("s3tables")
+    test_client = backend.test_client()
+
+    resp = test_client.put("/buckets", json={"name": "foo"})
+    arn = resp.get_json()["arn"]
+
+    quoted_arn = quote(arn, safe="")
+    resp = test_client.put(f"/namespaces/{quoted_arn}", json={"namespace": "bar"})
+
+    assert resp.status_code == 200, resp.text
