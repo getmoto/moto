659da7304219f2a9dea98c816750a999322139e0
Felix Scherz <felixwscherz@gmail.com>
2025-01-11 23:04:49 +0100

    test: use random bucket names for tests

--- tests/test_s3tables/test_server.py
+++ tests/test_s3tables/test_server.py
@@ -1,8 +1,19 @@
 """Test different server responses."""
 
+import string
+from random import choice
 from urllib.parse import quote
 
+import pytest
+
 import moto.server as server
+
+
+@pytest.fixture()
+def bucket_name() -> str:
+    prefix = "table-bucket"
+    random_tag = "".join(choice(string.ascii_letters) for _ in range(10))
+    return (prefix + random_tag).lower()
 
 
 def test_s3tables_list():
@@ -15,20 +26,20 @@ def test_s3tables_list():
     assert "tableBuckets" in resp.get_json()
 
 
-def test_s3tables_create_bucket():
+def test_s3tables_create_bucket(bucket_name: str):
     backend = server.create_backend_app("s3tables")
     test_client = backend.test_client()
 
-    resp = test_client.put("/buckets", json={"name": "foo"})
+    resp = test_client.put("/buckets", json={"name": bucket_name})
     assert "arn" in resp.get_json()
-    assert resp.get_json()["arn"].endswith("foo")
+    assert resp.get_json()["arn"].endswith(bucket_name)
 
 
-def test_s3tables_get_bucket():
+def test_s3tables_get_bucket(bucket_name: str):
     backend = server.create_backend_app("s3tables")
     test_client = backend.test_client()
 
-    resp = test_client.put("/buckets", json={"name": "foo"})
+    resp = test_client.put("/buckets", json={"name": bucket_name})
     arn = resp.get_json()["arn"]
 
     quoted_arn = quote(arn, safe="")
@@ -37,11 +48,11 @@ def test_s3tables_get_bucket():
     assert resp.status_code == 200
 
 
-def test_s3tables_delete_bucket():
+def test_s3tables_delete_bucket(bucket_name: str):
     backend = server.create_backend_app("s3tables")
     test_client = backend.test_client()
 
-    resp = test_client.put("/buckets", json={"name": "foo"})
+    resp = test_client.put("/buckets", json={"name": bucket_name})
     arn = resp.get_json()["arn"]
 
     quoted_arn = quote(arn, safe="")
@@ -50,14 +61,14 @@ def test_s3tables_delete_bucket():
     assert resp.status_code == 200
 
 
-def test_s3tables_create_namespace():
+def test_s3tables_create_namespace(bucket_name: str):
     backend = server.create_backend_app("s3tables")
     test_client = backend.test_client()
 
-    resp = test_client.put("/buckets", json={"name": "foo"})
+    resp = test_client.put("/buckets", json={"name": bucket_name})
     arn = resp.get_json()["arn"]
 
     quoted_arn = quote(arn, safe="")
     resp = test_client.put(f"/namespaces/{quoted_arn}", json={"namespace": "bar"})
 
-    assert resp.status_code == 200, resp.text
+    assert resp.status_code == 200
