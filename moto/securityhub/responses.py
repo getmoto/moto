cc27af445084581305f44e0d5ce87eeedadacc72
Aman <amanvir7@gmail.com>
2025-01-27 11:00:23 -0500

    Uncommented SortCriteria

--- moto/securityhub/responses.py
+++ moto/securityhub/responses.py
@@ -18,17 +18,17 @@ class SecurityHubResponse(BaseResponse):
     def get_findings(self) -> str:
         params = self._get_params()
 
-        # Don't try to parse JSON if we already have a dict with the right keys
-        if "SortCriteria" in params:
-            sort_criteria = params["SortCriteria"]
-        else:
-            # Try to parse JSON only if needed
-            try:
-                json_params = json.loads(list(params.keys())[0])
-                sort_criteria = json_params.get("SortCriteria")
-            except (json.JSONDecodeError, IndexError):
-                sort_criteria = None
-
+        # # Don't try to parse JSON if we already have a dict with the right keys
+        # if "SortCriteria" in params:
+        #     sort_criteria = params["SortCriteria"]
+        # else:
+        #     # Try to parse JSON only if needed
+        #     try:
+        #         json_params = json.loads(list(params.keys())[0])
+        #         sort_criteria = json_params.get("SortCriteria")
+        #     except (json.JSONDecodeError, IndexError):
+        #         sort_criteria = None
+        sort_criteria = params.get("SortCriteria")
         filters = params.get("Filters")
         next_token = params.get("NextToken")
         max_results = params.get("MaxResults")
