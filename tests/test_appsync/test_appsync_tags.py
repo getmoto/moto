7a9cb2308c49a4c0e9d90d697a85407db38f1508
Zachary Karpinski <zkarpinski@protonmail.com>
2025-03-28 15:09:04 -0400

    add scenario with no tags to reach 100% cov

--- tests/test_appsync/test_appsync_tags.py
+++ tests/test_appsync/test_appsync_tags.py
@@ -90,9 +90,13 @@ def test_resource_groups_tagging_api():
     region = "us-west-2"
     client = boto3.client("appsync", region_name=region)
     rtapi_client = boto3.client("resourcegroupstaggingapi", region_name=region)
+
     api = client.create_graphql_api(
-        name="api1", authenticationType="API_KEY", tags={"key": "val", "key2": "val2"}
+        name="api-with-tags", authenticationType="API_KEY", tags={"key": "val", "key2": "val2"}
     )["graphqlApi"]
+    client.create_graphql_api(name="api-no-tags", authenticationType="API_KEY")[
+        "graphqlApi"
+    ]
 
     resp = rtapi_client.get_resources(ResourceTypeFilters=["appsync"])
     assert len(resp["ResourceTagMappingList"]) == 1
