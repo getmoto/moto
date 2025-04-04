f8e78bbb2bce8f4dc9d193b77f07d81cfeeb5173
Mathieu Cloutier <cloutier.mat0@gmail.com>
2025-04-04 17:51:46 -0600

    add test

--- tests/test_route53/test_route53_query_logging_config.py
+++ tests/test_route53/test_route53_query_logging_config.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import boto3
 import pytest
 from botocore.exceptions import ClientError
@@ -5,9 +7,21 @@ from botocore.exceptions import ClientError
 from moto import mock_aws
 from moto.core import DEFAULT_ACCOUNT_ID as ACCOUNT_ID
 from moto.moto_api._internal import mock_random
+from moto.moto_server.threaded_moto_server import ThreadedMotoServer
 
 # The log group must be in the us-east-1 region.
 TEST_REGION = "us-east-1"
+
+
+@pytest.fixture()
+def moto_server() -> Iterable[str]:
+    """Fixture to run a mocked AWS server for testing."""
+    # Note: pass `port=0` to get a random free port.
+    server = ThreadedMotoServer(port=0)
+    server.start()
+    host, port = server.get_host_and_port()
+    yield f"http://{host}:{port}"
+    server.stop()
 
 
 def create_hosted_zone_id(route53_client, hosted_zone_test_name):
@@ -104,9 +118,14 @@ def test_create_query_logging_config_bad_args():
 
 
 @mock_aws
-def test_create_query_logging_config_good_args():
+@pytest.mark.parametrize("route53_region", ["us-west-1", TEST_REGION])
+def test_create_query_logging_config_good_args(moto_server, route53_region):
     """Test a valid create_logging_config() request."""
-    client = boto3.client("route53", region_name=TEST_REGION)
+    client = boto3.client(
+        "route53",
+        region_name=route53_region,
+        endpoint_url=moto_server if route53_region != TEST_REGION else None,
+    )
     logs_client = boto3.client("logs", region_name=TEST_REGION)
 
     hosted_zone_test_name = f"route53_query_log_{mock_random.get_random_hex(6)}.test"
