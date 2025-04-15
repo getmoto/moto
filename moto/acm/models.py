ec3224f772c8c3b46830f99e8dbc31944124e552
Harsh Mishra <erbeusgriffincasper@gmail.com>
2025-04-15 23:35:34 +0530

    fix(acm): Fix certificate issuance logic for private CAs and add corresponding tests (#8805)

--- moto/acm/models.py
+++ moto/acm/models.py
@@ -229,7 +229,9 @@ class CertBundle(BaseModel):
             certificate=cert_armored,
             private_key=private_key,
             cert_type="PRIVATE" if cert_authority_arn is not None else "AMAZON_ISSUED",
-            cert_status="PENDING_VALIDATION",
+            cert_status="ISSUED"
+            if cert_authority_arn is not None
+            else "PENDING_VALIDATION",
             cert_authority_arn=cert_authority_arn,
             account_id=account_id,
             region=region,
