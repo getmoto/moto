0c442614a95da5d022a2e524450f926f05e518a4
Bert Blommers <info@bertblommers.nl>
2025-05-11 11:38:12 +0000

    Tests: Add SageMakerClient test

---
+++ other_langs/tests_sagemaker_client/test_pipeline_session.py
@@ -0,0 +1,5 @@
+def test_pipeline_session():
+    # TODO:
+    # https://github.com/aws/sagemaker-python-sdk/blob/master/doc/amazon_sagemaker_model_building_pipeline.rst
+    # Need to copy the test here to verify when/if https://github.com/getmoto/moto/issues/7037 is implemented
+    pass
