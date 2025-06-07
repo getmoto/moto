41f2e8a10242eea6a2bd3afbcef46c365fd710ee
Bert Blommers <info@bertblommers.nl>
2025-06-07 20:59:20 +0000

    EC2: Serialize Transit Gateway Route Tables

--- moto/ec2/models/core.py
+++ moto/ec2/models/core.py
@@ -12,6 +12,10 @@ class TaggedEC2Resource(BaseModel):
             tags = self.ec2_backend.describe_tags(filters={"resource-id": [self.id]})  # type: ignore[attr-defined]
         return tags
 
+    @property
+    def tagSet(self) -> list[dict[str, str]]:
+        return self.get_tags()
+
     def add_tag(self, key: str, value: str) -> None:
         self.ec2_backend.create_tags([self.id], {key: value})  # type: ignore[attr-defined]
 
