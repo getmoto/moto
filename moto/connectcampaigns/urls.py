cfbd52f0bb121c1881e0b7bbf72e4d32789746b0
Leslie Etubo <leslie.etubo@gmail.com>
2025-06-20 15:04:12 -0500

    [Connect Campaigns] Added Tagging, List and other endpoints (#8966)

--- moto/connectcampaigns/urls.py
+++ moto/connectcampaigns/urls.py
@@ -7,8 +7,16 @@ url_bases = [
 ]
 
 url_paths = {
+    "{0}/tags/(?P<arn>.+)$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/tags/(?P<arn>[^/]+)?tagKeys=(?P<tagKeys>[^/]+)$": ConnectCampaignServiceResponse.dispatch,
     "{0}/campaigns$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/campaigns-summary$": ConnectCampaignServiceResponse.dispatch,
     "{0}/campaigns/(?P<id>[^/]+)$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/campaigns/(?P<id>[^/]+)/start$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/campaigns/(?P<id>[^/]+)/stop$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/campaigns/(?P<id>[^/]+)/pause$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/campaigns/(?P<id>[^/]+)/resume$": ConnectCampaignServiceResponse.dispatch,
+    "{0}/campaigns/(?P<id>[^/]+)/state$": ConnectCampaignServiceResponse.dispatch,
     "{0}/connect-instance/(?P<connectInstanceId>[^/]+)/config$": ConnectCampaignServiceResponse.dispatch,
     "{0}/connect-instance/(?P<connectInstanceId>[^/]+)/onboarding$": ConnectCampaignServiceResponse.dispatch,
 }
