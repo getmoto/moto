7ec289dc0823876393cb983f9de3e88c9107a583
Brian Pandola <bpandola@gmail.com>
2025-07-17 15:46:23 -0700

    Simplify exception definitions

--- moto/elasticbeanstalk/exceptions.py
+++ moto/elasticbeanstalk/exceptions.py
@@ -6,10 +6,8 @@ class ElasticBeanstalkException(ServiceException):
 
 
 class InvalidParameterValueError(ServiceException):
-    def __init__(self, message: str):
-        super().__init__("InvalidParameterValue", message)
+    code = "InvalidParameterValue"
 
 
 class ResourceNotFoundException(ServiceException):
-    def __init__(self, message: str):
-        super().__init__("ResourceNotFoundException", message)
+    code = "ResourceNotFoundException"
