58a04b93d44bd23285b353d06b91aeb67e4be7a7
Arjun Subrahmanyan <arjun.subrahmanyan@gmail.com>
2025-08-27 17:06:32 -0700

    SES: Adding validations for CreateReceiptRuleSet action

--- moto/ses/models.py
+++ moto/ses/models.py
@@ -1,6 +1,7 @@
 import datetime
 import email
 import json
+import re
 from email.encoders import encode_7or8bit
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
@@ -204,6 +205,8 @@ class SESBackend(BaseBackend):
 
     Note that, as this is an internal API, the exact format may differ per versions.
     """
+
+    __RULE_SET_NAME_REGEX = r"^[a-zA-Z0-9_.-]+$"
 
     def __init__(self, region_name: str, account_id: str):
         super().__init__(region_name, account_id)
@@ -638,6 +641,24 @@ class SESBackend(BaseBackend):
         self.templates.pop(name)
 
     def create_receipt_rule_set(self, rule_set_name: str) -> None:
+        """
+        We have to validate rule_set_name against the following conditions:
+            1. Must match a given regex pattern
+            2. Must start and end with a number or a character
+            3. Contain 64 characters or lesser
+        """
+        # Boto3 throws an error with the same message for both failures, even though we could have very well combined it into one regex
+        if (
+            not re.match(SESBackend.__RULE_SET_NAME_REGEX, rule_set_name)
+            or not rule_set_name[0].isalnum()
+            or not rule_set_name[-1].isalnum()
+        ):
+            raise ValidationError(
+                "Value at 'ruleSetName' failed to satisfy constraint: Member must satisfy regular expression pattern: ^[a-zA-Z0-9_.-]+$"
+            )
+        # A different message thrown for length of the rule_name_set string
+        if len(rule_set_name) > 64:
+            raise ValidationError(f"Not a valid ruleSetName: {rule_set_name}")
         if self.receipt_rule_set.get(rule_set_name) is not None:
             raise AlreadyExists(f"Rule set already exists: {rule_set_name}")
         self.receipt_rule_set[rule_set_name] = ReceiptRuleSet(name=rule_set_name)
