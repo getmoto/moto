6201a7bb8b108acf85eb20913fb8a79c3e71db90
Max Russell <mrussel4@ucsc.edu>
2025-09-02 14:27:50 -0700

    linting: format exceptions.py

--- moto/ssm/exceptions.py
+++ moto/ssm/exceptions.py
@@ -164,4 +164,7 @@ class AlreadyExistsException(JsonRESTError):
     code = 400
 
     def __init__(self, operating_system: str):
-        super().__init__("AlreadyExistsException", f"Patch Group baseline already has a baseline registered for OperatingSystem {operating_system}")
+        super().__init__(
+            "AlreadyExistsException",
+            f"Patch Group baseline already has a baseline registered for OperatingSystem {operating_system}",
+        )
