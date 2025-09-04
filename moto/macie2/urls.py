197ca8fd098ab04de754462ae7f1ab84c265a059
g4omar <g4omar@gmail.com>
2025-09-04 02:49:55 -0400

    Update urls.py

--- moto/macie2/urls.py
+++ moto/macie2/urls.py
@@ -10,6 +10,8 @@ url_paths = {
     "{0}/invitations/accept$": MacieResponse.dispatch,
     "{0}/members$": MacieResponse.dispatch,
     "{0}/members/[^/]+$": MacieResponse.dispatch,
+    "{0}/members/(?P<id>[^/]+)$": MacieResponse.dispatch,
     "{0}/administrator$": MacieResponse.dispatch,
     "{0}/macie$": MacieResponse.dispatch,
+    "{0}/session$": MacieResponse.dispatch,
 }
