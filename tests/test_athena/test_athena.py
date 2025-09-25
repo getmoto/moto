ee2e061229d255b213115731cad1dfb836962bd4
LeekSwan <lixuanron@gmail.com>
2025-09-25 10:21:55 -0400

    lint

--- tests/test_athena/test_athena.py
+++ tests/test_athena/test_athena.py
@@ -363,7 +363,9 @@ def test_list_tags_for_resource():
         Parameters={"catalog-id": "AWS Test account ID"},
         Tags=tags,
     )
-    resource_arn = "arn:aws:athena:us-east-1:123456789012:datacatalog/athena_datacatalog"
+    resource_arn = (
+        "arn:aws:athena:us-east-1:123456789012:datacatalog/athena_datacatalog"
+    )
     returned_tags = client.list_tags_for_resource(ResourceARN=resource_arn)
     assert returned_tags["Tags"] == tags
 
