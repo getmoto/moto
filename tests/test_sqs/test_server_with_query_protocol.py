90f70978b017b274b0f03efb9ea127afbfdce712
Brian Pandola <bpandola@gmail.com>
2025-09-29 05:38:21 -0700

    Initial commit - tests passing, but not linting
    Basic core changes to bring in AwsQueryCompatible operations/shapes
    for SQS when Query protocol detected
    
    Added some specific server mode query tests, but might just merge with test_server.py
    
    No changes to model or tests, but responses.py still is a bit messy and will need more polish

---
+++ tests/test_sqs/test_server_with_query_protocol.py
@@ -0,0 +1,25 @@
+from moto import server
+from urllib.parse import urlencode
+
+def test_create_queue_with_tags():
+    backend = server.create_backend_app("sqs")
+    queue_name = "test-queue"
+    test_client = backend.test_client()
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    params = {
+        "Action":"CreateQueue",
+        "QueueName":queue_name,
+        "Tag.1.Key": "foo",
+    "Tag.1.Value": "bar",}
+    resp = test_client.post(headers=headers, data=urlencode(params))
+    assert resp.status_code == 200
+    assert "<CreateQueueResult>" in resp.data.decode("utf-8")
+    params = {
+        "Action":"ListQueueTags",
+        "QueueUrl":queue_name,
+    }
+    resp = test_client.post(headers=headers, data=urlencode(params))
+    assert "<Tag><Key>foo</Key><Value>bar</Value></Tag>" in resp.data.decode("utf-8")
+
