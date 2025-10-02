52753a1c2e20c6ad30ea09d6858bd01f63c7cd31
Leslie Etubo <leslie@Leslies-MacBook-Pro-Work.local>
2025-10-01 21:03:39 -0500

    adds no tags tests

--- tests/test_resourcegroupstaggingapi/test_resourcegroupstagging_firehose.py
+++ tests/test_resourcegroupstaggingapi/test_resourcegroupstagging_firehose.py
@@ -24,3 +24,30 @@ def test_firehose():
     assert resp["ResourceTagMappingList"][0]["Tags"] == [
         {"Key": "key1", "Value": "val1"}
     ]
+
+
+# Negative Tests for tagging Firehose resources no tags or tag filters
+@mock_aws
+def test_firehose_no_tags_or_filters():
+    client = boto3.client("firehose", region_name="us-east-2")
+    rtapi = boto3.client("resourcegroupstaggingapi", region_name="us-east-2")
+    client.create_delivery_stream(
+        DeliveryStreamName="my-stream",
+        S3DestinationConfiguration={
+            "RoleARN": "arn:aws:iam::123456789012:role/my-role",
+            "BucketARN": "arn:aws:s3:::my-bucket",
+        },
+    )
+    resp = rtapi.get_resources(ResourceTypeFilters=["firehose"])
+    assert len(resp["ResourceTagMappingList"]) == 1
+    assert resp["ResourceTagMappingList"][0]["ResourceARN"].startswith(
+        "arn:aws:firehose:us-east-2:"
+    )
+    assert resp["ResourceTagMappingList"][0].get("Tags") is None
+
+    resp = rtapi.get_resources()
+    assert len(resp["ResourceTagMappingList"]) == 1
+    assert resp["ResourceTagMappingList"][0]["ResourceARN"].startswith(
+        "arn:aws:firehose:us-east-2:"
+    )
+    assert resp["ResourceTagMappingList"][0].get("Tags") is None
