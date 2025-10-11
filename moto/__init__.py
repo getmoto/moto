fed32df61c1ca669ed3576fbfd75b854c5840a46
Brian Pandola <bpandola@gmail.com>
2025-10-10 20:03:38 -0700

    Core: add custom loader for AWS service models with Moto extras

--- moto/__init__.py
+++ moto/__init__.py
@@ -1,4 +1,8 @@
-from moto.core.decorator import mock_aws as mock_aws
+import os
 
 __title__ = "moto"
 __version__ = "5.1.15.dev"
+
+MOTO_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+from moto.core.decorator import mock_aws as mock_aws  # noqa: E402
