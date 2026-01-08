b8570eb3610ca44e5a8526ca52e0948c33990d0c
YUNG CHENG CHEN <easoneason0905@gmail.com>
2026-01-08 19:04:53 +0800

    Fix linting: remove print and use English comments

--- tests/test_iot/test_issue_repro.py
+++ tests/test_iot/test_issue_repro.py
@@ -1,24 +1,19 @@
 import boto3
-import pytest
 from moto import mock_aws
-
 
 @mock_aws
 def test_list_thing_principals_v2():
-    # Create a fake AWS IoT client
+    # 1. Create a dummy AWS IoT client
     client = boto3.client("iot", region_name="us-east-1")
     thing_name = "my-test-thing"
 
-    # 2. Create a Thing (otherwise you won't be able to find anything).
+    # 2. Create a Thing (required to list principals)
     client.create_thing(thingName=thing_name)
 
-    # 3. Call the newly written V2 function!
-    # (If the IF statement is not written correctly, this will result in an error saying "Not Implemented")
+    # 3. Call the newly implemented V2 function
+    # (This would raise NotImplementedError before our fix)
     response = client.list_thing_principals_v2(thingName=thing_name)
 
-    # 4. Verify if the returned data includes the V2-specific field "thingPrincipalObjects"."
+    # 4. Verify the response contains the V2 specific key
     assert "thingPrincipalObjects" in response
-    # Since we haven't bound certificates yet, the list should be empty, but the key must exist.
     assert response["thingPrincipalObjects"] == []
-
-    print("\n🎉 Test successful! V2 functionality is working properly!")
