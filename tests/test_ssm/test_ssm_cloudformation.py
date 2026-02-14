0e3cec560963b2982e82fb6218729e5f1f69bbfb
Bert Blommers <info@bertblommers.nl>
2026-02-14 19:32:36 -0100

    CloudFormation: Support AWS::SSM::Document resources (#9748)

--- tests/test_ssm/test_ssm_cloudformation.py
+++ tests/test_ssm/test_ssm_cloudformation.py
@@ -1,4 +1,5 @@
 import json
+from uuid import uuid4
 
 import boto3
 
@@ -68,3 +69,87 @@ def test_get_command_invocations_from_stack():
     client.get_command_invocation(
         CommandId=cmd_id, InstanceId=instance_ids[0], PluginName="aws:runShellScript"
     )
+
+
+@mock_aws
+def test_ssm_documents():
+    # SETUP
+    doc_name = str(uuid4())
+    stack_name = f"Stack{str(uuid4())[0:6]}"
+    ssm_template_json = json.dumps(get_ssm_document_template(doc_name))
+
+    # CREATE
+    cf = boto3.client("cloudformation", region_name="us-east-1")
+    cf.create_stack(StackName=stack_name, TemplateBody=ssm_template_json)
+
+    # VERIFY
+    ssm = boto3.client("ssm", region_name="us-east-1")
+    document = ssm.describe_document(Name=doc_name)["Document"]
+
+    assert document["Description"] == "Sample Yaml"
+    assert document["Name"] == doc_name
+    assert document["Parameters"] == [
+        {
+            "DefaultValue": "3",
+            "Description": "Command Duration.",
+            "Name": "Parameter1",
+            "Type": "Integer",
+        }
+    ]
+
+    # DELETE
+    cf.delete_stack(StackName=stack_name)
+
+    # VERIFY
+    assert not ssm.list_documents()["DocumentIdentifiers"]
+
+
+def _get_document_content():
+    return {
+        "schemaVersion": "2.2",
+        "description": "Sample Yaml",
+        "parameters": {
+            "Parameter1": {
+                "type": "Integer",
+                "default": 3,
+                "description": "Command Duration.",
+                "allowedValues": [1, 2, 3, 4],
+            }
+        },
+        "mainSteps": [
+            {
+                "action": "aws:runShellScript",
+                "name": "sampleCommand",
+                "inputs": {"runCommand": ["echo hi"]},
+            }
+        ],
+    }
+
+
+def get_ssm_document_template(document_name: str):
+    attachment1 = {"Key": "attachment1_key", "Name": "My First", "Values": ["value1"]}
+    attachment2 = {"Key": "attachment2_key", "Name": "My Second", "Values": ["value2"]}
+    doc_requires1 = {"Name": "doc1", "Version": "doc1version"}
+    doc_requires2 = {"Name": "doc2", "Version": "doc2version"}
+    tag1 = {"Key": "key1", "Value": "value1"}
+    tag2 = {"Key": "tag2", "Value": "value2"}
+    return {
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Resources": {
+            "ImportantDocument": {
+                "Type": "AWS::SSM::Document",
+                "Properties": {
+                    "Attachments": [attachment1, attachment2],
+                    "Content": json.dumps(_get_document_content()),
+                    "DocumentFormat": "JSON",
+                    "DocumentType": "Policy",
+                    "Name": document_name,
+                    "Requires": [doc_requires1, doc_requires2],
+                    "Tags": [tag1, tag2],
+                    "TargetType": "TargetType",
+                    "UpdateMethod": "UpdateMethod",
+                    "VersionName": "VersionName",
+                },
+            }
+        },
+    }
