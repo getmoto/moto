8d4a53444daa278893167e4c5d26a35a79e36c47
Sayali Charhate <charhate@usc.edu>
2026-03-30 10:31:47 -0700

    lint

--- moto/acm/models.py
+++ moto/acm/models.py
@@ -174,7 +174,7 @@ class CertBundle(BaseModel):
 
         # Parse issuer CN, also optional
         issuer_cn_attrs = self._cert.issuer.get_attributes_for_oid(OID_COMMON_NAME)
-        self.issuer_common_name: str = (
+        self.issuer_common_name: str = str(
             issuer_cn_attrs[0].value if issuer_cn_attrs else ""
         )
 
