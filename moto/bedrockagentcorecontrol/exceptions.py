c0fa5b6a300b161ecd5cbd38317acd5d288572d7
Jack Danger <github@jackdanger.com>
2026-04-02 17:38:53 -0700

    BedrockAgentCoreControl: initial support with Agent Runtime CRUD (#9774)

---
+++ moto/bedrockagentcorecontrol/exceptions.py
@@ -0,0 +1,21 @@
+"""BedrockAgentCoreControl exceptions."""
+
+from moto.core.exceptions import JsonRESTError
+
+
+class BedrockAgentCoreControlClientError(JsonRESTError):
+    code = 400
+
+
+class ResourceNotFoundException(BedrockAgentCoreControlClientError):
+    code = 404
+
+    def __init__(self, message: str) -> None:
+        super().__init__("ResourceNotFoundException", message)
+
+
+class ConflictException(BedrockAgentCoreControlClientError):
+    code = 409
+
+    def __init__(self, message: str) -> None:
+        super().__init__("ConflictException", message)
