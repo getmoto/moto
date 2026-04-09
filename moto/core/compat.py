89b22dc47fa60acb19fe041b9e6ed959e1aae04a
Brian Pandola <bpandola@gmail.com>
2026-04-08 23:13:22 -0700

    Use find_spec to check package availability

--- moto/core/compat.py
+++ moto/core/compat.py
@@ -1,6 +1,4 @@
 from importlib.util import find_spec
 
-from botocore.compat import HAS_CRT as BOTOCORE_HAS_CRT_CHECK
-
-HAS_CRT = BOTOCORE_HAS_CRT_CHECK
+HAS_CRT = find_spec("awscrt") is not None
 HAS_CRC32C = find_spec("crc32c") is not None
