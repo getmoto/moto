2117d882455076a3369d9b93b64f73da5d69669b
algobarb <78746954+algobarb@users.noreply.github.com>
2026-07-24 16:43:24 -0400

    feat(paymentcrypto): fix alias signing backend

--- moto/paymentcryptography/exceptions.py
+++ moto/paymentcryptography/exceptions.py
@@ -4,10 +4,14 @@ from moto.core.exceptions import JsonRESTError
 
 
 class ResourceNotFoundException(JsonRESTError):
+    code = 404
+
     def __init__(self, msg: str):
         super().__init__("ResourceNotFoundException", f"{msg}")
 
 
 class ConflictException(JsonRESTError):
+    code = 409
+
     def __init__(self, msg: str):
         super().__init__("ConflictException", f"{msg}")
