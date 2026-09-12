4418c2c155318731c2a4fbf3e3b7293e97b7430c
Brian Pandola <bpandola@gmail.com>
2026-09-11 19:04:40 -0700

    Admin: fix test asserts after Botocore behavior change (#10259)

--- tests/test_scheduler/__init__.py
+++ tests/test_scheduler/__init__.py
@@ -3,18 +3,14 @@ from datetime import datetime, timedelta
 from moto.core.utils import utcnow
 
 
-def pytest_parametrize_test_create_get_schedule__with_start_date() -> list[
-    tuple[datetime, datetime]
-]:
+def pytest_parametrize_test_create_get_schedule__with_start_date() -> list[datetime]:
     now = utcnow()
     timedelta_kwargs = {"days": 1, "hours": 1, "weeks": 1}
     return_ = []
     for k, v in timedelta_kwargs.items():
-        to_append = now + timedelta(**{k: v})
-        return_.append((to_append, to_append.replace(microsecond=0)))
+        return_.append(now + timedelta(**{k: v}))
     else:
-        to_append = now.replace(year=now.year + 1)
-        return_.append((to_append, to_append.replace(microsecond=0)))
+        return_.append(now.replace(year=now.year + 1))
     return return_
 
 
